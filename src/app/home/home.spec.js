0c51bf935d639c1ff2d0ee6992d8edb643c56545
BenShaw <bshaw@avalanche.ca>
2014-05-22 15:56:39 -0700

    added MPLv2 license header to files

--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 /**
  * Tests sit right alongside the file they are testing, which is more intuitive
  * and portable than separating `src` and `test` directories. Additionally, the
