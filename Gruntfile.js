83d9477bb1e1d87c66c75cba23dd7004c62f85b1
BenShaw <bshaw@avalanche.ca>
2014-09-09 16:06:32 -0700

    Main Page Working on main page. Adding .scss and .jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -518,7 +518,7 @@ module.exports = function (grunt) {
         files: {
           '<%= yeoman.client %>/app/app.scss': [
             '<%= yeoman.client %>/{app,components}/**/*.{scss,sass}',
-            '!<%= yeoman.client %>/app/app.{scss,sass}'
+            '!<%= yeoman.client %>/app/*.{scss,sass}'
           ]
         }
       },
