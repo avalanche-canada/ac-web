1b569515210ffa5ab322523b64f7badad82147ba
BenShaw <bshaw@avalanche.ca>
2014-09-10 13:40:11 -0700

    styling mainpage

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,7 +8,6 @@ angular.module('avalancheCanadaApp')
           link: function(scope, ele, attrs) {
               angular.element(ele).fullpage({
                 anchors: ['mapPage', 'infoPage'],
-                sectionsColor: ['#4A6FB1', '#939FAA', '#323539'],
                 scrollOverflow: true
             });
           }
@@ -16,17 +15,5 @@ angular.module('avalancheCanadaApp')
   })
 
   .controller('MainCtrl', function ($scope, $http) {
-
-    /*$('#fullpage').fullpage({
-                anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
-                sectionsColor: ['#4A6FB1', '#939FAA', '#323539'],
-                scrollOverflow: true
-            });*/
-
-    //$scope.awesomeThings = [];
-
-    //$http.get('/api/things').success(function(awesomeThings) {
-    //  $scope.awesomeThings = awesomeThings;
-    //});
 
   });
