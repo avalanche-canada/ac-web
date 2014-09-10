671ce8bd53bc34409ba4336bda91ecb2e6302389
BenShaw <bshaw@avalanche.ca>
2014-09-10 13:26:10 -0700

    working on styling

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,8 +7,8 @@ angular.module('avalancheCanadaApp')
           restrict: 'E',
           link: function(scope, ele, attrs) {
               angular.element(ele).fullpage({
-                //anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
-                //sectionsColor: ['#4A6FB1', '#939FAA', '#323539'],
+                anchors: ['mapPage', 'infoPage'],
+                sectionsColor: ['#4A6FB1', '#939FAA', '#323539'],
                 scrollOverflow: true
             });
           }
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -24,50 +24,42 @@ angular.module('avalancheCanadaApp')
       };
   })
 
+  .directive('navbarHeaderMenu', function() {
+      return {
+          restrict: 'E',
+          templateUrl: 'components/navbar/header.html',
+          link: function(scope, ele, attrs) {
+             //custom js for the left of the mega menu fun
+              /*$("ul.nav > li").each(function(){
+                  //assign li id and rebuild for jquery
+                  li_id = "#" + this.id;
+                  //get left position of li parent
+                  yamm_offset = $(li_id).offset();
+                  yamm_left = "-=" + yamm_offset.left;
+                  //rebuild jquery element id
+                  li_id_drop_down = li_id + " .dropdown-menu";
+                  //assign the left position to the mega menu
+                  $(li_id_drop_down).css("left", yamm_left);
+              });
+              //Returns width of browser viewport
+              var browser_viewport = $(window).width();
+              //set the yamm - mega menu width
+              $(".dropdown-menu").width(browser_viewport);
+              //weird hover off bug. Need to apply open class and remove - http://stackoverflow.com/questions/8878033/how-to-make-twitter-bootstrap-menu-dropdown-on-hover-rather-than-click
+              $('ul.nav li.dropdown').hover(function() {
+                $(this).addClass('open');
+              }, function() {
+                $(this).removeClass('open');
+              });
+              /**/
+          }
+      };
+  })
+
   .controller('NavbarCtrl', function ($scope, $location) {
 
-    /*
-   $('#menu').multilevelpushmenu({
-        containersToPush: [$('#page-wrap')],
-        menuWidth: '320px',
-        menuHeight: '100%',
-        collapsed: true,
-        fullCollapse: true,
-        backText: '',
-        overlapWidth:60
-    });
-    $('#menu').multilevelpushmenu('option', 'menuHeight', $(document).height());
-    $('#menu').multilevelpushmenu('redraw');
-    //Multi Menu Expand
-    $('#multi-expand').click(function(){
-      $('#menu').multilevelpushmenu('expand');
-    }); */
-
-
     //! \todo get this working
-    //custom js for the left of the mega menu fun
-    /*$("ul.nav > li").each(function(){
-        //assign li id and rebuild for jquery
-        li_id = "#" + this.id;
-        //get left position of li parent
-        yamm_offset = $(li_id).offset();
-        yamm_left = "-=" + yamm_offset.left;
-        //rebuild jquery element id
-        li_id_drop_down = li_id + " .dropdown-menu";
-        //assign the left position to the mega menu
-        $(li_id_drop_down).css("left", yamm_left);
-    });
-    //Returns width of browser viewport
-    var browser_viewport = $(window).width();
-    //set the yamm - mega menu width
-    $(".dropdown-menu").width(browser_viewport);
-    //weird hover off bug. Need to apply open class and remove - http://stackoverflow.com/questions/8878033/how-to-make-twitter-bootstrap-menu-dropdown-on-hover-rather-than-click
-    $('ul.nav li.dropdown').hover(function() {
-      $(this).addClass('open');
-    }, function() {
-      $(this).removeClass('open');
-    });
-    /**/
+
 
     $scope.isCollapsed = true;
 
