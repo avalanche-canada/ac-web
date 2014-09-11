feac1c40db269383f41953eb6b23b89e5288dbe2
BenShaw <bshaw@avalanche.ca>
2014-09-11 11:10:55 -0700

    removed openshift as a choice for port

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -4,20 +4,18 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
+  ip:       process.env.IP ||
             undefined,
 
   // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
+  port:     process.env.PORT ||
             8080,
 
   // MongoDB connection options
-  mongo: {
+  /*mongo: {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/avalanchecanada'
-  }
+  }*/
 };
