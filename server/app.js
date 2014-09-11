2f8b60a15aec3b5a27e34302a950860283ce611e
BenShaw <bshaw@avalanche.ca>
2014-09-11 12:16:32 -0700

    footer working. server working locallly

--- server/app.js
+++ server/app.js
@@ -4,8 +4,8 @@
 
 'use strict';
 
-// Set default node environment to production
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
