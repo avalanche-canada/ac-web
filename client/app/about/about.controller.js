1da7c39f9a17a904867ac980d7d8e566513abb64
BenShaw <bshaw@avalanche.ca>
2014-09-22 08:18:00 -0700

    added about page

---
+++ client/app/about/about.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('avalancheCanadaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/about/about.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('avalancheCanadaApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutCtrl'
+      });
+  });
