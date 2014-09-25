5e917fbed0bd953e6e7ecd854754a2c3761b820d
Yves Richard <yves.richard@tesera.com>
2014-09-25 13:40:24 -0600

    major markup refactor... proceed with care
    Added ngAnnimate and ngRoute. Switched client routing to ngRoute.
    Transitions not working; wip.

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,21 +1,31 @@
 'use strict';
 
 angular.module('avalancheCanadaApp')
-  .config(function ($stateProvider) {
-    $stateProvider
-        .state('main', {
-            abstract: true,
-            url: '/',
-            templateUrl: 'app/main/main.html'
+    .config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/main/map.html'
         })
-        .state('main.map', {
-            url: '',
-            templateUrl: 'app/main/map.html',
-            controller: 'MapCtrl'
-        })
-        .state('main.more', {
-            url: '^/more',
+        .when('/more', {
             templateUrl: 'app/main/more.html',
             controller: 'MoreCtrl'
         });
   });
+  // .config(function ($stateProvider) {
+  //   $stateProvider
+  //       .state('main', {
+  //           abstract: true,
+  //           url: '/',
+  //           templateUrl: 'app/main/main.html'
+  //       })
+  //       .state('main.map', {
+  //           url: '',
+  //           templateUrl: 'app/main/map.html',
+  //           controller: 'MapCtrl'
+  //       })
+  //       .state('main.more', {
+  //           url: '^/more',
+  //           templateUrl: 'app/main/more.html',
+  //           controller: 'MoreCtrl'
+  //       });
+  // });
