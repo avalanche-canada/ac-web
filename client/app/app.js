b186b6ca775224c896ca1ba53283e789f784ff1b
BenShaw <bshaw@avalanche.ca>
2014-10-06 13:24:42 -0700

    added angular-prismicio to project

--- client/app/app.js
+++ client/app/app.js
@@ -14,12 +14,30 @@ angular.module('avalancheCanadaApp', [
           .otherwise('/');
 
         $locationProvider.html5Mode(true);
+
+        //! Prismic.io configuration
+        PrismicProvider.setApiEndpoint('https://avalancheca.prismic.io/api');
+        PrismicProvider.setAccessToken('');
+        PrismicProvider.setClientId('');
+        PrismicProvider.setClientSecret('');
+        PrismicProvider.setLinkResolver(function(ctx, doc) {
+            return '#/document/' + doc.id + '/' + doc.slug + ctx.maybeRefParam;
+        });
+
     })
 
     .run(function(ENV, $rootScope) {
         //! make env (environemnt constants) available globaly
         $rootScope.env = ENV;
     });
+
+
+
+
+
+
+
+
 
 
 
