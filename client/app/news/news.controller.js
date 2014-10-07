be35ffffbbfa16d8cf3b527b1ac7ad33d77416ce
BenShaw <bshaw@avalanche.ca>
2014-10-06 18:35:16 -0700

    linked news to prismic

--- client/app/news/news.controller.js
+++ client/app/news/news.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('avalancheCanadaApp')
-  .controller('NewsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('NewsCtrl', function ($scope, Prismic, $log) {
+
+     Prismic.ctx().then(function(ctx){
+        var query = '[[:d = at(document.type, "blog")]]';
+        ctx.api.form('everything').query(query).ref(ctx.ref).submit(function(err, documents){
+            if (err) {
+                $log.error('error getting news events from prismic');
+            }
+            else {
+                $scope.documents = documents;
+                // Angular doesn't repeat over collections created on the fly, so we have to create it here
+                if (documents.total_pages > 1) $scope.paginationRange = _.range(1, documents.total_pages+1);
+            }
+        });
+    });
+
   });
