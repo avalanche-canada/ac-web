6e203bdcfb231dc1c2ca87d076ac86ac23b0dd65
BenShaw <bshaw@avalanche.ca>
2014-10-08 08:41:56 -0700

    working on news

--- client/app/news/newsItem.controller.js
+++ client/app/news/newsItem.controller.js
@@ -3,14 +3,14 @@
 angular.module('avalancheCanadaApp')
   .controller('NewsItemCtrl', function ($scope, Prismic, $log, $routeParams, $location) {
 
-    Prismic.document($routeParams.id).then(function(document){
-        if (document.slug === $routeParams.slug) {
+    Prismic.document($routeParams.id).then(function(doc){
+        if (doc.slug === $routeParams.slug) {
             Prismic.ctx().then(function(ctx) {
-                $scope.documentHtml = document.asHtml(ctx);
+                $scope.documentHtml =  doc.getStructuredText('news.body').asHtml(ctx);//doc.asHtml(ctx);
             });
         }
-        else if (document.slugs.indexOf($routeParams.slug) >= 0) {
-            $location.path('/news/'+document.id+'/'+document.slug);
+        else if (doc.slugs.indexOf($routeParams.slug) >= 0) {
+            $location.path('/news/'+doc.id+'/'+doc.slug);
         }
         else {
             // Should display some kind of error; will just redirect to / for now
