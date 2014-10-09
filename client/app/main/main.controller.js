10e669c6baeeaad6b50d980b90df0f1ee08d3eab
BenShaw <bshaw@avalanche.ca>
2014-10-09 15:04:44 -0700

    news section cleaned up

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -22,12 +22,14 @@ angular.module('avalancheCanadaApp')
             }
         });
 
-        ctx.api.form('everything').query('[[:d = at(document.type, "news")] [:d = at(document.tags, ["featured"])]]').ref(ctx.ref).submit(function(err, documents){
+        ctx.api.form('everything').query('[[:d = at(document.type, "news")] [:d = any(document.tags, ["featured"])]]').ref(ctx.ref).submit(function(err, documents){
             if (err) {
                 $log.error('error getting featured news events from prismic');
             }
             else {
-                $scope.news_featured = documents.results;
+                $scope.news_featured = documents.results[0];
+                _.pull($scope.news, $scope.news_featured);
+                $scope.news = $scope.news.slice(0,2);
             }
         });
 
