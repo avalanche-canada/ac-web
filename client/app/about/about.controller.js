c95b711f53cb7ffae9d5a77cafdca277e494a889
BenShaw <bshaw@avalanche.ca>
2014-10-17 14:39:24 -0700

    adding sections as page anchors

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -5,12 +5,42 @@ angular.module('avalancheCanadaApp')
 
     Prismic.ctx().then(function(ctx){
 
+        /*$scope.overview   = '',
+            $scope.board  = '',
+            $scope.awards = '',
+            $scope.board  = '',
+            $scope.vision = '';*/
+
         $scope.ctx = ctx;
 
-        Prismic.bookmark('about').then(function(document){
-                $scope.documentHtml = document.asHtml(ctx);
+        Prismic.bookmark('about-overview').then(function(doc){
+                $scope.overview = doc.getStructuredText('generic.body').asHtml(ctx);
+            });
+
+        Prismic.bookmark('about-board').then(function(doc){
+                $scope.board = doc.getStructuredText('generic.body').asHtml(ctx);
         });
 
+        Prismic.bookmark('about-awards').then(function(doc){
+                $scope.awards =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        Prismic.bookmark('about-board').then(function(doc){
+                $scope.board =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        Prismic.bookmark('about-vision').then(function(doc){
+                $scope.vision =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        ctx.api.form('everything').query('[[:d = at(document.type, "staff")]]').ref(ctx.ref).submit(function(err, doc){
+            if (err) {
+                $log.error('error getting events from prismic');
+            }
+            else {
+                $scope.staff = doc.results;
+            }
+        });
 
     });
 
--- client/app/sponsors/sponsors.controller.js
+++ client/app/sponsors/sponsors.controller.js
@@ -7,10 +7,29 @@ angular.module('avalancheCanadaApp')
 
         $scope.ctx = ctx;
 
-        Prismic.bookmark('sponsors').then(function(document){
-                $scope.documentHtml = document.asHtml(ctx);
+        Prismic.bookmark('sponsor-associate').then(function(doc){
+                $scope.associate =  doc.getStructuredText('generic.body').asHtml(ctx);
         });
 
+        Prismic.bookmark('sponsor-brand').then(function(doc){
+                $scope.brand =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        Prismic.bookmark('sponsor-founding').then(function(doc){
+                $scope.founding =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        Prismic.bookmark('sponsor-partner').then(function(doc){
+                $scope.partner =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        Prismic.bookmark('sponsor-programs').then(function(doc){
+                $scope.programs =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
+
+        Prismic.bookmark('sponsor-supplier').then(function(doc){
+                $scope.supplier =  doc.getStructuredText('generic.body').asHtml(ctx);
+        });
 
     });
 
