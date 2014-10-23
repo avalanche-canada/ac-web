99307bf1843ddb24a9f417698c75a923313fd1d8
Yves Richard <yves.richard@tesera.com>
2014-10-23 11:43:24 -0600

    add cors to api

--- server/config/express.js
+++ server/config/express.js
@@ -14,10 +14,12 @@ var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
+var cors = require('cors');
 
 module.exports = function(app) {
   var env = app.get('env');
 
+  app.use(cors());
   app.set('views', config.root + '/server/views');
   app.set('view engine', 'jade');
   app.use(compression());
