29c61f50d0ea14e6acd294c3e390e2e04e7d3dd4
BenShaw <bshaw@avalanche.ca>
2014-11-12 18:09:47 -0800

    collaborators

--- client/app/collaborators/collaborators.js
+++ client/app/collaborators/collaborators.js
@@ -16,6 +16,23 @@ angular.module('avalancheCanadaApp')
       .state('ac.collaborators', {
         url: '^/collaborators',
         templateUrl: 'app/collaborators/collaborators.html',
-        controller: 'CollaboratorsCtrl'
+        controller: ['Prismic', '$scope',
+            function (Prismic, $scope) {
+
+                Prismic.ctx().then(function(ctx){
+
+                    $scope.ctx = ctx;
+
+                    Prismic.bookmark('collaborators-government').then(function(result){
+                            $scope.government = result.getStructuredText('generic.body').asHtml(ctx);
+                    });
+
+                    Prismic.bookmark('collaborators-other').then(function(result){
+                            $scope.other = result.getStructuredText('generic.body').asHtml(ctx);
+                    });
+
+
+                });
+            }]
       });
   });
