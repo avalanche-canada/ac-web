e1def672dc4d005ab5e0634ff634f973edbc1f3d
BenShaw <bshaw@avalanche.ca>
2014-11-13 17:51:53 -0800

    fixed jshint errors

--- client/app/sponsors/sponsors.controller.js
+++ client/app/sponsors/sponsors.controller.js
@@ -3,13 +3,9 @@
 angular.module('avalancheCanadaApp')
   .controller('SponsorsCtrl', function ($scope, Prismic) {
 
-Prismic.ctx().then(function(ctx){
+    Prismic.ctx().then(function(ctx){
 
         $scope.ctx = ctx;
-        var partner = '',
-            founding = '',
-            supplier= '',
-            associate = '';
 
         var getSponsor = function (tag)
         {
@@ -39,42 +35,12 @@ Prismic.ctx().then(function(ctx){
                     }
                 }
             });
-        }
+        };
 
         getSponsor('partner');
         getSponsor('founding sponsor');
         getSponsor('supplier');
         getSponsor('associate');
     });
-/*
-    Prismic.ctx().then(function(ctx){
-
-        $scope.ctx = ctx;
-
-        Prismic.bookmark('sponsor-associate').then(function(doc){
-                $scope.associate =  doc.getStructuredText('generic.body').asHtml(ctx);
-        });
-
-        Prismic.bookmark('sponsor-brand').then(function(doc){
-                $scope.brand =  doc.getStructuredText('generic.body').asHtml(ctx);
-        });
-
-        Prismic.bookmark('sponsor-founding').then(function(doc){
-                $scope.founding =  doc.getStructuredText('generic.body').asHtml(ctx);
-        });
-
-        Prismic.bookmark('sponsor-partner').then(function(doc){
-                $scope.partner =  doc.getStructuredText('generic.body').asHtml(ctx);
-        });
-
-        Prismic.bookmark('sponsor-programs').then(function(doc){
-                $scope.programs =  doc.getStructuredText('generic.body').asHtml(ctx);
-        });
-
-        Prismic.bookmark('sponsor-supplier').then(function(doc){
-                $scope.supplier =  doc.getStructuredText('generic.body').asHtml(ctx);
-        });
-
-    });*/
 
   });
