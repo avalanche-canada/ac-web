941177f406662dcc9c5503ba903a1bc439e5e88c
BenShaw <bshaw@avalanche.ca>
2014-11-16 12:53:26 -0800

    added weather section

---
+++ client/app/weather/weather.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('avalancheCanadaApp')
+.config(function ($stateProvider) {
+    $stateProvider
+      .state('ac.weather', {
+        url: '^/weather',
+        templateUrl: 'app/weather/weather.html',
+        controller: ['Prismic', '$scope',
+            function (Prismic, $scope) {
+
+                $scope.getDate = function(item){
+                    return moment.utc(item.getDate('weather-forecast.date')).format('dddd MMMM Do');
+                };
+                //! \todo decrement index to show previous forecasts
+                $scope.index = 0;
+
+                Prismic.ctx().then(function(ctx){
+
+                    $scope.ctx = ctx;
+                    var query = '[[:d = at(document.type, "weather-forecast")]]';
+                    ctx.api.form('everything').query(query)
+                                                .orderings('[my.weather-forecast.date desc]')
+                                                    .ref(ctx.ref).submit(function(err, documents){
+                        if (err) {
+                            $log.error('error getting blogs from prismic');
+                        }
+                        else {
+                            $scope.forecastContent = documents.results[$scope.index];
+                        }
+                    });
+
+                });
+            }]
+      })
+
+  });
+
