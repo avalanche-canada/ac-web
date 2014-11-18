7b2e751d58b97e5611ca7b63edf3faaa2a400dcb
BenShaw <bshaw@avalanche.ca>
2014-11-18 09:21:47 -0800

    added youth and date filter

---
+++ client/components/filters/dateUtc.js
@@ -0,0 +1,8 @@
+angular.module('avalancheCanadaApp.filters')
+    .filter('dateUtc', function () {
+        return function (datePST) {
+            if (datePST) {
+                return moment.utc(datePST).format('YYYY-MM-DD') ;
+            }
+        };
+    });
