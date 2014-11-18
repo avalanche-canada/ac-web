e1e70b1edf8b2f82cacba47da1f2e9c22e4f9c2d
BenShaw <bshaw@avalanche.ca>
2014-11-18 15:11:35 -0800

    added details to footer items

--- client/app/about/about.js
+++ client/app/about/about.js
@@ -1,19 +1,32 @@
 'use strict';
 
 angular.module('avalancheCanadaApp')
-    /*.config(function ($routeProvider) {
-
-        $routeProvider
-            .when('/about', {
-                templateUrl: 'app/about/about.html',
-                controller: 'AboutCtrl'
-            });
-    });*/
   .config(function ($stateProvider) {
     $stateProvider
       .state('ac.about', {
         url: '^/about',
         templateUrl: 'app/about/about.html',
         controller: 'AboutCtrl'
+      })
+      .state('ac.terms', {
+        url: '^/tou',
+        templateUrl: 'app/about/tou.html',
+        controller: ['Prismic', '$scope',
+            function (Prismic, $scope) {
+
+                Prismic.ctx().then(function(ctx){
+
+                    $scope.ctx = ctx;
+
+                    Prismic.bookmark('about-tou').then(function(doc){
+                            $scope.tou = doc.getStructuredText('generic.body').asHtml(ctx);
+                    });
+
+                    Prismic.bookmark('about-privacy').then(function(doc){
+                            $scope.privacy = doc.getStructuredText('generic.body').asHtml(ctx);
+                    });
+
+                });
+            }]
       });
   });
