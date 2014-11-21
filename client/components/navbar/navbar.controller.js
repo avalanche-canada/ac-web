945251d62fafbf4b8a2cf5cf4710621476eef9d0
BenShaw <bshaw@avalanche.ca>
2014-11-21 10:50:23 -0800

    added trip planner and removed weather

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -108,8 +108,8 @@ angular.module('avalancheCanadaApp')
     $scope.dataMenu = [//{'display':'Observations','url':'/observations'},
                        //{'display':'Incidents','url':'/incidents'},
                        {'display':'Blogs','url':'/blogs'},
-                       {'display':'Summaries & Outlooks','url':'/conditions'},
-                       {'display':'Weather Forecasts','url':'/weather'}];
+                       {'display':'Summaries & Outlooks','url':'/conditions'}];//,
+                       //{'display':'Weather Forecasts','url':'/weather'}];
 
     $scope.youth = [{'url':'#overview','display':'Overview'},
                     {'url':'#programs','display':'Programs'},
