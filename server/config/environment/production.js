525df008ddf09c94701e1d79596e0f08554d6593
Yves Richard <yves.richard@tesera.com>
2014-11-24 12:08:03 -0700

    add boundary req inspector

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,21 +1,11 @@
 'use strict';
+require('graphdat');
 
 // Production specific configuration
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.IP ||
-            undefined,
-
+  ip:       process.env.IP || undefined,
   // Server port
-  port:     process.env.PORT ||
-            8080,
-
-  // MongoDB connection options
-  /*mongo: {
-    uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
-            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/avalanchecanada'
-  }*/
+  port:     process.env.PORT || 8080,
 };
