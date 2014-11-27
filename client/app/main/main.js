acf3b5cf986a44fb22e2ad8de71d13d37e42d723
Yves Richard <yves.richard@tesera.com>
2014-11-26 20:21:38 -0700

    adds share route

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -6,7 +6,30 @@ angular.module('avalancheCanadaApp')
             .state('ac.map', {
                 url: '',
                 templateUrl: 'app/main/map.html',
-                controller: 'MapCtrl'
+                controller: 'MapCtrl',
+                resolve: {
+                  obs: function (acObservation) {
+                    return acObservation.byPeriod('2:days');
+                  },
+                  ob: function () {
+                    return null;
+                  }
+                }
+            })
+            .state('ac.share', {
+                url: '^/share/:obid',
+                templateUrl: 'app/main/map.html',
+                controller: 'MapCtrl',
+                resolve: {
+                  obs: function ($http) {
+                    return [];
+                  },
+                  ob: function ($stateParams, $http) {
+                    return $http.get('/api/min/observations/' + $stateParams.obid).then(function (res) {
+                        return [res.data];
+                    });
+                  }
+                }
             })
             .state('ac.sled', {
                 url: '^/sled',
