b4e8bc56fe2032bd3f3d973fdf16df025dfeda65
BenShaw <bshaw@avalanche.ca>
2014-12-01 11:44:07 -0800

    added submit route

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -105,10 +105,9 @@ angular.module('avalancheCanadaApp')
     $scope.forecastRegionList3 = $scope.forecastRegions.slice((third*2), length+1);
 
     $scope.dataMenu = [{'link':'<a href="/blogs" data-toggle="collapse" data-target=".navbar-collapse"><h2>Blogs<i class="fa fa-angle-right"></i></h2></a>'},
-                        {'link':'<a href="http://old.avalanche.ca/cac/library/incident-report-database/view" data-toggle="collapse" data-target=".navbar-collapse" target="_blank"><h2>Historic Incidents<i class="fa fa-angle-right"></i></h2></a>'}];
-                        //{'display':'Observations','url':'/observations'},
-                       //{'display':'Incidents','url':'/incidents'},
-                       //{'display':'Blogs','url':'/blogs'}]//,
+                        {'link':'<a href="http://old.avalanche.ca/cac/library/incident-report-database/view" data-toggle="collapse" data-target=".navbar-collapse" target="_blank"><h2>Historic Incidents<i class="fa fa-angle-right"></i></h2></a>'},
+                        {'link':'<a href="/submit" data-toggle="collapse" data-target=".navbar-collapse"><h2>Submit Information<i class="fa fa-angle-right"></i></h2></a>'}];//,
+
                        //{'display':'Summaries & Outlooks','url':'/conditions'}];//,
                        //{'display':'Weather Forecasts','url':'/weather'}];
 
