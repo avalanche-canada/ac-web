4b2616cc4df6a1447caf6d3cb29ef70871660a4d
Yves Richard <yves.richard@tesera.com>
2014-12-01 06:53:58 -0700

    adds httpInterceptor for api calls & cleans app.js

--- client/app/app.js
+++ client/app/app.js
@@ -15,18 +15,78 @@ angular.module('avalancheCanadaApp', [
         'prismic.io',
         'acComponents',
         'foundation',
-        'ngToast'
+        'ngToast',
         'auth0',
         'angular-storage',
         'angular-jwt'
     ])
 
-    .config(function ($locationProvider, PrismicProvider, $stateProvider, $urlRouterProvider, $sceProvider, authProvider) {
-
-        //! \todo *hack* set up $sce properly so that it doesnt remove iframes from prismic content
-        $sceProvider.enabled(false);
+    // main module configuration                            
+    .config(function ($locationProvider, $stateProvider, $urlRouterProvider, $sceProvider) {
+        $sceProvider.enabled(false); //! \todo *hack* set up $sce properly so that it doesnt remove iframes from prismic content
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
+
+        $urlRouterProvider.otherwise('/');
+
+        //! define abstract ac state
+        $stateProvider
+            .state('ac', {
+                abstract: true,
+                url: '/',
+                templateUrl: 'app/template.html',
+                controller: ['$scope','$state',  function($scope, $state){
+                    $scope.url = $state.href($state.current.name, $state.params, {absolute: true, inherit: true});
+                }]
+            });
+    })
+
+    // Prismic.io configuration
+    .config(function (PrismicProvider) {
+
+        PrismicProvider.setApiEndpoint('https://avalancheca.prismic.io/api');
+        PrismicProvider.setAccessToken('');
+        PrismicProvider.setClientId('');
+        PrismicProvider.setClientSecret('');
+        PrismicProvider.setLinkResolver(function(ctx, documentLink) {
+            var link = '';
+
+            if (documentLink.isBroken) {
+                console.log.error('prismic link resolver documentLink.isBroken');
+            } else {
+                /* Static pages based on bookmark name of the document */
+                if(documentLink.id === ctx.api.bookmarks.about) {
+                    link = '/about' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
+                }
+                if(documentLink.id === ctx.api.bookmarks.jobs) {
+                    link = '/jobs' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
+                }
+
+                /* Based on document type of the document */
+                if(documentLink.type === 'events') {
+                    link = '/events/' + documentLink.id + '/' + documentLink.slug + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
+                }
+                if(documentLink.type === 'news') {
+                    link = '/news/' + documentLink.id + '/' + documentLink.slug + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
+                }
+            }
+
+            return link;
+        });
+
+    })
+
+    // ng toast configuration
+    .config(['ngToastProvider', function(ngToast) {
+        ngToast.configure({
+          verticalPosition: 'top',
+          horizontalPosition: 'center',
+          maxNumber: 1
+        });
+    }])
+
+    // auth0 configuration
+    .config(function (authProvider, $httpProvider, jwtInterceptorProvider) {
 
         authProvider.init({
             domain: 'avalancheca.auth0.com',
@@ -53,96 +113,19 @@ angular.module('avalancheCanadaApp', [
             store.remove('token');
         });
 
-        //$uiViewScrollProvider.defaultScroll = 'anchor';
-        $uiViewScrollProvider.useAnchorScroll();
-        //$anchorScrollProvider.disableAutoScrolling();
-        $urlRouterProvider.otherwise('/');
+        jwtInterceptorProvider.tokenGetter = function(store) {
+            return store.get('token');
+        }
 
-        //! define abstract ac state
-        $stateProvider
-            .state('ac', {
-                abstract: true,
-                url: '/',
-                templateUrl: 'app/template.html',
-                controller: ['$scope','$state',  function($scope, $state){
-                    $scope.url = $state.href($state.current.name, $state.params, {absolute: true, inherit: true});
-                }]
-            })
-        ;
-
-        //! Prismic.io configuration
-        PrismicProvider.setApiEndpoint('https://avalancheca.prismic.io/api');
-        PrismicProvider.setAccessToken('');
-        PrismicProvider.setClientId('');
-        PrismicProvider.setClientSecret('');
-        PrismicProvider.setLinkResolver(function(ctx, documentLink) {
-            var retVal = '';
-
-            if (documentLink.isBroken) {
-                console.log.error('prismic link resolver documentLink.isBroken');
-            }
-            else
-            {
-                /* Static pages based on bookmark name of the document */
-                if(documentLink.id === ctx.api.bookmarks.about) {
-                    retVal = '/about' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-                if(documentLink.id === ctx.api.bookmarks.jobs) {
-                    retVal = '/jobs' + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-
-                /* Based on document type of the document */
-                if(documentLink.type === 'events') {
-                    retVal = '/events/' + documentLink.id + '/' + documentLink.slug + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-                if(documentLink.type === 'news') {
-                    retVal = '/news/' + documentLink.id + '/' + documentLink.slug + (ctx.maybeRef ? '?ref=' + ctx.maybeRef : '');
-                }
-            }
-
-            return retVal;
-        });
-
+        $httpProvider.interceptors.push('jwtInterceptor');
     })
-
-
-    .config(['ngToastProvider', function(ngToast) {
-        //! ng toast configuration
-        ngToast.configure({
-          verticalPosition: 'top',
-          horizontalPosition: 'center',
-          maxNumber: 1
-        });
-    }])
-
-    // .config(['$httpProvider', 'jwtInterceptorProvider', function ($httpProvider, jwtInterceptorProvider) {
-    //     jwtInterceptorProvider.tokenGetter = function(store) {
-    //         // Return the saved token
-    //         return store.get('token');
-    //     };
-
-    //     $httpProvider.interceptors.push('jwtInterceptor');
-    // }])
 
     .run(function(ENV, $rootScope, $location, auth, store, jwtHelper) {
         //! make env (environemnt constants) available globaly
         $rootScope.env = ENV;
 
+        // hooks routing requiresLogin data attribute
         auth.hookEvents();
-
-        // $rootScope.$on('$locationChangeStart', function() {
-        //     if (!auth.isAuthenticated) {
-        //       var token = store.get('token');
-        //       if (token) {
-        //         if (!jwtHelper.isTokenExpired(token)) {
-        //           auth.authenticate(store.get('profile'), token);
-        //         } else {
-        //           // Either show Login page or use the refresh token to get a new idToken
-        //           $location.path('/');
-        //         }
-        //       }
-        //     }
-        // });
     })
 
     .controller('HighlighCtrl', function (ngToast, Prismic, $log) {
