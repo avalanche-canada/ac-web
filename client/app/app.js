8866db21a0161167e571dfad98c64dc0724e50a3
Yves Richard <yves.richard@tesera.com>
2014-12-04 09:38:55 -0700

    fixes jshint error

--- client/app/app.js
+++ client/app/app.js
@@ -119,7 +119,7 @@ angular.module('avalancheCanadaApp', [
         $httpProvider.interceptors.push('jwtInterceptor');
     }])
 
-    .run(function ($rootScope, auth, store, jwtHelper, $location, ENV, $state) {
+    .run(function ($rootScope, auth, store, jwtHelper, $location, ENV) {
         //! make env (environemnt constants) available globaly
         $rootScope.env = ENV;
 
