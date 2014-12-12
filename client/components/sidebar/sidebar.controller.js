7d766b4409276561a164b2a5c86fec67ccae3789
BenShaw <bshaw@avalanche.ca>
2014-12-12 10:12:11 -0800

    added sidebar

---
+++ client/components/sidebar/sidebar.controller.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('avalancheCanadaApp')
+  .controller('SidebarCtrl', function ($scope, Prismic) {
+
+    $scope.items = [];
+    Prismic.ctx().then(function(ctx){
+
+        $scope.ctx = ctx;
+        var query = '[[:d = any(document.tags, ["featured"])]]';
+        ctx.api.form('everything').query(query)
+            .orderings('[my.blog.date desc, my.event.start_date, my.news.date desc]')
+                .ref(ctx.ref).submit(function(err, documents){
+            if (err) {
+                $log.error('error getting blogs events from prismic');
+            }
+            else {
+                var items = documents.results.slice(0,5);
+
+                _.forEach(items, function(item){
+                    switch(item.type){
+                        case 'blog':
+                            $scope.items.push({
+                                'title': item.getText('blog.shortlede'),
+                                'link': '/blogs/'+ item.id + '/' + item.slug
+                            });
+                            break;
+                        case 'news':
+                            $scope.items.push({
+                                'title': item.getText('news.shortlede'),
+                                'link': '/news/'+ item.id + '/' + item.slug
+                            });
+                            break;
+                        case 'event':
+                            //! \todo do we want to include events ??
+                            /*$scope.items.push({
+                                'title': item.getText('event.title'),
+                                'link': '/events/'+ item.id + '/' + item.slug
+                            });*/
+                            break;
+                    }
+                });
+
+            }
+        });
+    });
+
+  });
