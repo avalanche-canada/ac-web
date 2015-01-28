6200050ecb1dc5c2d22a0fba374efe75739e5085
BenShaw <bshaw@avalanche.ca>
2015-01-27 17:47:26 -0800

    added resources to youth section

--- client/app/youth/youth.js
+++ client/app/youth/youth.js
@@ -9,11 +9,29 @@ angular.module('avalancheCanadaApp')
         controller: 'YouthCtrl'
       });
   })
-  .controller('YouthCtrl', function ($scope , Prismic) {
+  .controller('YouthCtrl', function ($scope , Prismic, $log) {
 
-
+        $scope.sections = ['powerpoints','photo','video','map'];
+        var resourceList = {};
 
         Prismic.ctx().then(function(ctx){
+
+            $scope.sections.forEach(function(elm, index, array){
+                var query  = '[[:d = any(document.type, ["resource"])][:d = any(document.tags, ["youth"])][:d = any(document.tags, ["'+elm+'"])]]';
+                $log.info(query);
+                ctx.api.form('everything').query(query)
+                        .ref(ctx.ref).submit(function(err, documents){
+                    if (err) {
+                        $log.error('error getting sponsor from prismic');
+                    }
+                    else {
+                        resourceList[elm] = documents.results;
+                    }
+                });
+            });
+            $scope.resourceList = resourceList;
+
+
             Prismic.bookmark('youth-overview').then(function(doc){
                     $scope.overview = doc.getStructuredText('generic.body').asHtml(ctx);
             });
@@ -22,7 +40,7 @@ angular.module('avalancheCanadaApp')
                     $scope.programs = doc.getStructuredText('generic.body').asHtml(ctx);
             });
 
-             Prismic.bookmark('youth-resources').then(function(doc){
+             Prismic.bookmark('youth-resource-text').then(function(doc){
                     $scope.resources = doc.getStructuredText('generic.body').asHtml(ctx);
             });
 
