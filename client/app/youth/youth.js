1ccd766b01209bfcaddd8ba3a4a87cdc62c4db29
BenShaw <bshaw@avalanche.ca>
2015-01-30 13:21:30 -0800

    updated youth page

--- client/app/youth/youth.js
+++ client/app/youth/youth.js
@@ -11,42 +11,59 @@ angular.module('avalancheCanadaApp')
   })
   .controller('YouthCtrl', function ($scope , Prismic, $log) {
 
-        $scope.sections = ['powerpoints','photo','video','map','internet','handout','activities','ast','newsletter'];
+        $scope.tags =  []; //['powerpoints','photo','video','map','internet','handout','activities','ast','newsletter'];
         var resourceList = {};
 
         Prismic.ctx().then(function(ctx){
-
-            $scope.sections.forEach(function(elm){
-                var query  = '[[:d = any(document.type, ["resource"])][:d = any(document.tags, ["youth"])][:d = any(document.tags, ["'+elm+'"])]]';
-                $log.info(query);
-                ctx.api.form('everything').query(query)
-                        .ref(ctx.ref).submit(function(err, documents){
-                    if (err) {
-                        $log.error('error getting sponsor from prismic');
-                    }
-                    else {
-                        resourceList[elm] = documents.results;
-                    }
+            var getResources = function(){
+                $scope.tags.forEach(function(elm){
+                    var query  = '[[:d = any(document.type, ["resource"])][:d = any(document.tags, ["youth"])][:d = any(document.tags, ["'+elm+'"])]]';
+                    $log.info(query);
+                    ctx.api.form('everything').query(query)
+                            .ref(ctx.ref).submit(function(err, documents){
+                        if (err) {
+                            $log.error('error getting sponsor from prismic');
+                        }
+                        else {
+                            resourceList[elm] = documents.results;
+                        }
+                    });
                 });
-            });
-            $scope.resourceList = resourceList;
+                $scope.resourceList = resourceList;
+            } ;
+
+            var getTags = function() {
+                var query  = '[[:d = any(document.type, ["resource"])][:d = any(document.tags, ["youth"])]]';
+                ctx.api.form('everything').query(query)
+                    .ref(ctx.ref)
+                        .submit(function(err, documents){
+                            if (err) {
+                                $log.error('error getting sponsor from prismic');
+                            }
+                            else {
+                                documents.results.forEach(function(result){
+                                    result.tags.forEach(function(tag){
+                                        if($scope.tags.indexOf(tag) === -1 && tag != 'youth'){
+                                            $scope.tags.push(tag);
+                                        }
+                                    })
+                                })
+                                getResources();
+                            }
+                        });
+            }();
 
 
-            Prismic.bookmark('youth-overview').then(function(doc){
-                    $scope.overview = doc.getStructuredText('generic.body').asHtml(ctx);
-            });
+            var bookmarks = [['overview','youth-overview'],
+                             ['programs','youth-programs'],
+                             ['resource','youth-resource-text'],
+                             ['curriculum','youth-curriculum']];
 
-             Prismic.bookmark('youth-programs').then(function(doc){
-                    $scope.programs = doc.getStructuredText('generic.body').asHtml(ctx);
-            });
-
-             Prismic.bookmark('youth-resource-text').then(function(doc){
-                    $scope.resources = doc.getStructuredText('generic.body').asHtml(ctx);
-            });
-
-            Prismic.bookmark('youth-curriculum').then(function(doc){
-                    $scope.curriculum = doc.getStructuredText('generic.body').asHtml(ctx);
-            });
+            bookmarks.forEach(function(bookmark){
+                Prismic.bookmark(bookmark[1]).then(function(doc){
+                    $scope[bookmark[0]] = doc.getStructuredText('generic.body').asHtml(ctx);
+                });
+            })
 
         });
   })
