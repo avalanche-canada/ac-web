d6ac7c637d04e26b256918146cb1cf6c111c0ac0
BenShaw <bshaw@avalanche.ca>
2015-02-06 16:34:29 -0800

    fixed scrolling in about, collaborators and sponsors. updated weather forecast page. loaded data before rendering and made all images responsive

--- client/app/about/about.js
+++ client/app/about/about.js
@@ -1,18 +1,91 @@
 'use strict';
 
+
+
+
+
 angular.module('avalancheCanadaApp')
   .config(function ($stateProvider) {
     $stateProvider
       .state('ac.about', {
         url: '^/about',
         templateUrl: 'app/about/about.html',
-        controller: 'AboutCtrl'
+        resolve:{
+            overview: function($q, Prismic){
+                var deferred = $q.defer();
+                Prismic.ctx().then(function(ctx){
+                    Prismic.bookmark('about-overview').then(function(doc){
+                        deferred.resolve(doc.getStructuredText('generic.body').asHtml(ctx));
+                    });
+                });
+                return deferred.promise;
+            },
+
+            board: function($q, Prismic){
+                var deferred = $q.defer();
+                Prismic.ctx().then(function(ctx){
+                    Prismic.bookmark('about-board').then(function(doc){
+                        deferred.resolve(doc.getStructuredText('generic.body').asHtml(ctx));
+                    });
+                });
+                return deferred.promise;
+            },
+
+            awards: function($q, Prismic){
+                var deferred = $q.defer();
+                Prismic.ctx().then(function(ctx){
+                    Prismic.bookmark('about-awards').then(function(doc){
+                        deferred.resolve(doc.getStructuredText('generic.body').asHtml(ctx));
+                    });
+                });
+                return deferred.promise;
+            },
+
+            vision: function($q, Prismic){
+                var deferred = $q.defer();
+                Prismic.ctx().then(function(ctx){
+                    Prismic.bookmark('about-vision').then(function(doc){
+                        deferred.resolve(doc.getStructuredText('generic.body').asHtml(ctx));
+                    });
+                });
+                return deferred.promise;
+            },
+
+            contact: function($q, Prismic){
+                var deferred = $q.defer();
+                Prismic.ctx().then(function(ctx){
+                    Prismic.bookmark('about-contact').then(function(doc){
+                        deferred.resolve(doc.getStructuredText('generic.body').asHtml(ctx));
+                    });
+                });
+                return deferred.promise;
+            },
+
+            staff: function($q, Prismic){
+                var deferred = $q.defer();
+
+                Prismic.ctx().then(function(ctx){
+                    ctx.api.form('everything')
+                        .query('[[:d = at(document.type, "staff")]]')
+                            .ref(ctx.ref).submit(function(err, doc){
+                                if (err) {
+                                    $log.error('error getting events from prismic');
+                                }
+                                else {
+                                    deferred.resolve(doc.results);
+                                }
+                        });
+                });
+                return deferred.promise;
+            }
+        },
+        controller: 'AboutCtrl',
       })
       .state('ac.terms', {
         url: '^/tou',
         templateUrl: 'app/about/tou.html',
         controller: ['Prismic', '$scope',
-            function (Prismic, $scope) {
+            function (Prismic, $scope, $timeout, $anchorScroll) {
 
                 Prismic.ctx().then(function(ctx){
 
@@ -29,4 +102,17 @@ angular.module('avalancheCanadaApp')
                 });
             }]
       });
-  });
+  })
+.controller('AboutCtrl', function ($scope, $anchorScroll, $timeout, Prismic, overview, board, awards, vision, contact, staff) {
+    $scope.overview = overview;
+    $scope.board    = board;
+    $scope.awards   = awards;
+    $scope.vision   = vision;
+    $scope.contact  = contact;
+    $scope.staff    = staff;
+
+    //! once rendered call anchor scroll
+    $timeout($anchorScroll, 0, false);
+
+});
+
