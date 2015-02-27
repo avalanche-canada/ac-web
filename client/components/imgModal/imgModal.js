6edf3004d67f1d153ebcd0f13ff0d783141f35bd
BenShaw <bshaw@avalanche.ca>
2015-02-27 11:58:50 -0800

    added image modal to weather page

---
+++ client/components/imgModal/imgModal.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('avalancheCanadaApp')
+
+  .directive('imgModal', function($modal) {
+      return {
+          restrict: 'E',
+          scope:{
+              thumb: '=thumb',
+              highRes: '=high'
+          },
+          template: '<div class="imgModal"><a ng-click="openModal()"><img class="img-responsive" src="{{thumb}}"/></a></div>',
+          link: function(scope, ele, attrs) {
+
+            scope.openModal = function (size) {
+              var modalInstance = $modal.open({
+                  template: '<img src="{{highRes}}" ng-click="close()">',
+                  controller: ['$scope', 'highRes',
+                    function ($scope,highRes) {
+                      $scope.highRes = highRes;
+
+                      $scope.close = function () {
+                        $modalInstance.dismiss('cancel');
+                      };
+                    }],
+                  size: 'lg',
+                  resolve: {
+                    highRes: function () {
+                      return scope.highRes;
+                    }
+                  }
+                });
+            };
+          }
+      };
+  });
+
