2dd2992b429443d604dafc96a6ddb6d97bfac17f
BenShaw <bshaw@avalanche.ca>
2015-03-22 17:45:40 -0700

    fixed jshint wrong quotes

--- client/app/app.js
+++ client/app/app.js
@@ -39,9 +39,9 @@ angular.module('avalancheCanadaApp', [
                 url: '/',
                 templateUrl: 'app/template.html',
                 controller: ['$scope','store', function($scope,store){
-                    $scope.ogTitle = "Avalanche Canada";
-                    $scope.ogImage = "http://www.avalanche.ca/assets/avalanche_canada.png";
-                    $scope.ogDescription = "Avalanche Canada is a non-government, not-for-profit organization dedicated to public avalanche safety. We issue daily avalanche forecasts throughout the winter for much of the mountainous regions of western Canada, providing this free information via our website and our app, Avalanche Canada Mobile. We also coordinate and deliver avalanche awareness and education programs, provide curriculum and support to instructors of Avalanche Canada training programs, act as a central point-of-contact for avalanche information, and work closely with many different avalanche research projects, both at home and abroad.";
+                    $scope.ogTitle = 'Avalanche Canada';
+                    $scope.ogImage = 'http://www.avalanche.ca/assets/avalanche_canada.png';
+                    $scope.ogDescription = 'Avalanche Canada is a non-government, not-for-profit organization dedicated to public avalanche safety. We issue daily avalanche forecasts throughout the winter for much of the mountainous regions of western Canada, providing this free information via our website and our app, Avalanche Canada Mobile. We also coordinate and deliver avalanche awareness and education programs, provide curriculum and support to instructors of Avalanche Canada training programs, act as a central point-of-contact for avalanche information, and work closely with many different avalanche research projects, both at home and abroad.';
                 }]
             })
             .state('ac.404', {
