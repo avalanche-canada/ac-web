1df389db2ea6e00b2e2f8892e92db4a98c3a0549
BenShaw <bshaw@avalanche.ca>
2015-04-08 14:54:10 -0700

    course add working and code cleanup

--- server/api/ast/ast.js
+++ server/api/ast/ast.js
@@ -9,9 +9,6 @@ var docClient = new DOC.DynamoDB();
 var AST_TABLE = process.env.AST_DYNAMODB_TABLE;
 
 exports.getProviderList = function getProviderList(filters, success, fail) {
-
-    console.log('getting provders from' + AST_TABLE);
-
     var params = { TableName: AST_TABLE };
 
     docClient.scan(params, function(err, res) {
@@ -28,8 +25,6 @@ exports.getProvider = function getProvider(provId, success, fail) {
     var params = { TableName: AST_TABLE };
     params.KeyConditions = [docClient.Condition('providerid', 'EQ', provId)];
 
-    console.log('getting provider from' + AST_TABLE + 'with id' + provId);
-
     docClient.query(params, function(err, res) {
         if (err) {
             fail(err);
@@ -45,20 +40,17 @@ exports.addProvider = function addProvider(provider, success, fail) {
     params.Item =  provider;
     params.Item.providerid = uuid.v4();
 
-    console.log('Adding provider to ' + AST_TABLE + ' provider = ', JSON.stringify(params.Item));
-
     docClient.
         putItem(params, function(err, res) {
             if (err) {
                 fail(err);
             } else {
-                //var providerList = res.Items;
-                success(res);
+                success(res.items);
             }
         });
 };
 
-exports.updateProvider = function updateProvider(provId, provider, success, fail) {
+exports.updateProviderDetails = function updateProviderDetails(provId, provider, success, fail) {
     var params  = {}
     params.TableName = AST_TABLE;
     params.Key = {providerid:provId};
@@ -69,15 +61,43 @@ exports.updateProvider = function updateProvider(provId, provider, success, fail
     params.ExpressionAttributeNames  = {'#a' : 'provider'};
     params.ExpressionAttributeValues = {':x' : provider};
 
-    console.log('Updating provider = ', JSON.stringify(params.ExpressionAttributeValues));
+    docClient.
+        updateItem(params, function(err, res) {
+            if (err) {
+                fail(err);
+            } else {
+                success(res.items);
+            }
+        });
+};
+
+exports.addCourse = function addCourse(provId, course, success, fail) {
+    var params  = {}
+    params.TableName = AST_TABLE;
+    params.Key = {providerid:provId};
+    params.UpdateExpression = 'SET #courses = list_append(#courses, :x)';
+    params.ExpressionAttributeNames  = {'#courses' : 'courses'};
+    params.ExpressionAttributeValues = {':x' : course};
+
+    console.log('Adding Course = ', JSON.stringify(params.ExpressionAttributeValues));
 
     docClient.
         updateItem(params, function(err, res) {
             if (err) {
                 fail(err);
             } else {
-                //var providerList = res.Items;
-                success(res);
+                console.log('Sucesfully Added c', JSON.stringify(params.ExpressionAttributeValues));
+                success(res.items);
             }
         });
-};
+}
+
+exports.updateCourse = function updateCourse(provId, courseId, course, success, fail) {
+
+}
+
+exports.addInstructor = function addInstructor(provId, inctructor, success, fail) {}
+
+exports.updateInstructor = function updateInstructor(provId, inctructorId, inctructor, success, fail) {}
+
+
--- server/api/ast/index.js
+++ server/api/ast/index.js
@@ -8,65 +8,82 @@ var debug = require('debug')('dev');
 
 var jsonParser = bodyParser.json();
 
+var errorCallback = function(res, errStr){
+    return function(err){
+        console.log (errStr, err);
+        res.send(500, {error: errStr})
+    };
+};
+
+var successCallback = function(res){
+    return function(ret){
+        res.json(ret);
+    }
+};
+
+//! Get Provider List
 router.get('/providers', function (req, res) {
-    var filters = req.query;
-    debug('fetching providers with fiters:' +  JSON.stringify(filters));
-
-    var success = function success(providerList){
-        res.json(providerList);
-    };
-
-    var fail = function fail(err){
-        console.log ('Error retrieving providers', err);
-        res.send(500, {error: 'error retrieving providers '})
-    };
-
-    ast.getProviderList(filters, success, fail);
+    var filters = req.query; //! \todo
+    ast.getProviderList(filters,
+                        successCallback(res),
+                        errorCallback(res, 'Error retrieving providers'));
 });
 
-
+//! get provider with id
 router.get('/providers/:provid', function (req, res) {
     var provid = req.params.provid;
-    debug('fetching provider with id:' + provid);
-
-    var success = function success(providerList){
-        res.json(providerList);
-    };
-
-    var fail = function fail(err){
-        console.log('Error retrieving provider with id' + provid ,  err);
-        res.send(500, {error: 'error retrieving provider'})
-    };
-
-    ast.getProvider(provid, success, fail);
+    ast.getProvider(provid,
+                    successCallback(res),
+                    errorCallback(res, 'Error fetching provider with id '+ provid));
 });
 
+//! Add provider (details + courses + instructors)
 router.post('/providers', jsonParser, function (req, res) {
-
-    var success = function success(details){
-        res.json(details);
-    };
-
-    var fail = function fail(err){
-        console.log('Error adding provider', req , err);
-        res.send(500, {error: 'error adding provider'})
-    };
-
-    ast.addProvider(req.body, success, fail);
+    ast.addProvider(req.body,
+                    successCallback(res),
+                    errorCallback(res, 'Error adding provider'));
 });
 
-router.put('/providers/:provid', jsonParser, function (req, res) {
-
-    var success = function success(details){
-        res.json(details);
-    };
-
-    var fail = function fail(err){
-        console.log('Error adding provider', req , err);
-        res.send(500, {error: 'error adding provider'})
-    };
-
-    ast.updateProvider(req.params.provid, req.body, success, fail);
+//! Update provider[id] details
+router.put('/providers/:provid/details', jsonParser, function (req, res) {
+    ast.updateProviderDetails(req.params.provid,
+                              req.body,
+                              successCallback(res),
+                              errorCallback(res, 'Error updating provider'));
 });
+
+//! Add provider[id] Course
+router.post('/providers/:provid/course', jsonParser, function (req, res) {
+   ast.addCourse(req.params.provid,
+                 req.body,
+                 successCallback(res),
+                 errorCallback(res, 'Error adding course'));
+});
+
+//! Update provider[id] course[id]
+router.put('/providers/:provid/course/:courseid', jsonParser, function (req, res) {
+    ast.updateCourse(req.params.provid,
+                     req.params.courseid,
+                     req.body,
+                     successCallback(res),
+                     errorCallback(res, 'Error updating course'));
+});
+
+//! Add provider[id] Instructor
+router.post('/providers/:provid/instructor', jsonParser, function (req, res) {
+   ast.addInstructor(req.params.provid,
+                     req.body,
+                     successCallback(res),
+                     errorCallback(res, 'Error adding instructor'));
+});
+
+//! Update provider[id] Instructor[id]
+router.put('/providers/:provid/course/:courseid', jsonParser, function (req, res) {
+   ast.updateInstructor(req.params.provid,
+                        req.body,
+                        successCallback(res),
+                        errorCallback(res, 'Error updating instructor'));
+});
+
 
 module.exports = router;
