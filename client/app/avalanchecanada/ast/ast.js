d85b22e1f06a2989cff9cc59cadb3e9b62a978eb
Nathan Bertram <nbertram@gmail.com>
2015-06-16 10:16:46 -0700

    Link up toggle

--- client/app/avalanchecanada/ast/ast.js
+++ client/app/avalanchecanada/ast/ast.js
@@ -18,17 +18,26 @@ angular.module('avalancheCanadaApp')
 .controller('AstProvidersCtrl', function ($scope, $http, $log) {
   $scope.providers = []
   $scope.providers_page = true;
+
   $http.get('/api/ast/providers').then(function (res) {
     $scope.providers = res.data;
   });
 
-  $log.debug("AstProvidersCtrl Initialized")
+  $scope.toggleMoreInfo = function(provider){
+    provider.more_info = !provider.more_info
+  }
+
 })
 .controller('AstCoursesCtrl', function ($scope, $http, $log) {
   $scope.courses = [];
   $scope.courses_page = true;
+
   $http.get('/api/ast/courses').then(function (res) {
     $scope.courses = res.data;
   });
-  $log.debug("AstCoursesCtrl Initialized")
+
+  $scope.toggleMoreInfo = function(course){
+    course.more_info = !course.more_info
+  }
+
 });
