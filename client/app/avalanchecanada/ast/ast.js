f33358e08f091c13aa33813a1728e000df3b786c
Nathan Bertram <nbertram@gmail.com>
2015-06-18 07:44:40 -0700

    Get related provider / courses info; Enabled dropdown filters

--- client/app/avalanchecanada/ast/ast.js
+++ client/app/avalanchecanada/ast/ast.js
@@ -16,28 +16,72 @@ angular.module('avalancheCanadaApp')
 
 })
 .controller('AstProvidersCtrl', function ($scope, $http, $log) {
-  $scope.providers = []
+  $scope.providers = [];
+  $scope.courses = [];
+  $scope.levels = [];
+  $scope.current_level = null;
   $scope.providers_page = true;
 
   $http.get('/api/ast/providers').then(function (res) {
     $scope.providers = res.data;
+
+    $http.get('/api/ast/courses').then(function (res) {
+      $scope.levels = _.unique(_.pluck(res.data, 'level'));
+
+      res.data.forEach(function(course){
+        var provider = _.find($scope.providers, {providerid: course.providerid});
+
+        if(provider){
+          if(typeof provider.courses == "undefined"){
+            provider.courses = [];
+          }
+          provider.courses.push(course);
+        }
+
+      });
+
+    });
+
   });
 
   $scope.toggleMoreInfo = function(provider){
-    provider.more_info = !provider.more_info
+    provider.more_info = !provider.more_info;
+  }
+
+  $scope.selectLevel = function(level){
+    $scope.current_level = level;
+    return false;
   }
 
 })
 .controller('AstCoursesCtrl', function ($scope, $http, $log) {
   $scope.courses = [];
+  $scope.providers = [];
+  $scope.levels = [];
+  $scope.current_level = null;
   $scope.courses_page = true;
 
   $http.get('/api/ast/courses').then(function (res) {
     $scope.courses = res.data;
+    $scope.levels = _.unique(_.pluck(res.data, 'level'));
+
+    $http.get('/api/ast/providers').then(function (res) {
+      var providers = res.data;
+      $scope.courses.forEach(function(course){
+        course.provider = _.find(providers, {providerid: course.providerid}); 
+      });
+
+      window.courses = $scope.courses;
+    });
+
   });
 
   $scope.toggleMoreInfo = function(course){
-    course.more_info = !course.more_info
+    course.more_info = !course.more_info;
   }
 
+  $scope.selectLevel = function(level){
+    $scope.current_level = level;
+    return false;
+  }
 });
