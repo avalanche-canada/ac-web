d4845b5d8c39531c1687daca72df0223d75bf06e
Will Harding <wharding@acromediainc.com>
2015-09-28 15:28:03 -0700

    Removed livereload

--- server/config/express.js
+++ server/config/express.js
@@ -36,7 +36,6 @@ module.exports = function(app) {
   }
 
   if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
