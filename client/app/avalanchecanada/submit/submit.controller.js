580b3e2ca6f79c173d18e7883e05be28a0a68415
Will Harding <wharding@avalanche.ca>
2015-10-28 19:45:11 -0700

    Add MIN landing page
     - update header with MIN menu item
     - Add basic MIN landing page

--- client/app/avalanchecanada/submit/submit.controller.js
+++ client/app/avalanchecanada/submit/submit.controller.js
@@ -1,6 +1,33 @@
 'use strict';
 
+function prsmBookmark(id) {
+    console.log('its working!!!!');
+    return function($q, Prismic){
+        var deferred = $q.defer();
+        Prismic.ctx().then(function(ctx){
+            Prismic.bookmark(id).then(function(doc){
+                deferred.resolve(doc);
+            });
+        });
+        return deferred.promise;
+    };
+}
+
 angular.module('avalancheCanadaApp')
-    .controller('SubmitCtrl', function () {
-        
-    });
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('ac.minOverview', {
+        url: '^/mountain-information-network',
+        templateUrl: 'app/avalanchecanada/submit/min-overview.html',
+        resolve: {page: prsmBookmark('min-landing')},
+        controllerAs: 'page',
+        controller: function($q, $location, $window, page){
+            this.url = $window.encodeURIComponent($location.absUrl());
+            this.title = page.getText('generic.title');
+            this.body  = page.getStructuredText('generic.body').asHtml(null);
+        }
+      });
+  })
+.controller('SubmitCtrl', function () {
+});
+
