bcd56d2f6142452ec0eefbe209ecd83a56b58347
Will Harding <wharding@avalanche.ca>
2016-01-20 13:29:34 -0800

    Remove cdnify
     - Issues with the 'optimist' library is breaking the build and is
       required by cdnify.
     - Removed because no dependencies were being rewritten anyway

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,6 @@ module.exports = function (grunt) {
     express: 'grunt-express-server',
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
-    cdnify: 'grunt-google-cdn',
     protractor: 'grunt-protractor-runner',
     injector: 'grunt-asset-injector'
   });
@@ -398,12 +397,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
 
     // Copies remaining files to places other tasks can use
     copy: {
@@ -747,7 +740,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'rev',
