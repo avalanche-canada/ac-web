398db19cddcea1a21a9ba4cc9af82d1dca27d1d8
Will Harding <wharding@avalanche.ca>
2016-01-28 12:36:43 -0800

    Removed unused auth callback functions

--- client/app/avalanchecanada/auth/auth.service.js
+++ client/app/avalanchecanada/auth/auth.service.js
@@ -2,37 +2,19 @@
 
 angular.module('avalancheCanadaApp')
 .service('AcAuth', function(ENV, $location, store, auth, ngToast){
-    function loginCallback(profile, idToken, accessToken, state, refreshToken) {
-        store.set('profile', profile);
-        store.set('token', idToken);
-    };
-
-    function errorCallback(error) {
-      console.log('Error authenticating user:',error);
-      ngToast.create({
-        content: 'Error logging in: ' + JSON.stringify(error),
-        class: 'warn'
-      });
-    };
 
     function signin() {
       console.log('setting loginRedirectUrl=', $location.url());
       store.set('loginRedirectUrl', $location.url());
-      auth.signin(
-          {
-            authParams: {scope: 'openid profile'},
-            callbackURL: ENV.DOMAIN + '/login-complete',
-            responseType: 'token'
-          }//, 
-          //loginCallback, 
-          //errorCallback
-      );
+      auth.signin({
+        authParams: {scope: 'openid profile'},
+        callbackURL: ENV.DOMAIN + '/login-complete',
+        responseType: 'token'
+      });
     };
     
     function signout() {
-      auth.signout(function(){
-        console.log('yayayayay');
-      });
+      auth.signout();
     } 
 
     function profile() {
