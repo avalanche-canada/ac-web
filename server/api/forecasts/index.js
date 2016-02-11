36880164aaed34c982724112e89f930dbee05449
Will Harding <wharding@avalanche.ca>
2016-02-10 18:35:23 -0800

    Remove API cache for cleanup

--- server/api/forecasts/index.js
+++ server/api/forecasts/index.js
@@ -18,55 +18,30 @@ var acAvalxUrls = _.chain(regions.features).filter(function (feature) {
 }).value();
 
 
-var apiWebcache = null,
-    avalxWebcache = null;
+var avalxWebcache = null;
 
 if(process.env.REDIS_HOST) {
     var webcacheOptions ={ store: new WebCacheRedis(6379, process.env.REDIS_HOST) };
     if(!process.env.NO_CACHE_REFRESH) webcacheOptions.refreshInterval = 300000;
-    apiWebcache = new WebCache(webcacheOptions);
     avalxWebcache = new WebCache(webcacheOptions);
 }
 else {
-    apiWebcache = new WebCache();
     avalxWebcache = new WebCache();
 }
 
 
 avalxWebcache.seed(acAvalxUrls);
-avalxWebcache.on('refreshed', _.bind(apiWebcache.refresh, apiWebcache));
 
 // webcache middleware
 router.use(function (req, res, next) {
     var url = req.protocol + '://' + req.headers.host + req.originalUrl;
 
     var webcacher = function (data) {
-        if(typeof data === 'object'){
-            data = JSON.stringify(data);
-        }
-        apiWebcache.cacheUrl(url, data);
+        console.log('NOT CACHING THIS THING');
     };
 
-    if(req.headers['cache-control'] === 'no-cache') {
-        //console.log('cache bypass for %s', url);
-        logger.log('info','cache bypass for', url);
-        req.webcache = webcacher;
-        next();
-    } else {
-        apiWebcache.get(url).then(function (data) {
-            if(data){
-                //console.log('cache hit for %s', url);
-                //logger.log('info','cache hit for', url);
-                apiWebcache.cacheUrl(url);
-                req.webcached = data;
-            } else {
-                //console.log('cache miss for %s', url);
-                logger.log('info','cache miss for', url);
-                req.webcache = webcacher;
-            }
-            next();
-        });
-    }
+    req.webcache = webcacher;
+    next();
 });
 
 router.param('region', function (req, res, next) {
