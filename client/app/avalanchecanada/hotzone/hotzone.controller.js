1e7d8809c1893110e295b040213499e936e468e5
Mandy Ferrari <mandy.ferrari@tesera.com>
2016-03-14 15:06:43 -0600

    Get hot zone location ids from json for hot zone report form

--- client/app/avalanchecanada/hotzone/hotzone.controller.js
+++ client/app/avalanchecanada/hotzone/hotzone.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('avalancheCanadaApp')
-  .controller('HotZoneCtrl', function () {
-
+  .controller('HotZoneCtrl', function ($scope, acForecast) {
+        acForecast.fetch().then(function (forecastRegions) {
+            $scope.hotzones = _.filter(forecastRegions.features, function (feature) {
+                return feature.properties.type === 'hotzone';
+            });
+        });
   });
