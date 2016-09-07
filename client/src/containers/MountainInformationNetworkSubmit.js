793d010e8c16fb426a27eadedab06063d766dab9
Karl Guillotte <karl.guillotte.1@gmail.com>
2016-09-06 20:10:22 -0700

    Form before schema changes.

--- client/src/containers/MountainInformationNetworkSubmit.js
+++ client/src/containers/MountainInformationNetworkSubmit.js
@@ -1,52 +1,54 @@
 import React, {Component} from 'react'
-import {compose, withHandlers, toClass} from 'recompose'
+import {compose, withState, withHandlers, withProps, lifecycle} from 'recompose'
 import {Fieldset, Legend, Submit, Control, ControlSet} from 'components/form'
 import {Tab, TabSet} from 'components/tab'
 import {Input} from 'components/controls'
+import {Loading} from 'components/misc'
 import {Page, Header, Main, Section} from 'components/page'
 import * as Colors from 'components/icons/min/colors'
+import {fetchMountainInformationNetwork} from 'api/schema'
 import Form from 'react-jsonschema-form'
 
-export default function MountainInformationNetworkForm() {
-    return (
-        <Page>
-            <Header title='A nice Mountain Information Network Form goes here!!!!' />
-        </Page>
-    )
+import schema from './MountainInformationNetworkForm.json'
+import uiSchema from './MountainInformationNetworkFormUI.json'
+
+function MountainInformationNetworkForm({isReady, ...form}) {
     return (
         <Page>
             <Header title='Mountain Information Network' />
             <Main>
-                <Form onSubmit={::console.warn}>
-                    <Fieldset>
-                        <Legend>Step 1. Required Info</Legend>
-                    </Fieldset>
-                    <Fieldset>
-                        <Legend>Step 2. Uploads</Legend>
-                    </Fieldset>
-                    <Fieldset>
-                        <Legend>Step 3. Observations</Legend>
-                        <TabSet arrow>
-                            <Tab title='Quick' color={Colors.QUICK}>
-                                Content for QUICK
-                            </Tab>
-                            <Tab title='Avalanche' color={Colors.AVALANCHE}>
-                                Content for AVALANCHE
-                            </Tab>
-                            <Tab title='Snowpack' color={Colors.SNOWPACK}>
-                                Content for SNOWPACK
-                            </Tab>
-                            <Tab title='Weather' color={Colors.WEATHER}>
-                                Content for WEATHER
-                            </Tab>
-                            <Tab title='Incident' color={Colors.INCIDENT}>
-                                Content for INCIDENT
-                            </Tab>
-                        </TabSet>
-                    </Fieldset>
-                    <Submit disabled={pristine || submitting}>Submit</Submit>
-                </Form>
+                {isReady ? <Form {...form} /> : <Loading />}
             </Main>
         </Page>
     )
 }
+
+export default compose(
+    withState('schema', 'setSchema', null),
+    withState('uiSchema', 'setUISchema', null),
+    withHandlers({
+        onSchemaFetchSucceed: props => response => props.setSchema(response.data),
+        onUISchemaFetchSucceed: props => response => props.setUISchema(response.data),
+        onSubmit: props => data => console.warn(data.formData),
+        onChange: props => data => console.warn(data.formData),
+        onError: props => data => console.warn(data.formData),
+    }),
+    lifecycle({
+        componentDidMount() {
+            const {onSchemaFetchSucceed, onUISchemaFetchSucceed} = this.props
+
+            onUISchemaFetchSucceed({
+                data: uiSchema
+            })
+            onSchemaFetchSucceed({
+                data: schema
+            })
+
+            // fetchMountainInformationNetwork().then(onSchemaFetchSucceed)
+            // fetchMountainInformationNetwork().then(onUISchemaFetchSucceed)
+        },
+    }),
+    withProps(({schema, uiSchema}) => ({
+        isReady: schema && uiSchema
+    })),
+)(MountainInformationNetworkForm)
