edce0e01748281f7bf2e5db7454a26940ea46fb2
Karl Guillotte <karl.guillotte.1@gmail.com>
2016-09-17 01:04:57 -0700

    Raven fixes.

--- client/src/services/raven/index.js
+++ client/src/services/raven/index.js
@@ -4,24 +4,19 @@ import {key, project} from './config.json'
 Raven.config(`https://${key}@sentry.io/${project}`).install()
 
 export function captureException(exception, context) {
-    Raven.captureException(exception, {
-        extra: context
-    })
+    Raven.captureException(exception, context)
 
     console.error(exception)
 }
 
 export function captureMessage(message, context) {
-    Raven.captureMessage(message, {
-        extra: context
-    })
+    Raven.captureMessage(message, context)
 }
 
-export function setUserContext({id, email, username, ip_address}) {
+export function setUserContext({user_id, email, name}) {
     Raven.setUserContext({
-        id,
+        id: user_id,
         email,
-        username,
-        ip_address
+        username: name,
     })
 }
