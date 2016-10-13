ddb17450feb21460b9a1f7fb6a9260283cb82170
Will Harding <wharding@acromediainc.com>
2016-10-13 15:53:42 -0700

    Split 2016 regions into separate gis/prop files

--- server/data/season/2016/forecast-regions.js
+++ server/data/season/2016/forecast-regions.js
@@ -1,23 +1,33 @@
+ 
+var fs   = require('fs');
+var path = require('path');
 
-var regions  = require('./forecast-regions.json');
-var hotzones = require('./hotzones.json');
-var avalxMapping = require('./avalxMapping.json')
+var regionsMeta  = require('./forecast-metadata.json');
+var hotzones     = require('./hotzones.json');
+var avalxMapping = require('./avalxMapping.json');
 
 console.log('*** USING 2016 SEASON ***')
 
 //TODO(wnh): Remove old URLS from the forecast json
 
-regions.features.forEach((reg, idx) => {
-    if(reg.properties.type !== 'avalx') {
-        return;
+var features = Object.keys(regionsMeta).map(id => {
+    console.log(id);
+    var props = regionsMeta[id];
+
+    var url = null;
+    if(props.type === 'avalx') {
+        url = 'http://avalxdev.avalanche.ca/public/CAAML-eng.aspx?r=' + String(avalxMapping[id]);
     }
-    var url = 'http://avalxdev.avalanche.ca/public/CAAML-eng.aspx?r=' + String(avalxMapping[reg.id]);
-    regions.features[idx].properties.url = url;
+    props.url = url || props.url;
+
+    var feature = JSON.parse(fs.readFileSync(path.join(__dirname, 'regionGeo', id + '.geojson')));
+
+    return Object.assign({}, feature, {properties: props});
 })
 
 var all =  {
   "type": "FeatureCollection",
-  "features": regions.features.concat(hotzones.features)
+  "features": features.concat(hotzones.features)
 };
 
 module.exports = all;
