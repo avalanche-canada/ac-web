a998601c599f72916653c7aa0e10b67a58b5edfb
Will Harding <wharding@acromediainc.com>
2016-10-17 16:30:32 -0700

    Get AvalX server from Env variable

--- server/data/season/2016/forecast-regions.js
+++ server/data/season/2016/forecast-regions.js
@@ -8,6 +8,8 @@ var avalxMapping = require('./avalxMapping.json');
 
 console.log('*** USING 2016 SEASON ***')
 
+var AVALX_HOST = process.env.AVALX_HOST || 'http://avalx2016.avalanche.ca';
+
 //TODO(wnh): Remove old URLS from the forecast json
 
 var features = Object.keys(regionsMeta).map(id => {
@@ -15,7 +17,7 @@ var features = Object.keys(regionsMeta).map(id => {
 
     var url = null;
     if(props.type === 'avalx') {
-        url = 'http://avalxdev.avalanche.ca/public/CAAML-eng.aspx?r=' + String(avalxMapping[id]);
+        url = AVALX_HOST+'/public/CAAML-eng.aspx?r='+String(avalxMapping[id]);
     }
     props.url = url || props.url;
 
