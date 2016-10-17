a81e7e0d354c6258cd739e9daf4e31c01f27c59b
Will Harding <wharding@acromediainc.com>
2016-10-17 09:48:19 -0700

    Add Ids back to regions and hotzones
     - Frontend indexs on hotzone/region Ids

--- server/data/season/2016/forecast-regions.js
+++ server/data/season/2016/forecast-regions.js
@@ -11,7 +11,6 @@ console.log('*** USING 2016 SEASON ***')
 //TODO(wnh): Remove old URLS from the forecast json
 
 var features = Object.keys(regionsMeta).map(id => {
-    console.log(id);
     var props = regionsMeta[id];
 
     var url = null;
@@ -22,12 +21,14 @@ var features = Object.keys(regionsMeta).map(id => {
 
     var feature = JSON.parse(fs.readFileSync(path.join(__dirname, 'regionGeo', id + '.geojson')));
 
-    return Object.assign({}, feature, {properties: props});
+    return Object.assign({}, feature, {properties: props, id: id});
 })
+
+var hzFeaturesWithId = hotzones.features.map(h => Object.assign({}, h, {id: h.properties.id}));
 
 var all =  {
   "type": "FeatureCollection",
-  "features": features.concat(hotzones.features)
+  "features": features.concat(hzFeaturesWithId)
 };
 
 module.exports = all;
