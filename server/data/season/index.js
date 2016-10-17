8f3ecd32047c794bcf7462c22609feb4f33e2607
Will Harding <wharding@acromediainc.com>
2016-10-17 10:12:20 -0700

    Lock season to only 2015/2016

--- server/data/season/index.js
+++ server/data/season/index.js
@@ -3,8 +3,8 @@ var path = require('path');
 
 var season  = process.env.AC_SEASON || '2015';
 
-if(!season.match(/^\d\d\d\d$/)) {
-    throw new Exception('Season MUST be a valid year')
+if(!season.match(/^(2015|2016)$/)) {
+    throw new Exception('Season MUST be 2016 or 2015')
 }
 
 module.exports = {
