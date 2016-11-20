9156ae2717773928612a3c79bbb7bc85072e8ae9
Karl Guillotte <karl.guillotte.1@gmail.com>
2016-11-20 15:07:21 -0800

    Code refactoring

--- client/src/reducers/geolocation.js
+++ client/src/reducers/geolocation.js
@@ -3,7 +3,3 @@ import {GEOLOCATION_POSITION_CHANGED} from 'actions/geolocation'
 import {getPayload} from 'reducers/utils'
 
 export default handleAction(GEOLOCATION_POSITION_CHANGED, getPayload, null)
-
-export function getGeolocation(state) {
-    return state.geolocation
-}
