c7ac2125ecd3bf7694eace9a01c97c56baf74020
Karl Guillotte <karl.guillotte.1@gmail.com>
2016-11-30 18:53:50 -0800

    [MIN] Add context map to full page view
    - Just need to change the condition to render the map

--- client/src/containers/MountainInformationNetworkSubmission.js
+++ client/src/containers/MountainInformationNetworkSubmission.js
@@ -1,9 +1,22 @@
-import React, {PropTypes, createElement} from 'react'
+import React, {PropTypes} from 'react'
 import {Link} from 'react-router'
 import {Page, Header, Main, Content, Headline} from 'components/page'
 import {Muted, Error} from 'components/misc'
 import {mountainInformationNetworkSubmission} from 'containers/connectors'
 import {Submission, Metadata} from 'components/mountainInformationNetwork'
+import {Map, Marker, NavigationControl} from 'components/map'
+import mapbox from 'services/mapbox/map'
+import min from 'components/icons/min/min-pin.svg'
+
+const MAP_STYLE = {
+    height: 250
+}
+function createElement(props) {
+    return Object.assign(document.createElement('img'), {
+        ...props,
+        src: min,
+    })
+}
 
 Container.propTypes = {
     title: PropTypes.string.isRequired,
@@ -20,6 +33,11 @@ function Container({
     props,
     messages,
 }) {
+    const center = metadata ? new mapbox.LngLat(
+        metadata.longitude,
+        metadata.latitude
+    ) : null
+
     return (
         <Page>
             <Header title={title} />
@@ -28,6 +46,12 @@ function Container({
                     {metadata && <Metadata {...metadata} />}
                     {isLoading && <Muted>Loading submission...</Muted>}
                     {isError && <Error>Error happened while loading submission.</Error>}
+                    {false &&
+                        <Map style='2016' center={center} zoom={8} containerStyle={MAP_STYLE}>
+                            <Marker element={createElement({title})} lngLat={center} />
+                            <NavigationControl />
+                        </Map>
+                    }
                     {props && <Submission {...props} />}
                 </Main>
             </Content>
