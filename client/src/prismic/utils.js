aa5ccfa651e81006dd3d89fdc2fe9b426f25df0b
Karl Guillotte <karl.guillotte.1@gmail.com>
2016-12-23 00:21:12 -0500

    [HZR] Fix date range test for valid report

--- client/src/prismic/utils.js
+++ client/src/prismic/utils.js
@@ -1,5 +1,11 @@
 import isWithinRange from 'date-fns/is_within_range'
+import startOfDay from 'date-fns/start_of_day'
+import endOfDay from 'date-fns/end_of_day'
 
 export function isHotZoneReportValid({dateOfIssue, validUntil}) {
-    return isWithinRange(new Date(), dateOfIssue, validUntil)
+    return isWithinRange(
+        new Date(),
+        startOfDay(dateOfIssue),
+        endOfDay(validUntil)
+    )
 }
