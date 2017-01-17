490f0360ccb45fc1d98f31fd43bb16c065f6bf8a
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-01-16 22:42:45 -0800

    [MAP] Fix layers that do not appear when map data is still loading

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -171,21 +171,19 @@ export default class MapComponent extends Component {
     componentWillReceiveProps({style}) {
         const {map} = this.state
 
-        if (!map) {
+        if (!map || style === this.props.style) {
             return
         }
 
         if (this.props.style === null) {
             this.style = style
-            map.once('load', () => {
-                this.style = this.props.style
-            })
-            return
-        }
-
-        if (style !== this.props.style && map.loaded()) {
-            this.style = style
-            return
+            map.once('load', () => this.style = this.props.style)
+        } else {
+            if (map.loaded()) {
+                this.style = style
+            } else {
+                map.once('load', () => this.style = style)
+            }
         }
     }
     set style(style) {
