664ca4ffb78db26ce320c3f2323b52fc9f358380
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-02-18 20:31:12 -0800

    Import the date format function

--- client/src/api/transformers.js
+++ client/src/api/transformers.js
@@ -1,8 +1,9 @@
 import QUICK_REPORT from 'containers/min/quick.json'
 import t from 'services/tcomb-form'
+import format from 'date-fns/format'
+import identity from 'lodash/identity'
 import Submission from 'containers/min/types'
 import {QUICK, AVALANCHE, SNOWPACK, WEATHER, INCIDENT} from 'constants/min'
-import identity from 'lodash/identity'
 
 const ObservationKeys = new Map([
     [QUICK, 'quickReport'],
