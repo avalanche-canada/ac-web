917dbae2dc3cea7fe1fb1f14df3901404bde440e
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-02-24 13:44:57 -0800

    Add Alert component

---
+++ client/src/components/alert/index.js
@@ -0,0 +1,31 @@
+import React, {PropTypes} from 'react'
+import {compose, setPropTypes, mapProps} from 'recompose'
+import CSSModules from 'react-css-modules'
+import styles from './Alert.css'
+
+export const DANGER = 'DANGER'
+export const INFO = 'INFO'
+export const WARNING = 'WARNING'
+export const SUCCESS = 'SUCCESS'
+
+const styleNames = new Map([
+    [DANGER, 'Danger'],
+    [INFO, 'Info'],
+    [WARNING, 'Warning'],
+    [SUCCESS, 'Success'],
+])
+
+Alert.propTypes = {
+    type: PropTypes.oneOf([DANGER, INFO, WARNING, SUCCESS]).isRequired,
+    children: PropTypes.node,
+}
+
+function Alert({type = DANGER, children, ...props}) {
+    return (
+        <div styleName={styleNames.get(type)} {...props}>
+            {children}
+        </div>
+    )
+}
+
+export default CSSModules(Alert, styles)
