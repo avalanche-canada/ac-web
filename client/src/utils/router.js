853bef0eb38bc411702a71c6ff1ef7450ecb31ea
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-03-01 14:35:43 -0800

    Add handler functions for router

--- client/src/utils/router.js
+++ client/src/utils/router.js
@@ -1,3 +1,8 @@
+import {HeaderCellOrders} from 'components/table'
+import identity from 'lodash/identity'
+
+const {NONE, DESC} = HeaderCellOrders
+
 function merge(location, {query = {}, state = {}, ...rest}) {
     return {
         ...location,
@@ -19,4 +24,33 @@ export function push(newLocation, {router, location}) {
 
 export function replace(newLocation, {router, location}) {
     return router.replace(merge(location, newLocation))
+}
+
+
+export function valueHandlerFactory(name, format = identity) {
+    return props => value => {
+        push({
+            query: {
+                [name]: format(value)
+            }
+        }, props)
+    }
+}
+
+export function sortingHandlerFactory(propName = 'sorting') {
+    return props => (name, order = NONE) => {
+        replace({
+            query: {
+                [propName]: order === NONE ? undefined : `${order === DESC ? '-' : ''}${name}`
+            }
+        }, props)
+    }
+}
+
+function asArray(values) {
+    return typeof values === 'string' ? new Set([values]) : values
+}
+
+export function arrayValueHandlerFactory(name) {
+    return valueHandlerFactory(name, asArray)
 }
