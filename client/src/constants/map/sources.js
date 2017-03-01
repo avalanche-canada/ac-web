a10797af234c64bd3b0b93efca72daccbf1339e6
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-02-28 19:07:27 -0800

    Clean up some code

--- client/src/constants/map/sources.js
+++ client/src/constants/map/sources.js
@@ -4,13 +4,6 @@ import turf from '@turf/helpers'
 const data = turf.featureCollection([])
 const type = 'geojson'
 
-const extent = {
-    xmin: -118.46607345514553,
-    ymin: 50.970683262197014,
-    xmax: -118.08955006935989,
-    ymax: 51.13845267759706,
-}
-
 export default {
     [Layers.MOUNTAIN_INFORMATION_NETWORK_INCIDENTS]: {
         type,
