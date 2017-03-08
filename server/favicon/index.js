e55edccf28bfefd454f6109624851be5029c1e4c
Will Harding <wharding@avalanche.ca>
2017-03-08 12:02:08 -0800

    Add favicon to server

---
+++ server/favicon/index.js
@@ -0,0 +1,14 @@
+'use strict';
+var path = require('path');
+
+var express = require('express');
+
+
+var router = express.Router();
+
+router.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, 'img/favicon.ico'));
+});
+
+
+module.exports = router;
