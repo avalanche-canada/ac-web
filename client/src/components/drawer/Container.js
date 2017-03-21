77b83cfc7c6d9099bdb8578dfa250686eb095c17
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-03-21 16:02:30 -0700

    Move tree manipulation functions to utils/tree module

--- client/src/components/drawer/Container.js
+++ client/src/components/drawer/Container.js
@@ -8,59 +8,9 @@ import Cabinet from './Cabinet'
 import Item from './Item'
 import styles from './Drawer.css'
 import noop from 'lodash/noop'
+import {findNode, getPath, getParent} from 'utils/tree'
 
 const preset = presets.noWobble
-
-// Tree manipulation
-function findNode(root, id) {
-    let node = null;
-
-    (function recurse(current) {
-        for (let child of current.children || []) {
-            if (child.id === id) {
-                node = child
-            } else if (node === null) {
-                recurse(child)
-            }
-        }
-    })(root)
-
-
-    return node
-}
-function getPath(root, node) {
-    if (!node || root === node) {
-        return [root]
-    }
-
-    const path = [node]
-    let parent = getParent(root, node.id)
-
-    while (parent !== root) {
-        path.push(parent)
-
-        parent = getParent(root, parent.id)
-    }
-
-    path.push(root)
-
-    return path.reverse()
-}
-function getParent(root, id) {
-    let parent = root;
-
-    (function recurse(current) {
-        for (let child of current.children || []) {
-            if (child.id === id) {
-                parent = current
-            } else if (parent === root) {
-                recurse(child)
-            }
-        }
-    })(root)
-
-    return parent
-}
 
 // Handlers
 function handleClick(id, event) {
---
+++ client/src/utils/tree.js
@@ -0,0 +1,51 @@
+export function findNode(root, id) {
+    let node = null;
+
+    (function recurse(current) {
+        for (let child of current.children || []) {
+            if (child.id === id) {
+                node = child
+            } else if (node === null) {
+                recurse(child)
+            }
+        }
+    })(root)
+
+
+    return node
+}
+
+export function getPath(root, node) {
+    if (!node || root === node) {
+        return [root]
+    }
+
+    const path = [node]
+    let parent = getParent(root, node.id)
+
+    while (parent !== root) {
+        path.push(parent)
+
+        parent = getParent(root, parent.id)
+    }
+
+    path.push(root)
+
+    return path.reverse()
+}
+
+export function getParent(root, id) {
+    let parent = root;
+
+    (function recurse(current) {
+        for (let child of current.children || []) {
+            if (child.id === id) {
+                parent = current
+            } else if (parent === root) {
+                recurse(child)
+            }
+        }
+    })(root)
+
+    return parent
+}
