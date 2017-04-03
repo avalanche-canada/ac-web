7374a19bccdc63afe6101a52e1108d2fa861f632
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-03 12:17:01 -0700

    Remove CSS !important hack

---
+++ client/src/components/page/drawer/Close.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import {Close} from 'components/button'
+import styles from './Drawer.css'
+
+function DrawerClose({children, ...props}) {
+    return (
+        <Close styleName='Button--Close' {...props}>
+            {children}
+        </Close>
+    )
+}
+
+export default CSSModules(DrawerClose, styles)
