7ad912fd3d08b770becbdbce9ab7f0c6e793e3ef
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-05 16:54:01 -0700

    Remove PlayPause button component

--- client/src/components/button/index.js
+++ client/src/components/button/index.js
@@ -7,7 +7,6 @@ export Top from './Top'
 export Print from './Print'
 export Expand from './Expand'
 export Sorting from './Sorting'
-export PlayPause from './PlayPause'
 export Close from './Close'
 export Locate from './Locate'
 
