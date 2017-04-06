36d8f998e9be685f897584798f60efafe57cbf02
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-06 15:06:24 -0700

    Modify server to use static middleware

--- server/static/index.js
+++ server/static/index.js
@@ -1,11 +1,3 @@
-var path = require('path');
 var express = require('express');
 
-var router = express.Router();
-
-router.get('/sponsors.json', (req, res) => {
-    res.sendFile(path.join(__dirname, 'sponsors.json'));
-})
-
-module.exports = router;
-
+module.exports = express.static(__dirname);
