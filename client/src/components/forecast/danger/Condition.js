844f1edc8c8dc2c67ffb0a726def103051dafa64
Will Harding <wharding@avalanche.ca>
2017-04-07 10:13:12 -0700

    Fix missing titles for spring messages
    - Aligned some things because OCD

--- client/src/components/forecast/danger/Condition.js
+++ client/src/components/forecast/danger/Condition.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './Danger.css'
-import {SPRING, SUMMER, OFF, EARLY_SEASON, Titles} from 'constants/forecast/mode'
+import {SPRING, SUMMER, OFF, EARLY_SEASON, Texts} from 'constants/forecast/mode'
 import {Generic} from 'prismic/components'
 import {domain} from 'assets/config.json'
 
@@ -9,16 +9,16 @@ import {domain} from 'assets/config.json'
 const HANDLED = new Set([SUMMER, SPRING, OFF, EARLY_SEASON])
 
 const ICONS = new Map([
-    [SPRING, `${domain}images/spring_situation_icon.svg`],
-    [SUMMER, `${domain}images/summer_conditions_icon.svg`],
-    [OFF, `${domain}images/summer_conditions_icon.svg`],
     [EARLY_SEASON, `${domain}images/early_season_icon.svg`],
+    [SPRING,       `${domain}images/spring_situation_icon.svg`],
+    [SUMMER,       `${domain}images/summer_conditions_icon.svg`],
+    [OFF,          `${domain}images/summer_conditions_icon.svg`],
 ])
 const UIDS = new Map([
-    [SPRING, 'forecast-spring-conditions-message'],
-    [SUMMER, 'forecast-summer-conditions-message'],
-    [OFF, 'forecast-off-season-message'],
     [EARLY_SEASON, 'forecast-early-season-message'],
+    [SPRING,       'forecast-spring-conditions-message'],
+    [SUMMER,       'forecast-summer-conditions-message'],
+    [OFF,          'forecast-off-season-message'],
 ])
 
 Condition.propTypes = {
@@ -33,7 +33,7 @@ function Condition({mode}) {
     return (
         <div styleName='Condition'>
             <h2 styleName='ConditionHeader'>
-                {Titles.get(mode)}
+                {Texts.get(mode)}
             </h2>
             <img styleName='ConditionIcon' src={ICONS.get(mode)} />
             <div styleName='ConditionContent'>
