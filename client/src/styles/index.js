573fa2755ad09c5839f59a5d2a21114d1af4ebc2
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-11 12:53:11 -0700

    Fix imports

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -3,8 +3,8 @@ import 'normalize.css'
 // TODO: Move this import to appropriate module
 import 'react-image-gallery/styles/css/image-gallery.css'
 
-import 'styles/scaffolding.css'
-import 'styles/prismic.css'
+import '/styles/scaffolding.css'
+import '/styles/prismic.css'
 
 // TODO: Move this import to appropriate module
-import 'styles/map.css'
+import '/styles/map.css'
