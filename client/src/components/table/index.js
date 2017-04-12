024647ba3f074130b74d7d3e62ed88b9b68d0053
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-12 14:56:08 -0700

    Clean up imports

--- client/src/components/table/index.js
+++ client/src/components/table/index.js
@@ -2,13 +2,9 @@ export Table from './Table'
 export Row from './Row'
 export Cell from './Cell'
 export TBody, {Controlled as ControlledTBody} from './TBody'
-
 export Header from './Header'
-export HeaderCell from './HeaderCell';
-
+export HeaderCell from './HeaderCell'
 export Footer from './Footer'
-
 export Caption from './Caption'
 export Responsive from './Responsive'
-
 export PageSizeSelector from './PageSizeSelector'
