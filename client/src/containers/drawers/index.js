93f79439cde14d375f51b59846576b1ea7d3a260
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-12 19:40:21 -0700

    Remove router HOC + remove redundant code

--- client/src/containers/drawers/index.js
+++ client/src/containers/drawers/index.js
@@ -2,11 +2,10 @@ import React from 'react'
 import {createSelector, createStructuredSelector} from 'reselect'
 import {compose, withProps, defaultProps} from 'recompose'
 import {connect} from 'react-redux'
-import {withRouter} from 'react-router'
 import {isMenuOpen} from 'getters/drawers'
 import Drawer, {LEFT, RIGHT} from 'components/page/drawer'
 import {closeMenu} from 'actions/drawers'
-import Content from './content/Menu'
+import MenuContent from './content/Menu'
 import {getPrimaryDrawer, getSecondaryDrawer} from 'getters/drawers'
 
 export ToggleMenu from './controls/ToggleMenu'
@@ -14,28 +13,20 @@ export ToggleMenu from './controls/ToggleMenu'
 export Forecast from './content/Forecast'
 export HotZoneReport from './content/HotZoneReport'
 
-export const Primary = compose(
-    withRouter,
-    connect(createSelector(
-        getPrimaryDrawer,
-        drawer => drawer.toObject()
-    )),
-    defaultProps({
-        side: RIGHT,
-    }),
-)(Drawer)
+function drawer(side, getter) {
+    return compose(
+        defaultProps({
+            side,
+        }),
+        connect(createSelector(
+            getter,
+            drawer => drawer.toObject()
+        )),
+    )
+}
 
-export const Secondary = compose(
-    withRouter,
-    defaultProps({
-        side: LEFT,
-    }),
-    connect(createSelector(
-        getSecondaryDrawer,
-        drawer => drawer.toObject()
-    )),
-)(Drawer)
-
+export const Primary = drawer(RIGHT, getPrimaryDrawer)(Drawer)
+export const Secondary = drawer(LEFT, getSecondaryDrawer)(Drawer)
 export const Menu = compose(
     connect(createStructuredSelector({
         open: isMenuOpen
@@ -46,6 +37,6 @@ export const Menu = compose(
         side: LEFT,
         width: 300,
         backdrop: true,
-        children: <Content />
+        children: <MenuContent />
     }),
 )(Drawer)
