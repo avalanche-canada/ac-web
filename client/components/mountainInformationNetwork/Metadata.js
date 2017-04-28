ae12ca0469b45e2a4d7f799f499f0ff961634264
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-04-28 11:48:31 -0700

    [MIN] Add prop types to Metadata entries

--- client/components/mountainInformationNetwork/Metadata.js
+++ client/components/mountainInformationNetwork/Metadata.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {compose, withProps, mapProps} from 'recompose'
+import {compose, withProps, mapProps, setPropTypes} from 'recompose'
 import {DateTime} from '~/components/misc'
 import {Link} from 'react-router'
 import {MapLocation} from '~/components/icons'
@@ -10,44 +10,57 @@ import styles from './MountainInformationNetwork.css'
 import {MountainInformationNetworkSubmission as Schema} from '~/api/schemas'
 
 export const SubmittedBy = compose(
+    setPropTypes({
+        children: PropTypes.string.isRequired,
+    }),
     withProps({
         term: 'Submitted by'
     })
 )(Entry)
 
 export const SubmittedOn = compose(
+    setPropTypes({
+        children: PropTypes.instanceOf(Date).isRequired,
+    }),
     mapProps(props => ({
         term: 'Submitted on',
         children: <DateTime value={props.children} />
     }))
 )(Entry)
 
-export const Location = mapProps(({longitude, latitude, link}) => {
-    return {
-        term: 'Location',
-        children: (
-            <span className={styles.MapLocationWrap}>
-                <span className={styles.MapLocationItem}>
-                    {roundCoordinate(longitude)}&nbsp;&deg;, {roundCoordinate(latitude)}&nbsp;&deg;
+export const Location = compose(
+    setPropTypes({
+        longitude: PropTypes.number.isRequired,
+        latitude: PropTypes.number.isRequired,
+        link: PropTypes.string,
+    }),
+    mapProps(({longitude, latitude, link}) => {
+        return {
+            term: 'Location',
+            children: (
+                <span className={styles.MapLocationWrap}>
+                    <span className={styles.MapLocationItem}>
+                        {roundCoordinate(longitude)}&nbsp;&deg;, {roundCoordinate(latitude)}&nbsp;&deg;
+                    </span>
+                    {link && (
+                        <Wrapper tooltip='View on Main Map'>
+                            <Link to={link} className={styles.MapLocationLink}>
+                                <MapLocation />
+                            </Link>
+                        </Wrapper>
+                    )}
                 </span>
-                {link && (
-                    <Wrapper tooltip='View on Main Map'>
-                        <Link to={link} className={styles.MapLocationLink}>
-                            <MapLocation />
-                        </Link>
-                    </Wrapper>
-                )}
-            </span>
-        )
-    }
-})(Entry)
+            )
+        }
+    })
+)(Entry)
 
 MountainInformationNetworkMetadata.propTypes = {
     submittedBy: PropTypes.string.isRequired,
     submittedOn: PropTypes.instanceOf(Date).isRequired,
     shareUrl: PropTypes.string,
-    latitude: PropTypes.number.isRequired,
     longitude: PropTypes.number.isRequired,
+    latitude: PropTypes.number.isRequired,
     submissionId: PropTypes.string.isRequired
 }
 
