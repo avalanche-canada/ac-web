8a041524b869f14e4998a0d52b487f53e18c42a3
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-07-04 12:45:36 -0700

    Simplify Drawer.Banner component
    Remove dependency to recompose

--- client/components/page/drawer/Banner.js
+++ client/components/page/drawer/Banner.js
@@ -1,16 +1,14 @@
-import { DOM } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { compose, withProps, setPropTypes, renameProp } from 'recompose'
 import CSSModules from 'react-css-modules'
 import styles from './Drawer.css'
 
-export default compose(
-    setPropTypes({
-        url: PropTypes.string,
-    }),
-    withProps({
-        styleName: 'Banner',
-    }),
-    renameProp('url', 'src'),
-    CSSModules(styles)
-)(DOM.img)
+Banner.propTypes = {
+    url: PropTypes.string,
+}
+
+function Banner({ url, ...rest }) {
+    return <img styleName="Banner" {...rest} src={url} />
+}
+
+export default CSSModules(Banner, styles)
