8ab48970d21795a90821b83c79d4c19bda6cf918
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-07-11 13:51:24 -0700

    Fix prismic fetching url params encoding

--- client/prismic/Api.js
+++ client/prismic/Api.js
@@ -1,12 +1,38 @@
 import { Predicates } from '~/prismic'
-import axios from 'axios'
-import { endpoint } from './config.json'
+import Axios from 'axios'
+import { root, version } from './config.json'
 
 let API = null
 let API_PROMISE = null
 
+const SEARCH = `/${version}/documents/search`
+
+const axios = Axios.create({
+    baseURL: root,
+    paramsSerializer(params) {
+        return Object.keys(params)
+            .filter(key => Boolean(params[key]))
+            .map(key => {
+                const value = params[key]
+
+                if (Array.isArray(value)) {
+                    const content = value.map(encodeURIComponent).join(',')
+
+                    return `${key}=%5B${content}%5D`
+                } else {
+                    return `${key}=${encodeURIComponent(value)}`
+                }
+            })
+            .join('&')
+    },
+})
+
 function toQuery(predicate) {
     return Predicates.toQuery(predicate)
+}
+
+function getData(response) {
+    return response.data
 }
 
 function getApi() {
@@ -18,8 +44,7 @@ function getApi() {
         return API_PROMISE
     }
 
-    API_PROMISE = axios.get(endpoint).then(response => response.data)
-    // API_PROMISE = fetch(endpoint).then(response => response.json())
+    API_PROMISE = axios.get(version).then(getData)
 
     return API_PROMISE.then(api => {
         API = api
@@ -28,21 +53,21 @@ function getApi() {
     })
 }
 
-export function Query(predicates, options) {
-    return getApi().then(api => query(api, options, predicates))
-}
-
 function query(api, options = {}, predicates) {
     const [{ ref }] = api.refs
 
-    return axios
-        .get(`${endpoint}/documents/search`, {
-            params: {
-                page: 1,
-                ...options,
-                q: `[${predicates.map(toQuery).join('')}]`,
-                ref,
-            },
-        })
-        .then(response => response.data)
+    options = {
+        params: {
+            page: 1,
+            ...options,
+            q: `[${predicates.map(toQuery).join('')}]`,
+            ref,
+        },
+    }
+
+    return axios.get(SEARCH, options).then(getData)
+}
+
+export function Query(predicates, options) {
+    return getApi().then(api => query(api, options, predicates))
 }
