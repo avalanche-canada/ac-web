c8977898fef6e125a35b203beaad7d6200062f33
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-07-11 18:02:53 -0700

    Add the Quote slice type

--- client/prismic/components/slice/Quote.js
+++ client/prismic/components/slice/Quote.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { DateElement } from '~/components/misc'
 import { Blockquote, Footer } from '~/components/blockquote'
 import { StructuredText } from '~/prismic/components/base'
 
 Quote.propTypes = {
     value: PropTypes.arrayOf(
         PropTypes.shape({
-            content: PropTypes.string.isRequired,
-            footer: PropTypes.string,
+            content: PropTypes.array.isRequired,
+            author: PropTypes.string.isRequired,
+            date: PropTypes.instanceOf(Date),
         })
     ).isRequired,
 }
 
 export default function Quote({ value }) {
-    const [{ content, footer }] = value
+    const [{ content, author, date }] = value
 
     return (
         <Blockquote>
             <StructuredText value={content} />
-            {footer &&
+            {author &&
                 <Footer>
-                    <StructuredText value={footer} />
+                    {author}
+                    {date && ' '}
+                    {date && <DateElement value={date} />}
                 </Footer>}
         </Blockquote>
     )
