fec1f74c0aa5bc2d65796bc678199cb1701f1fef
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-07-14 15:08:33 -0700

    [MWF] Remove legacy code for Day1 and Day2
    Images are still not being created by MSC, so there is no point getting
    this code. Once they are being created, we could reintroduce this code.

--- client/components/weather/tabs/Day1.js
+++ client/components/weather/tabs/Day1.js
@@ -1,41 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { DateElement } from '~/components/misc'
-import { StructuredText, Image } from '~/prismic/components/base'
 import Section from './Section'
-import { formatForecastUrl } from '~/services/msc/loop/url'
 
 Day1.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
-    above: PropTypes.string.isRequired,
-    between: PropTypes.string.isRequired,
-    below: PropTypes.string.isRequired,
     children: PropTypes.node,
 }
 
-export default function Day1({ date, above, between, below, children }) {
-    const hasHardWired = above || between || below
-    const type = 'AC_RDPS_BC_weather-systems'
-    const alt = 'Weather Systems'
-    const title = 'Weather Systems'
-    const image1 = {
-        alt,
-        title,
-        src: formatForecastUrl(type, date, 6, 6),
-    }
-    const image2 = {
-        alt,
-        title,
-        src: formatForecastUrl(type, date, 6, 18),
-    }
-
+export default function Day1({ date, children }) {
     return (
         <Section title={<DateElement value={date} />}>
-            {hasHardWired && <StructuredText {...above} />}
-            {hasHardWired && <Image {...image1} openNewTab />}
-            {hasHardWired && <StructuredText {...between} />}
-            {hasHardWired && <Image {...image2} openNewTab />}
-            {hasHardWired && <StructuredText {...below} />}
             {children}
         </Section>
     )
--- client/components/weather/tabs/Day2.js
+++ client/components/weather/tabs/Day2.js
@@ -1,51 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { DateElement } from '~/components/misc'
-import {
-    StructuredText,
-    Image,
-    OpenImageInNewTab,
-} from '~/prismic/components/base'
 import Section from './Section'
-import { formatForecastUrl } from '~/services/msc/loop/url'
 import addDays from 'date-fns/add_days'
 
 Day2.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
-    above: PropTypes.string.isRequired,
-    between: PropTypes.string.isRequired,
-    below: PropTypes.string.isRequired,
     children: PropTypes.node,
 }
 
-export default function Day2({ date, above, between, below, children }) {
-    const hasHardWired = above || between || below
-    const type = 'AC_RDPS_BC_weather-systems'
-    // TODO: Create a "title" and a "alt" that make sense considering the parameters!!!
-    const image1 = {
-        src: formatForecastUrl(type, date, 6, 30),
-        alt: 'Weather Systems',
-        title: 'Weather Systems',
-    }
-    const image2 = {
-        src: formatForecastUrl(type, date, 6, 42),
-        alt: 'Weather Systems',
-        title: 'Weather Systems',
-    }
-
+export default function Day2({ date, children }) {
     return (
         <Section title={<DateElement value={addDays(date, 1)} />}>
-            {hasHardWired && <StructuredText {...above} />}
-            {hasHardWired &&
-                <OpenImageInNewTab>
-                    <Image {...image1} />
-                </OpenImageInNewTab>}
-            {hasHardWired && <StructuredText {...between} />}
-            {hasHardWired &&
-                <OpenImageInNewTab>
-                    <Image {...image2} />
-                </OpenImageInNewTab>}
-            {hasHardWired && <StructuredText {...below} />}
             {children}
         </Section>
     )
