419a51d1cb7191a157f227bd55c819871b9f46b8
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-07-31 13:10:14 -0700

    Convert Button component to simple stateless component

--- client/components/button/Button.js
+++ client/components/button/Button.js
@@ -1,12 +1,5 @@
-import React, { DOM } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import {
-    compose,
-    setPropTypes,
-    setDisplayName,
-    mapProps,
-    defaultProps,
-} from 'recompose'
 import CSSModules from 'react-css-modules'
 import styles from './Button.css'
 import classNames from 'classnames'
@@ -15,66 +8,55 @@ import KIND, { ALL as KINDS } from './kinds'
 const LEFT = 'LEFT'
 const RIGHT = 'RIGHT'
 
-export default compose(
-    setDisplayName('Button'),
-    setPropTypes({
-        children: PropTypes.node,
-        active: PropTypes.bool,
-        shadow: PropTypes.bool,
-        large: PropTypes.bool,
-        transparent: PropTypes.bool,
-        chevron: PropTypes.oneOf([LEFT, RIGHT, true]),
-        kind: PropTypes.oneOf(Array.from(KINDS)),
-        icon: PropTypes.node,
-    }),
-    defaultProps({
-        kind: KIND,
-        active: false,
-        shadow: false,
-        large: false,
-        transparent: false,
-    }),
-    mapProps(
-        ({
-            icon,
-            active,
-            shadow,
-            large,
-            kind,
-            transparent,
-            chevron,
-            ...props
-        }) => {
-            const styleName = classNames({
-                [kind]: true,
-                Active: active,
-                Shadow: shadow,
-                Large: large,
-                Transparent: transparent,
-                ChevronLeft: chevron === LEFT,
-                ChevronRight: chevron === RIGHT,
-                Chevron: chevron === true,
-            })
+Button.propTypes = {
+    children: PropTypes.node,
+    active: PropTypes.bool,
+    shadow: PropTypes.bool,
+    large: PropTypes.bool,
+    transparent: PropTypes.bool,
+    chevron: PropTypes.oneOf([LEFT, RIGHT, true]),
+    kind: PropTypes.oneOf(Array.from(KINDS)),
+    icon: PropTypes.node,
+}
 
-            if (!icon) {
-                return { ...props, styleName }
-            }
+function Button({
+    kind = KIND,
+    active = false,
+    shadow = false,
+    large = false,
+    transparent = false,
+    icon,
+    chevron,
+    children,
+    ...rest
+}) {
+    const styleName = classNames({
+        [kind]: true,
+        Active: active,
+        Shadow: shadow,
+        Large: large,
+        Transparent: transparent,
+        ChevronLeft: chevron === LEFT,
+        ChevronRight: chevron === RIGHT,
+        Chevron: chevron === true,
+    })
 
-            const { children } = props
+    return (
+        <button
+            styleName={`${styleName} ${children ? '' : 'IconOnly'}`}
+            {...rest}>
+            {icon
+                ? <div styleName="IconContainer">
+                      {icon}
+                      {typeof children === 'string'
+                          ? <span>
+                                {children}
+                            </span>
+                          : children}
+                  </div>
+                : children}
+        </button>
+    )
+}
 
-            return {
-                ...props,
-                styleName: `${styleName} ${children ? '' : 'IconOnly'}`,
-                children: (
-                    <div styleName="IconContainer">
-                        {icon}
-                        {typeof children === 'string'
-                            ? <span>{children}</span>
-                            : children}
-                    </div>
-                ),
-            }
-        }
-    ),
-    CSSModules(styles, { allowMultiple: true })
-)(DOM.button)
+export default CSSModules(Button, styles, { allowMultiple: true })
