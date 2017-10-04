275396332ef84b2c93b814d0d88f28bedb1f1fe5
Will Harding <wharding@avalanche.ca>
2017-10-04 16:23:20 -0700

    Cache MCR list page, clean up logging

--- server/api/mcr/index.js
+++ server/api/mcr/index.js
@@ -14,22 +14,27 @@ var AC_MCR_HOST = process.env.AC_MCR_HOST;
 var AC_MCR_URL          = AC_MCR_HOST + '/sapi/public';
 
 router.get('/', function(req, res) {
-    return getNodeList(function (err, nodes){
-        if(err){
-            return _error(res,err);
-        }
-        async.map(nodes, getReportAndUser, function(err, reports){
+    //TODO(wnh): Clean this up a bit 
+    cache.wrap('mcr/format_list', function(cb) {
+        return getNodeList(function (err, nodes){
+            async.map(nodes, getReportAndUser, function(err, reports){
+                if(err){
+                    return cb(err)
+                }
+                var fmt_reports = reports.map(function(ru){
+                    return mcr_format.formatReportFull(ru.report, ru.user);
+                });
+                cb(null, fmt_reports);
+            });
+        });
+    } , function(err, data){
             if(err){
                 return _error(res,err);
             }
-            var fmt_reports = reports.map(function(ru){
-                return mcr_format.formatReportFull(ru.report, ru.user);
-            });
             return res
                     .status(200)
-                    .json(fmt_reports)
+                    .json(data)
                     .end();
-        });
     });
 });
 
@@ -73,21 +78,21 @@ function getNodeList(cb) {
 }
 
 function __getNodeList(cb) {
-    logger.debug('MCR - getNodeList(report_type=5)')
+    logger.debug('MCR::getNodeList(report_type=5)')
     return request({url: AC_MCR_URL + '/node.json', qs: {report_type: 5}}, function(err, resp, body){
         if (err) {cb(err);}
-        logger.debug('MCR - getNodeList(report_type=5) - return ' + resp.statusCode);
+        logger.debug('MCR::getNodeList(report_type=5) - return_status=%d', resp.statusCode);
 
         var body_5 = JSON.parse(body);
 
-        logger.debug('MCR - getNodeList(report_type=3)')
+        logger.debug('MCR::getNodeList(report_type=3)')
         return request({url: AC_MCR_URL + '/node.json', qs: {report_type: 5}}, function(err, resp, body){
             if (err) {cb(err);}
-            logger.debug('MCR - getNodeList(report_type=3) - return ' + resp.statusCode);
+            logger.debug('MCR::getNodeList(report_type=3) return_status=%d', resp.statusCode);
             var body_3 = JSON.parse(body);
             var list = body_5.concat(body_3);
             list = list.sort(function(node) { return Number.parseInt(node.updated); });
-            logger.debug('MCR - getNodeList() -> ' + list.length + ' nodes');
+            logger.debug('MCR::getNodeList() length=%d', list.length);
             cb(null, list);
         });
     });
@@ -97,13 +102,13 @@ function __getNodeList(cb) {
 function getReport(node_id, cb) {
     var cache_key = 'mcr/report/' + node_id;
     return cache.wrap(cache_key, function(_cb){
-        logger.debug("REPORT FETCH")
+        logger.debug("MCR::getReport(node_id=%d) action=fetching", node_id);
         return __getReport(node_id, _cb);
     }, cb);
 }
 
 function __getReport(node_id, cb) {
-    logger.debug('MCR - getReport(node_id='+node_id+', cb)');
+    logger.debug('MCR::getReport(node_id=%d', node_id);
     return request({
         url: AC_MCR_URL + '/node/' + node_id + '.json', 
         qs: {report_type: 5}}, 
@@ -111,7 +116,7 @@ function __getReport(node_id, cb) {
             if(err) {
                 cb(err);
             }
-            logger.debug('MCR - getReport(node_id='+node_id+', cb) -> ' + resp.statusCode);
+            logger.debug('MCR::getReport(node_id=%d) return_status=%d', node_id, resp.statusCode);
             cb(null, JSON.parse(body));
         }
     );
@@ -120,17 +125,17 @@ function __getReport(node_id, cb) {
 function getUser(user_id, cb) {
     var cache_key = 'mcr/user/' + user_id;
     return cache.wrap(cache_key, function(cache_cb){
-        logger.debug("USER FETCH");
+        logger.debug("MCR::getUser(user_id=%d) action=fetching", user_id);
         __getUser(user_id, cache_cb);
     }, cb);
 }
 function __getUser(user_id, cb) {
-    logger.debug('MCR - getUser(user_id='+user_id+', cb)');
+    logger.debug('MCR::getUser(user_id=%d', user_id);
     return request(AC_MCR_URL + '/user/' + user_id + '.json', function(err, resp, body){
         if(err) {
             cb(err);
         }
-        logger.debug('MCR - getUser(user_id='+user_id+', cb)');
+        logger.debug('MCR::getUser(user_id=%d)', user_id);
         cb(null, JSON.parse(body));
     });
 }
