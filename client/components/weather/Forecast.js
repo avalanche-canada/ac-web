6c5a184f40e71b21018ed2b940aa0573c870cbce
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-10-18 20:49:57 -0700

    [MWF] Fix error with rendering

--- client/components/weather/Forecast.js
+++ client/components/weather/Forecast.js
@@ -1,4 +1,4 @@
-import React, { createElement } from 'react'
+import React, { PureComponent, createElement } from 'react'
 import PropTypes from 'prop-types'
 import Tabs, { HeaderSet, Header, PanelSet, Panel } from '~/components/tabs'
 import Synopsis from './tabs/Synopsis'
@@ -10,39 +10,58 @@ import SliceSet from './tabs/SliceSet'
 import Tutorial from '~/containers/WeatherTutorial'
 import TABS, { DAY5TO7 } from '~/components/weather/tabs'
 
-Forecast.propTypes = {
-    forecast: PropTypes.object.isRequired,
-    tabs: PropTypes.arrayOf(PropTypes.oneOf(TABS)),
-}
+export default class Forecast extends PureComponent {
+    static propTypes = {
+        forecast: PropTypes.object.isRequired,
+        tabs: PropTypes.arrayOf(PropTypes.oneOf(TABS)),
+    }
+    get hasDay5To7() {
+        const { forecast, tabs } = this.props
 
-export default function Forecast({ forecast = {}, tabs = TABS }) {
-    const hasDay5To7 =
-        tabs.includes(DAY5TO7) && !forecast[name] && !forecast[`${name}More`]
+        return (
+            tabs.includes(DAY5TO7) && (forecast.day5To7 || forecast.day5To7More)
+        )
+    }
+    get headers() {
+        return [
+            <Header key="synopsis">Synopsis</Header>,
+            <Header key="day1">Day 1</Header>,
+            <Header key="day2">Day 2</Header>,
+            <Header key="day3To4">Day 3-4</Header>,
+            this.hasDay5To7 && <Header key="day5To7">Day 5-7</Header>,
+            <Header key="tutorial">Tutorial</Header>,
+        ].filter(Boolean)
+    }
+    get panels() {
+        const { forecast } = this.props
 
-    return (
-        <Tabs>
-            <HeaderSet>
-                <Header>Synopsis</Header>
-                <Header>Day 1</Header>
-                <Header>Day 2</Header>
-                <Header>Day 3-4</Header>
-                {hasDay5To7 && <Header>Day 5-7</Header>}
-                <Header>Tutorial</Header>
-            </HeaderSet>
-            <PanelSet>
-                <Panel>{createPanel(Synopsis, 'synopsis', forecast)}</Panel>
-                <Panel>{createPanel(Day1, 'day1', forecast)}</Panel>
-                <Panel>{createPanel(Day2, 'day2', forecast)}</Panel>
-                <Panel>{createPanel(Day3To4, 'day3To4', forecast)}</Panel>
-                {hasDay5To7 && (
-                    <Panel>{createPanel(Day5To7, 'day5To7', forecast)}</Panel>
-                )}
-                <Panel>
-                    <Tutorial uid="weather" />
+        return [
+            <Panel key="synopsis">
+                {createPanel(Synopsis, 'synopsis', forecast)}
+            </Panel>,
+            <Panel key="day1">{createPanel(Day1, 'day1', forecast)}</Panel>,
+            <Panel key="day2">{createPanel(Day2, 'day2', forecast)}</Panel>,
+            <Panel key="day3To4">
+                {createPanel(Day3To4, 'day3To4', forecast)}
+            </Panel>,
+            this.hasDay5To7 && (
+                <Panel key="day5To7">
+                    {createPanel(Day5To7, 'day5To7', forecast)}
                 </Panel>
-            </PanelSet>
-        </Tabs>
-    )
+            ),
+            <Panel key="tutorial">
+                <Tutorial uid="weather" />
+            </Panel>,
+        ].filter(Boolean)
+    }
+    render() {
+        return (
+            <Tabs>
+                <HeaderSet>{this.headers}</HeaderSet>
+                <PanelSet>{this.panels}</PanelSet>
+            </Tabs>
+        )
+    }
 }
 
 function createPanel(component, name, forecast) {
