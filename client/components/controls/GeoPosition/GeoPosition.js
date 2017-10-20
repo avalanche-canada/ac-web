bb8186dc2d91e805f04eeaa5996a0c3cd1959a57
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-10-20 14:42:36 -0700

    Fix import for PropTypes

--- client/components/controls/GeoPosition/GeoPosition.js
+++ client/components/controls/GeoPosition/GeoPosition.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
 import CSSModules from 'react-css-modules'
 import mapbox from 'services/mapbox/map'
 import {
