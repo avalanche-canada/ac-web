9b13089bc46719c0d3be0bcc9f18a99df2ccae61
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-10-22 10:48:09 -0700

    Make Forecast using the new Tabs component

--- client/components/forecast/Forecast.js
+++ client/components/forecast/Forecast.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Headline from './Headline'
 import Summary from './Summary'
 import Footer from './Footer'
 import ArchiveWarning from './ArchiveWarning'
 import { Table, Day, DaySet, Condition, Confidence } from './danger'
 import { Problem, Topic, TopicSet, Advice, Comment } from './problem'
-import { TabSet, Tab, LOOSE } from 'components/tab'
+import Tabs, { HeaderSet, Header, PanelSet, Panel } from 'components/tabs'
 import { InnerHTML } from 'components/misc'
 
 Forecast.propTypes = {
@@ -44,81 +44,89 @@ export default function Forecast({
         <section>
             {isArchived && <ArchiveWarning region={region} date={date} />}
             <Headline>{highlights}</Headline>
-            <TabSet stacked={false} theme={LOOSE}>
-                <Tab title="Danger ratings">
-                    <Condition mode={dangerMode} />
-                    <Table mode={dangerMode}>
-                        <DaySet>
-                            {dangerRatings.map(
-                                ({ date, dangerRating }, index) => (
-                                    <Day
-                                        key={index}
-                                        date={date}
-                                        {...dangerRating}
-                                    />
-                                )
-                            )}
-                        </DaySet>
-                        <Confidence {...confidence} />
-                    </Table>
-                </Tab>
-                <Tab title="Problems" disabled={problems.length === 0}>
-                    {problems.map(
-                        (
-                            { type, icons, comment, travelAndTerrainAdvice },
-                            index
-                        ) => (
-                            <Problem
-                                key={type}
-                                title={`Avalanche Problem ${index + 1}: ${type}`}>
-                                <TopicSet>
-                                    <Topic
-                                        title="What Elevation?"
-                                        src={icons.elevations}
-                                    />
-                                    <Topic
-                                        title="Which Slopes?"
-                                        src={icons.aspects}
-                                    />
-                                    <Topic
-                                        title="Chances of Avalanches?"
-                                        src={icons.likelihood}
-                                    />
-                                    <Topic
-                                        title="Expected Size?"
-                                        src={icons.expectedSize}
-                                    />
-                                </TopicSet>
-                                <Comment>{comment}</Comment>
-                                <Advice>{travelAndTerrainAdvice}</Advice>
-                            </Problem>
-                        )
-                    )}
-                </Tab>
-                <Tab title="Details">
-                    <Summary title="Avalanche Summary">
-                        <InnerHTML>
-                            {avalancheSummary}
-                        </InnerHTML>
-                    </Summary>
-                    <Summary title="Snowpack Summary">
-                        <InnerHTML>
-                            {snowpackSummary}
-                        </InnerHTML>
-                    </Summary>
-                    <Summary title="Weather Forecast">
-                        <InnerHTML>
-                            {weatherForecast}
-                        </InnerHTML>
-                        <p>
-                            More details can be found on the
-                            {' '}
-                            <Link to="/weather">Mountain Weather Forecast</Link>
-                            .
-                        </p>
-                    </Summary>
-                </Tab>
-            </TabSet>
+            <Tabs theme="loose">
+                <HeaderSet>
+                    <Header>Danger ratings</Header>
+                    <Header disabled={problems.length === 0}>Problems</Header>
+                    <Header>Details</Header>
+                </HeaderSet>
+                <PanelSet>
+                    <Panel>
+                        <Condition mode={dangerMode} />
+                        <Table mode={dangerMode}>
+                            <DaySet>
+                                {dangerRatings.map(
+                                    ({ date, dangerRating }, index) => (
+                                        <Day
+                                            key={index}
+                                            date={date}
+                                            {...dangerRating}
+                                        />
+                                    )
+                                )}
+                            </DaySet>
+                            <Confidence {...confidence} />
+                        </Table>
+                    </Panel>
+                    <Panel>
+                        {problems.map(
+                            (
+                                {
+                                    type,
+                                    icons,
+                                    comment,
+                                    travelAndTerrainAdvice,
+                                },
+                                index
+                            ) => (
+                                <Problem
+                                    key={type}
+                                    title={`Avalanche Problem ${index +
+                                        1}: ${type}`}>
+                                    <TopicSet>
+                                        <Topic
+                                            title="What Elevation?"
+                                            src={icons.elevations}
+                                        />
+                                        <Topic
+                                            title="Which Slopes?"
+                                            src={icons.aspects}
+                                        />
+                                        <Topic
+                                            title="Chances of Avalanches?"
+                                            src={icons.likelihood}
+                                        />
+                                        <Topic
+                                            title="Expected Size?"
+                                            src={icons.expectedSize}
+                                        />
+                                    </TopicSet>
+                                    <Comment>{comment}</Comment>
+                                    <Advice>{travelAndTerrainAdvice}</Advice>
+                                </Problem>
+                            )
+                        )}
+                    </Panel>
+                    <Panel>
+                        <Summary title="Avalanche Summary">
+                            <InnerHTML>{avalancheSummary}</InnerHTML>
+                        </Summary>
+                        <Summary title="Snowpack Summary">
+                            <InnerHTML>{snowpackSummary}</InnerHTML>
+                        </Summary>
+                        <Summary title="Weather Forecast">
+                            <InnerHTML>{weatherForecast}</InnerHTML>
+                            <p>
+                                More details can be found on the{' '}
+                                <Link to="/weather">
+                                    Mountain Weather Forecast
+                                </Link>
+                                .
+                            </p>
+                        </Summary>
+                    </Panel>
+                </PanelSet>
+            </Tabs>
             <Footer region={region} />
         </section>
     )
