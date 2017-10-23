10a1ac182f50f767963a2556546cf629ad6ca947
Will Harding <wharding@avalanche.ca>
2017-10-23 14:50:57 -0700

    Prep for v8.4.0 update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,7 @@
 set -e
 . ~/.nvm/nvm.sh 
-nvm install v4.4.3
-nvm use v4.4.3
-npm install npm@3.10.7 -g
+nvm install v8.4.0
+nvm use v8.4.0
 npm config set registry http://registry.npmjs.org/
 npm cache clean
 npm install
