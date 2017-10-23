026ee4af8bc026e04ffba8005da5b89c89d2e8e0
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-10-23 15:03:57 -0700

    [TAB] Create a PanelSet that renders all panels
    tcomb-form prefers to have everything rendered.

--- client/components/tabs/PanelSet.js
+++ client/components/tabs/PanelSet.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, Children } from 'react'
+import React, { PureComponent, Children, cloneElement } from 'react'
 import PropTypes from 'prop-types'
 import styles from './Tabs.css'
 
@@ -16,6 +16,25 @@ export default class PanelSet extends PureComponent {
         const { activeIndex } = this.props
 
         return this.children[activeIndex] || null
+    }
+}
+
+const EAGER_STYLES = new Map([
+    [true, { display: 'inherit' }],
+    [false, { display: 'none' }],
+])
+
+export class EagerPanelSet extends PureComponent {
+    static propTypes = {
+        children: PropTypes.arrayOf(PropTypes.element).isRequired,
+        activeIndex: PropTypes.number,
+    }
+    renderPanel = (panel, index) =>
+        cloneElement(panel, {
+            style: EAGER_STYLES.get(index === this.props.activeIndex),
+        })
+    render() {
+        return Children.map(this.props.children, this.renderPanel)
     }
 }
 
