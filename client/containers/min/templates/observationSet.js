1d34a7256f0f255bc5fd333a4f53d225c03ffd88
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-10-23 15:04:26 -0700

    [MIN] Make observation set to use the new tab component

--- client/containers/min/templates/observationSet.js
+++ client/containers/min/templates/observationSet.js
@@ -1,6 +1,6 @@
 import React, { Children } from 'react'
 import t from 'vendor/tcomb-form'
-import { TabSet, Tab } from 'components/tab'
+import { HeaderSet, ColoredHeader, EagerPanelSet, Panel } from 'components/tabs'
 import { Reset } from 'components/button'
 import { NAMES, COLORS } from 'constants/min'
 
@@ -10,33 +10,48 @@ export default struct.clone({
     renderFieldsetFields(children, locals) {
         const { disabled, config: { onTabActivate, activeIndex } } = locals
 
-        return (
-            <TabSet
-                lazy={false}
-                arrow
-                onActivate={onTabActivate}
-                activeIndex={activeIndex}>
-                {Children.toArray(children).map(child => {
-                    const { ref, props: { value } } = child
-                    const title = NAMES.get(ref)
-                    const color = value ? COLORS.get(ref) : null
-                    function handleReset() {
-                        locals.onChange(ref, null)
-                        locals.config.onReportRemove(ref)
-                    }
+        children = Children.toArray(children)
 
-                    return (
-                        <Tab key={ref} title={title} color={color}>
-                            {child}
-                            <Reset
-                                disabled={disabled || !value}
-                                onClick={handleReset}>
-                                Remove your {title} report
-                            </Reset>
-                        </Tab>
-                    )
-                })}
-            </TabSet>
+        return (
+            <div>
+                <HeaderSet
+                    activeIndex={activeIndex}
+                    onActiveIndexChange={onTabActivate}>
+                    {children.map(child => {
+                        const { ref, props: { value } } = child
+                        const color = value ? COLORS.get(ref) : null
+
+                        return (
+                            <ColoredHeader
+                                arrow={Boolean(value)}
+                                key={ref}
+                                color={color}>
+                                {NAMES.get(ref)}
+                            </ColoredHeader>
+                        )
+                    })}
+                </HeaderSet>
+                <EagerPanelSet activeIndex={activeIndex}>
+                    {children.map(child => {
+                        const { ref, props: { value } } = child
+                        function handleReset() {
+                            locals.onChange(ref, null)
+                            locals.config.onReportRemove(ref)
+                        }
+
+                        return (
+                            <Panel key={ref}>
+                                {child}
+                                <Reset
+                                    disabled={disabled || !value}
+                                    onClick={handleReset}>
+                                    Remove your {NAMES.get(ref)} report
+                                </Reset>
+                            </Panel>
+                        )
+                    })}
+                </EagerPanelSet>
+            </div>
         )
     },
 })
