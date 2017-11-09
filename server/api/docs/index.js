96bcf3358460ad00ab48ad3d61472d840bf245bd
Will Harding <wharding@avalanche.ca>
2017-11-09 15:38:45 -0800

    Use local swagger ui

--- server/api/docs/index.js
+++ server/api/docs/index.js
@@ -8,12 +8,10 @@ var fs = require('fs');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    var fname = path.join(__dirname, 'doc_index.html');
-    return res
-        .status(200)
-        .set('Content-Type', 'text/html')
-        .send(fs.readFileSync(fname))
-        .end();
+    return res.sendFile(path.join(__dirname, 'doc_index.html'));
+});
+router.get('/view', function(rep, res) {
+    return res.sendFile(path.join(__dirname, 'swagger-ui.html'));
 });
 
 var JSON_SCHEMA = yaml.safeLoad(
