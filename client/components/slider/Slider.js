10b3b53505976d84d956aa6470875e3f92b5297b
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-11-27 13:06:55 -0800

    Revert "Remove duplicated component"
    This reverts commit 638c0d7fbc77e2f744a70d9d761e6851bd0b30be.

---
+++ client/components/slider/Slider.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { compose, withState, defaultProps, setPropTypes } from 'recompose'
+import ImageGallery from 'components/gallery'
+
+// TODO: Remove this duplicate component
+
+export default compose(
+    withState('instance', 'ref', null),
+    setPropTypes({
+        children: PropTypes.node,
+    }),
+    defaultProps({
+        renderItem({ children, ...props }) {
+            return (
+                <div className="image-gallery-image">
+                    <img {...props} />
+                    {children}
+                </div>
+            )
+        },
+        showBullets: true,
+    })
+)(ImageGallery)
