0d742c8ab006b51686b4db9149090aa1ca4fa74a
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-12-20 14:21:32 -0800

    [Post] Move components to layouts

--- client/layouts/feed/Post.js
+++ client/layouts/feed/Post.js
@@ -2,8 +2,12 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom'
 import { Status } from 'components/misc'
-import Component from 'components/page/post'
 import { Post as Container } from 'prismic/containers'
+import { Page, Content, Header, Main, Headline, Aside } from 'components/page'
+import { Metadata, Entry } from 'components/metadata'
+import { DateElement } from 'components/time'
+import { StructuredText } from 'prismic/components/base'
+import Sidebar from './Sidebar'
 import { NEWS, BLOG, EVENT } from 'constants/prismic'
 
 class Post extends PureComponent {
@@ -11,16 +15,74 @@ class Post extends PureComponent {
         type: PropTypes.string.isRequired,
         uid: PropTypes.string.isRequired,
     }
+    renderHeader(status, post) {
+        const title = status.isLoading ? 'Loading...' : post.title
+
+        return <Header title={title} />
+    }
+    renderMetadata({ date, source, location, hostedBy, startDate, endDate }) {
+        const hasDateRange = startDate && endDate
+
+        return (
+            <Metadata>
+                {date && (
+                    <Entry term="Date">
+                        {hasDateRange ? (
+                            <span>
+                                <DateElement value={startDate} /> <em>to</em>{' '}
+                                <DateElement value={endDate} />
+                            </span>
+                        ) : (
+                            <DateElement value={date} />
+                        )}
+                    </Entry>
+                )}
+                {typeof location === 'string' && (
+                    <Entry term="Location">{location}</Entry>
+                )}
+                {source && <Entry term="Source">{source}</Entry>}
+                {hostedBy && <Entry term="Hosted by">{hostedBy}</Entry>}
+            </Metadata>
+        )
+    }
+    renderContent({ headline, content }) {
+        return [
+            headline ? (
+                <Headline>
+                    <StructuredText value={headline} />
+                </Headline>
+            ) : null,
+            <StructuredText value={content} />,
+        ]
+    }
     children = ({ status, post }) => {
         if (status.isLoaded && !post) {
             // Post not found, redirecting to feed
             return <Redirect to={this.props.type} />
         }
 
-        return [<Status {...status} />, <Component {...post} {...this.props} />]
+        const { uid, type } = this.props
+
+        return [
+            this.renderHeader(status, post),
+            <Content>
+                <Main>
+                    {post && this.renderMetadata(post)}
+                    {post && this.renderContent(post)}
+                    <Status {...status} />
+                </Main>
+                <Aside>
+                    <Sidebar type={type} uid={uid} />
+                </Aside>
+            </Content>,
+        ]
     }
     render() {
-        return <Container {...this.props}>{this.children}</Container>
+        return (
+            <Page>
+                <Container {...this.props}>{this.children}</Container>
+            </Page>
+        )
     }
 }
 
