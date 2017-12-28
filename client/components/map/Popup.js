7111c036a5d086c9648ed801ddb2549c25a3f423
Karl Guillotte <karl.guillotte.1@gmail.com>
2017-12-28 13:15:13 -0800

    Simplify Popup component

--- client/components/map/Popup.js
+++ client/components/map/Popup.js
@@ -1,6 +1,6 @@
 import { Component } from 'react'
-import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 import mapbox from 'services/mapbox/map'
 
 const { LngLat } = mapbox
@@ -18,7 +18,6 @@ const ANCHORS = [
 export default class Popup extends Component {
     static propTypes = {
         lngLat: PropTypes.instanceOf(LngLat).isRequired,
-        show: PropTypes.bool,
         children: PropTypes.element.isRequired,
         options: PropTypes.shape({
             closeButton: PropTypes.bool,
@@ -27,49 +26,23 @@ export default class Popup extends Component {
             offset: PropTypes.object,
         }),
     }
-    static defaultProps = {
-        show: false,
-    }
     static contextTypes = {
         map: PropTypes.object.isRequired,
     }
-    popup = null
-    constructor(props) {
-        super(props)
-
+    componentDidMount() {
         this.container = document.createElement('div')
-    }
-    get map() {
-        return this.context.map
-    }
-    createPopup() {
-        const { lngLat, children, show, options } = this.props
-        const popup = new mapbox.Popup(options)
-
-        ReactDOM.render(children, this.container)
-
-        popup.setLngLat(lngLat)
-        popup.setDOMContent(this.container)
-
-        if (show) {
-            popup.addTo(this.map)
-        }
-
-        this.popup = popup
-    }
-    destroyPopup() {
-        this.popup.remove()
-        ReactDOM.unmountComponentAtNode(this.container)
-    }
-    componentWillMount() {
-        this.createPopup()
+        this.popup = new mapbox.Popup(this.props.options)
     }
     componentDidUpdate() {
-        this.destroyPopup()
-        this.createPopup()
+        ReactDOM.render(this.props.children, this.container, () => {
+            this.popup
+                .setDOMContent(this.container)
+                .setLngLat(this.props.lngLat)
+                .addTo(this.context.map)
+        })
     }
     componentWillUnmount() {
-        this.destroyPopup()
+        this.popup.remove()
     }
     render() {
         return null
