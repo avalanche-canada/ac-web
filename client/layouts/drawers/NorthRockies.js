edc1148398f153f511ce0ef6fd3411eb7e935701
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-02-09 10:35:32 -0800

    [SPAW] Add spaw to North Rockies drawer and full page

--- client/layouts/drawers/NorthRockies.js
+++ client/layouts/drawers/NorthRockies.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import ForecastRegion from 'containers/ForecastRegion'
 import { NorthRockiesBlogFeed } from 'layouts/feed'
+import { SPAW as SPAWComponent } from 'components/misc'
+import { Region as SPAW } from 'layouts/SPAW'
 import Sponsor from 'layouts/Sponsor'
 import { Navbar, Header, Container, Body, Close } from 'components/page/drawer'
 import DisplayOnMap from './DisplayOnMap'
@@ -25,10 +27,18 @@ export default class NorthRockies extends PureComponent {
 
         return <DisplayOnMap onClick={handleLocateClick} />
     }
+    renderSPAW = ({ link }) => {
+        const style = {
+            flex: 1,
+        }
+
+        return <SPAWComponent link={link} style={style} />
+    }
     render() {
         return (
             <Container>
                 <Navbar>
+                    <SPAW name="north-rockies">{this.renderSPAW}</SPAW>
                     <Sponsor label={null} />
                     <Close onClick={this.props.onCloseClick} />
                 </Navbar>
