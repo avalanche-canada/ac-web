030914e76e2fd14613ba3c2ff3d3b77b815f1458
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-02-14 18:59:13 -0800

    Add unsupported webgl message

--- client/layouts/TripPlanner/index.js
+++ client/layouts/TripPlanner/index.js
@@ -1,2 +1,5 @@
-export default from './Layout'
+import supported from '@mapbox/mapbox-gl-supported'
+import Unsupported from './Unsupported'
+import Layout from './Layout'
 
+export default (supported() ? Layout : Unsupported)
