0b9424e79167464095f91921ef41052e87707047
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-03-20 17:13:07 -0700

    Use static component for contact form

--- client/prismic/components/slice/ContactForm.js
+++ client/prismic/components/slice/ContactForm.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import StaticComponent from 'components/StaticComponent'
 
-export default function ContactForm() {
-    return (
-        <iframe
-            src="https://avalanchecanada.supportbee.com/web_tickets/new?embed=true&locale=en"
-            style={{ width: '100%', height: 500, border: 'none' }}
-        />
-    )
+export default class ContactForm extends StaticComponent {
+    render() {
+        return (
+            <iframe
+                src="https://avalanchecanada.supportbee.com/web_tickets/new?embed=true&locale=en"
+                style={{ width: '100%', height: 500, border: 'none' }}
+            />
+        )
+    }
 }
