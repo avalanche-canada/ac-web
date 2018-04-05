ec1ebc7fb14fa406a6718ed29fabb952f5792c27
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-04-05 11:02:13 -0700

    Extract problem rendering into a function

--- client/components/forecast/problem/index.js
+++ client/components/forecast/problem/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Problem from './Problem'
 import TopicSet from './TopicSet'
@@ -6,28 +6,27 @@ import Topic from './Topic'
 import Advice from './Advice'
 import Comment from './Comment'
 
-Set.propTypes = {
+ProblemSet.propTypes = {
     problems: PropTypes.array.isRequired,
 }
 
-export default function Set({ problems }) {
-    return problems.map(
-        ({ type, icons, comment, travelAndTerrainAdvice }, index) => (
-            <Problem
-                key={type}
-                title={`Avalanche Problem ${index + 1}: ${type}`}>
-                <TopicSet>
-                    <Topic title="What Elevation?" src={icons.elevations} />
-                    <Topic title="Which Slopes?" src={icons.aspects} />
-                    <Topic
-                        title="Chances of Avalanches?"
-                        src={icons.likelihood}
-                    />
-                    <Topic title="Expected Size?" src={icons.expectedSize} />
-                </TopicSet>
-                <Comment>{comment}</Comment>
-                <Advice>{travelAndTerrainAdvice}</Advice>
-            </Problem>
-        )
+export default function ProblemSet({ problems }) {
+    return <Fragment>{problems.map(renderProblem)}</Fragment>
+}
+
+function renderProblem(problem, index) {
+    const { type, icons, comment, travelAndTerrainAdvice } = problem
+
+    return (
+        <Problem title={`Avalanche Problem ${index + 1}: ${type}`}>
+            <TopicSet>
+                <Topic title="What Elevation?" src={icons.elevations} />
+                <Topic title="Which Slopes?" src={icons.aspects} />
+                <Topic title="Chances of Avalanches?" src={icons.likelihood} />
+                <Topic title="Expected Size?" src={icons.expectedSize} />
+            </TopicSet>
+            <Comment>{comment}</Comment>
+            <Advice>{travelAndTerrainAdvice}</Advice>
+        </Problem>
     )
 }
