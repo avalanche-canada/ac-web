91fd2d1bcdb2ea07fd996869ce360d6e20a9cb48
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-04-13 10:41:04 -0700

    [COMPONENTS/MAP/MARKER] Remove draggable capabilities
    - it is not used in the codebase

--- client/components/map/Marker.js
+++ client/components/map/Marker.js
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types'
 import StaticComponent from 'components/StaticComponent'
 import mapbox from 'mapbox-gl/dist/mapbox-gl'
-import noop from 'lodash/noop'
 
 const { LngLat } = mapbox
 
@@ -11,18 +10,10 @@ export default class Marker extends StaticComponent {
         element: PropTypes.object.isRequired,
         onClick: PropTypes.func,
         options: PropTypes.object,
-        // TODO: Draggable marker is comming soonish
-        // https://github.com/mapbox/mapbox-gl-js/issues/3087
         draggable: PropTypes.bool,
-        onDragStart: PropTypes.func,
-        onDragEnd: PropTypes.func,
     }
     static contextTypes = {
         map: PropTypes.object.isRequired,
-    }
-    static defaultProps = {
-        onDragStart: noop,
-        onDragEnd: noop,
     }
     _marker = null
     set marker(marker) {
@@ -30,23 +21,17 @@ export default class Marker extends StaticComponent {
 
         this._marker = marker
 
-        marker.addTo(this.map)
+        marker.addTo(this.context.map)
     }
     get marker() {
         return this._marker
-    }
-    get map() {
-        return this.context.map
-    }
-    get element() {
-        return this.props.element
     }
     remove() {
         if (this.marker) {
             this.marker.remove()
         }
     }
-    createMarker({ element, lngLat, options, onClick, draggable }) {
+    createMarker({ element, lngLat, options, onClick }) {
         // TODO: Remove that. Should by provided as part of the element
         if (onClick) {
             Object.assign(element, {
@@ -54,44 +39,7 @@ export default class Marker extends StaticComponent {
             })
         }
 
-        if (draggable) {
-            Object.assign(element, {
-                onmousedown: this.handleMousedown,
-            })
-            element.classList.add('draggable-map-marker')
-        }
-
         return new mapbox.Marker(element, options).setLngLat(lngLat)
-    }
-    handleMousedown = ({ target }) => {
-        const { map } = this
-        const canvas = map.getCanvas()
-
-        canvas.style.cursor = 'move'
-        target.style.pointerEvents = 'none'
-
-        map.dragPan.disable()
-
-        map.on('mousemove', this.handleMapMousemove)
-        map.on('mouseup', this.handleMapMouseup)
-    }
-    handleMapMousemove = event => {
-        this.marker.setLngLat(event.lngLat)
-        this.element.style.transform += ' scale(1.25)'
-    }
-    handleMapMouseup = event => {
-        const { map } = this
-        const canvas = map.getCanvas()
-
-        canvas.style.cursor = ''
-        this.element.style.pointerEvents = ''
-
-        map.dragPan.enable()
-
-        map.off('mousemove', this.handleMapMousemove)
-        map.off('mouseup', this.handleMapMouseup)
-
-        this.props.onDragEnd(event)
     }
     componentDidMount() {
         this.marker = this.createMarker(this.props)
@@ -99,7 +47,7 @@ export default class Marker extends StaticComponent {
     componentWillReceiveProps(nextProps) {
         const { element, lngLat } = nextProps
 
-        if (this.element !== element) {
+        if (this.props.element !== element) {
             this.marker = this.createMarker(nextProps)
             return
         }
