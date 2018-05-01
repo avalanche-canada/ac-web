e174f6c06becea5ba2cd3009dba1d908fa8aa2cd
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-05-01 14:00:25 -0700

    Remove a TODO

--- client/components/Device.js
+++ client/components/Device.js
@@ -1,7 +1,6 @@
 import { Component } from 'react'
 import PropTypes from 'prop-types'
 
-// TODO: Create a StaticComponent
 export default class Device extends Component {
     static propTypes = {
         children: PropTypes.func.isRequired,
