88a512b7b7f920718a3fcac8e330df78a5042f57
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-05-02 11:31:35 -0700

    Better test for function

--- client/components/map/Marker.js
+++ client/components/map/Marker.js
@@ -32,7 +32,7 @@ export default class Marker extends StaticComponent {
     }
     createMarker({ element, lngLat, options, onClick }) {
         // TODO: Remove that. Should by provided as part of the element
-        if (onClick) {
+        if (typeof onClick === 'function') {
             Object.assign(element, {
                 onclick: event => onClick(this.props, event),
             })
