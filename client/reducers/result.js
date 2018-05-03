15da893f0aa974e04ba49d47206f4f6ab80a197c
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-05-03 11:24:53 -0700

    [STORE/RESULT] Remove unused "props" property

--- client/reducers/result.js
+++ client/reducers/result.js
@@ -13,8 +13,6 @@ export const Result = Immutable.Record({
     pageSize: null,
     totalPages: null,
     totalResultsSize: null,
-    // TODO: Look if this is being used and remove!!!
-    props: {},
 })
 
 Object.assign(Result, {
