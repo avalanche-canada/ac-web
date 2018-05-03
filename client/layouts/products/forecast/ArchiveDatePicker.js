d4121bcd0cbfe064409a3c658bedbbd07d802cb4
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-05-03 10:44:02 -0700

    [COMPONENTS/ArchiveDatePicker] Rename style class name

--- client/layouts/products/forecast/ArchiveDatePicker.js
+++ client/layouts/products/forecast/ArchiveDatePicker.js
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
-import styles from './ArchiveDatePicker.css'
-import { DateElement } from 'components/time'
 import format from 'date-fns/format'
+import { Link } from 'react-router-dom'
+import { DateElement } from 'components/time'
 import { DayPicker } from 'components/controls'
+import styles from './ArchiveDatePicker.css'
 
 // TODO: Move to another location, so it can be used between components.
 
@@ -22,13 +22,13 @@ export default class ArchiveDatePicker extends PureComponent {
         const { date } = this.state
 
         return (
-            <div className={styles.ArchiveDatePicker}>
+            <div className={styles.Container}>
                 <DayPicker date={date} onChange={this.setDate}>
                     {date ? <DateElement value={date} /> : 'Select a date'}
                 </DayPicker>
                 {date && (
                     <Link
-                        className={styles['ArchiveDatePicker--Link']}
+                        className={styles.Link}
                         to={`/forecasts/archives/${region}/${format(
                             date,
                             'YYYY-MM-DD'
