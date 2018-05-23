30eb26de6c53da91259a3b99b4755b1cb4bdefee
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-05-23 12:17:28 -0700

    [COMPONENT/FORM/SEARCH] Add search icon

--- client/components/form/Search.js
+++ client/components/form/Search.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import debounce from 'lodash/debounce'
+import * as Icons from 'components/icons'
 import { Close } from 'components/button'
 import { Control } from 'components/form'
+import { GRAY_LIGHT } from 'constants/colors'
 
 export default class Search extends PureComponent {
     static propTypes = {
@@ -24,9 +26,12 @@ export default class Search extends PureComponent {
         const value = ''
 
         this.setState({ value }, () => {
-            this.input.focus()
+            this.focus()
             this.props.onChange(value)
         })
+    }
+    focus = () => {
+        this.input.focus()
     }
     handleChange = event => {
         const { value } = event.target
@@ -38,11 +43,14 @@ export default class Search extends PureComponent {
 
         return (
             <Control horizontal bordered>
+                <i style={SEARCH_ICON_STYLE} onClick={this.focus}>
+                    <Icons.Search color={GRAY_LIGHT} />
+                </i>
                 <input
                     ref={this.setRef}
                     name="search"
                     type="search"
-                    placeholder="Search for a definition"
+                    placeholder={this.props.placeholder}
                     value={value}
                     onChange={this.handleChange}
                 />
@@ -50,4 +58,9 @@ export default class Search extends PureComponent {
             </Control>
         )
     }
+}
+
+// Constants
+const SEARCH_ICON_STYLE = {
+    marginLeft: 10,
 }
--- client/constants/colors.js
+++ client/constants/colors.js
@@ -1,5 +1,7 @@
 export const PRIMARY = '#245EAC'
 export const SECONDARY = '#E6252F'
+export const GRAY_LIGHT = '#CCC'
+export const GRAY_LIGHTER = '#E5E5E5'
 export const BLACK = '#0F0F0F'
 export const WHITE = '#FFFFFF'
 export default PRIMARY
