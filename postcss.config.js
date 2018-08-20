e9f77f3b275aea53a956bb4fe1c68a6bac2d2b9d
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-08-20 16:34:15 -0700

    [CSS] Fix some warning w/ missing color and gray function

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('postcss-custom-properties'),
+        require('postcss-color-function'),
         require('postcss-cssnext'),
     ],
 }
