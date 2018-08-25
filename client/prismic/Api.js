ca35b85ab1e323e44899a5fe937fdda99b3ccfb4
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-08-24 23:05:08 -0700

    Use fetch for prismic

--- client/prismic/Api.js
+++ client/prismic/Api.js
@@ -1,39 +1,9 @@
 import { Predicates } from 'prismic'
-import Axios from 'axios'
 import { root, version } from './config.json'
+import { status } from 'services/fetch/utils'
 
 let API = null
 let API_PROMISE = null
-
-const SEARCH = `/${version}/documents/search`
-
-const axios = Axios.create({
-    baseURL: root,
-    paramsSerializer(params) {
-        return Object.keys(params)
-            .filter(key => Boolean(params[key]))
-            .map(key => {
-                const value = params[key]
-
-                if (Array.isArray(value)) {
-                    const content = value.map(encodeURIComponent).join(',')
-
-                    return `${key}=%5B${content}%5D`
-                } else {
-                    return `${key}=${encodeURIComponent(value)}`
-                }
-            })
-            .join('&')
-    },
-})
-
-function toQuery(predicate) {
-    return Predicates.toQuery(predicate)
-}
-
-function getData(response) {
-    return response.data
-}
 
 function getApi() {
     if (API) {
@@ -44,7 +14,7 @@ function getApi() {
         return API_PROMISE
     }
 
-    API_PROMISE = axios.get(version).then(getData)
+    API_PROMISE = fetch(`${root}/${version}`).then(status)
 
     return API_PROMISE.then(api => {
         API = api
@@ -53,19 +23,18 @@ function getApi() {
     })
 }
 
-function query(api, options = {}, predicates) {
-    const [{ ref }] = api.refs
+async function query(api, { orderings = [], ...options } = {}, predicates) {
+    const { ref } = api.refs.find(ref => ref.isMasterRef)
+    const params = serializeParams({
+        page: 1,
+        ...options,
+        q: `[${predicates.map(Predicates.toQuery).join('')}]`,
+        ref,
+    })
+    const url = `${root}/${version}/documents/search?${params}`
+    const response = await fetch(url)
 
-    options = {
-        params: {
-            page: 1,
-            ...options,
-            q: `[${predicates.map(toQuery).join('')}]`,
-            ref,
-        },
-    }
-
-    return axios.get(SEARCH, options).then(getData)
+    return await status(response)
 }
 
 export async function Query(predicates, options) {
@@ -125,5 +94,19 @@ export async function tags(type) {
     return tags
 }
 
-// Constants
+// Constants and utils
 const MAX_PAGE_SIZE = 100
+function serializeParams(params) {
+    return Object.entries(params)
+        .filter(tuple => Boolean(tuple[1]))
+        .map(([key, value]) => {
+            if (Array.isArray(value)) {
+                const content = value.map(encodeURIComponent).join(',')
+
+                return `${key}=[${content}]`
+            } else {
+                return `${key}=${encodeURIComponent(value)}`
+            }
+        })
+        .join('&')
+}
