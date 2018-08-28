9e41be60cc1d77aca028345f1119cf2d53beffef
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-08-28 16:05:37 -0700

    [MAP] Add context for layers

--- client/contexts/layers.js
+++ client/contexts/layers.js
@@ -1,28 +1,112 @@
 import React, { createContext, Component } from 'react'
-
-const Context = createContext({ opened: false })
+import PropTypes from 'prop-types'
+import * as LAYERS from 'constants/drawers'
+import { LocalStorage } from 'services/storage'
 
 export default class Provider extends Component {
+    static propTypes = {
+        children: PropTypes.element,
+    }
     state = {}
     get value() {
         return {
-            on: this.on,
-            off: this.off,
+            layers: this.state,
             toggle: this.toggle,
         }
     }
-    on = id => {
-        this.setState({ [id]: true })
-    }
-    off = id => {
-        this.setState({ [id]: false })
-    }
     toggle = id => {
-        // this.setState(({ opened }) => ({ opened: !opened }))
+        this.setState(state => ({
+            [id]: {
+                ...state[id],
+                visible: !state[id].visible,
+            },
+        }))
     }
     render() {
-        return <Context.Provider value={this.value} />
+        return (
+            <Context.Provider value={this.value}>
+                {this.props.children}
+            </Context.Provider>
+        )
     }
 }
 
-export const Consumer = Context.Consumer
+export class Layer extends Component {
+    static propTypes = {
+        id: PropTypes.oneOf(LAYERS.default).isRequired,
+        children: PropTypes.func.isRequired,
+    }
+    children = ({ toggle, layers }) => {
+        const { children, id } = this.props
+
+        return children({
+            ...layers[id],
+            toggle() {
+                toggle(id)
+            },
+        })
+    }
+
+    render() {
+        return <Consumer>{this.children}</Consumer>
+    }
+}
+
+export class Layers extends Component {
+    static propTypes = {
+        children: PropTypes.func.isRequired,
+    }
+    children = ({ layers }) => this.props.children(layers)
+    render() {
+        return <Consumer>{this.children}</Consumer>
+    }
+}
+
+const LAYERS_VISIBILITY = LocalStorage.create({
+    keyPrefix: 'layers-visibility',
+})
+const LAYERS_FILTERS = LocalStorage.create({
+    keyPrefix: 'layers-filters',
+})
+
+const Context = createContext({
+    [LAYERS.FORECASTS]: {
+        visible: true,
+    },
+    [LAYERS.HOT_ZONE_REPORTS]: {
+        visible: true,
+    },
+    [LAYERS.SPECIAL_INFORMATION]: {
+        visible: true,
+    },
+    [LAYERS.MOUNTAIN_INFORMATION_NETWORK]: {
+        visible: true,
+        filters: {
+            days: Number(
+                LAYERS_FILTERS.get(
+                    `${LAYERS.MOUNTAIN_INFORMATION_NETWORK}-days`,
+                    7
+                )
+            ),
+            types: new Set(),
+        },
+    },
+    [LAYERS.WEATHER_STATION]: {
+        visible: Boolean(LAYERS_VISIBILITY.get(LAYERS.WEATHER_STATION, true)),
+    },
+    [LAYERS.FATAL_ACCIDENT]: {
+        visible: Boolean(LAYERS_VISIBILITY.get(LAYERS.FATAL_ACCIDENT, false)),
+    },
+    [LAYERS.TOYOTA_TRUCK_REPORTS]: {
+        visible: Boolean(
+            LAYERS_VISIBILITY.get(LAYERS.TOYOTA_TRUCK_REPORTS, true)
+        ),
+    },
+    [LAYERS.MOUNTAIN_CONDITIONS_REPORTS]: {
+        visible: Boolean(
+            LAYERS_VISIBILITY.get(LAYERS.MOUNTAIN_CONDITIONS_REPORTS, true)
+        ),
+    },
+})
+
+const { Consumer } = Context
