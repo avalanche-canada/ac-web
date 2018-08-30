7fbcc3d6757c64daa3d49346494a3df0e79c86f3
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-08-30 04:24:52 -0700

    [MCR] Create a <Report> container

--- client/containers/mcr.js
+++ client/containers/mcr.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Fetch from 'components/fetch'
+import { Memory } from 'components/fetch/Cache'
 import * as mcr from 'api/requests/mcr'
 
 export class Report extends Component {
@@ -8,20 +9,32 @@ export class Report extends Component {
         id: PropTypes.string.isRequired,
         children: PropTypes.func.isRequired,
     }
+    children = ({ data, ...props }) => {
+        const { id } = this.props
+
+        Object.assign(props, {
+            data: Array.isArray(data)
+                ? data.find(report => report.id === id)
+                : false,
+        })
+
+        return this.props.children(props)
+    }
     render() {
-        return (
-            <Fetch request={mcr.report(this.props.id)}>
-                {this.props.children}
-            </Fetch>
-        )
+        return <Reports>{this.children}</Reports>
     }
 }
 
 export class Reports extends Component {
+    static CACHE = new Memory()
     static propTypes = {
         children: PropTypes.func.isRequired,
     }
     render() {
-        return <Fetch request={mcr.reports()}>{this.props.children}</Fetch>
+        return (
+            <Fetch cache={Reports.CACHE} request={mcr.reports()}>
+                {this.props.children}
+            </Fetch>
+        )
     }
 }
