0a8b2010dd68ce363abb9bda90f8320db952d71a
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-09-13 21:36:33 -0700

    Remove unused module

--- client/utils/redux.js
+++
@@ -1,9 +0,0 @@
-import noop from 'lodash/noop'
-
-export function createOptimisticAction(tester, action = noop) {
-    return payload => (dispatch, getState) => {
-        if (tester(getState(), payload)) {
-            return dispatch(action(payload))
-        }
-    }
-}
