b70154c3b1f05d08662f739c51d9c2624e26f2eb
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-10-03 15:53:13 -0700

    [INCIDENTS] Create containers using <Fetch>

---
+++ client/containers/incidents.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Fetch from 'components/fetch'
+import { Memory } from 'components/fetch/Cache'
+import * as requests from 'api/requests/incidents'
+
+export class Incident extends Component {
+    static propTypes = {
+        id: PropTypes.string.isRequired,
+        children: PropTypes.func.isRequired,
+    }
+    static CACHE = new Memory()
+    render() {
+        const { id, children } = this.props
+
+        return (
+            <Fetch cache={CACHE} request={requests.incident(id)}>
+                {children}
+            </Fetch>
+        )
+    }
+}
+
+export class Incidents extends Component {
+    static propTypes = {
+        children: PropTypes.func.isRequired,
+    }
+    render() {
+        const { children, ...params } = this.props
+
+        return (
+            <Fetch cache={CACHE} request={requests.incidents(params)}>
+                {children}
+            </Fetch>
+        )
+    }
+}
+
+const CACHE = new Memory()
