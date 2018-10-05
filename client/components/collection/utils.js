ef6afa337999fff6f8a4b3e13c5a60d63250081c
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-10-05 13:30:57 -0700

    [COLLECTION] Fix children renderer by testing against a function instead

--- client/components/collection/utils.js
+++ client/components/collection/utils.js
@@ -1,7 +1,7 @@
-import { cloneElement, isValidElement } from 'react'
+import { cloneElement } from 'react'
 
 export function renderChildren(children, values) {
-    return isValidElement(children)
-        ? cloneElement(children, { values })
-        : children(values)
+    return typeof children === 'function'
+        ? children(values)
+        : cloneElement(children, { values })
 }
