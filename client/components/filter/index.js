0de9a1a76ba6556aa1f3cc55a213d2d5eaae2f19
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-10-05 13:45:43 -0700

    [COMPONENTS/FILTER] Move all components to the index

--- client/components/filter/index.js
+++ client/components/filter/index.js
@@ -1,2 +1,18 @@
-export FilterSet from './Set'
-export FilterEntry from './Entry'
+import React from 'react'
+import styles from './Filter.css'
+
+export function FilterSet({ children, ...props }) {
+    return (
+        <div {...props} className={styles.Set}>
+            {children}
+        </div>
+    )
+}
+
+export function FilterEntry({ children, ...props }) {
+    return (
+        <div {...props} className={styles.Entry}>
+            {children}
+        </div>
+    )
+}
