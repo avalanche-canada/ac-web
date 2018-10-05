6b97f0a6f15e4c986cbec2cb61ed508d18d5e7c0
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-10-05 15:13:18 -0700

    [COMPONENTS/MEDIA] Combine components into one module

--- client/components/media/index.js
+++ client/components/media/index.js
@@ -1,2 +1,27 @@
-export Media from './Media'
-export Caption from './Caption'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Media.css'
+import classnames from 'classnames'
+
+Media.propTypes = {
+    caption: PropTypes.node,
+    className: PropTypes.string,
+    children: PropTypes.node,
+}
+
+export function Media({ caption, className, children }) {
+    return (
+        <figure className={classnames(styles.Container, className)}>
+            {children}
+            {caption}
+        </figure>
+    )
+}
+
+Caption.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+export function Caption({ children }) {
+    return <figcaption className={styles.Caption}>{children}</figcaption>
+}
