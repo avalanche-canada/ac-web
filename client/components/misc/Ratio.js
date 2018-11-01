ea540d8e1b5c98e3acbca82b5091c80cd579599b
Karl Guillotte <karl.guillotte.1@gmail.com>
2018-10-31 17:15:58 -0700

    [COMPONENTS/RATIO] Improve code + make it compatible with React concurent

--- client/components/misc/Ratio.js
+++ client/components/misc/Ratio.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component, createRef } from 'react'
 import PropTypes from 'prop-types'
 import debounce from 'lodash/debounce'
 
@@ -7,69 +7,46 @@ export default class Ratio extends Component {
         x: PropTypes.number,
         y: PropTypes.number,
         children: PropTypes.func.isRequired,
-        traverse: PropTypes.bool,
     }
     static defaultProps = {
         x: 16,
         y: 9,
-        traverse: false,
     }
     state = {
         width: 0,
         height: 0,
     }
-    getComputedDimensions(props) {
-        return this.computeDimensionsElement(this.container, props)
-    }
-    computeDimensionsElement(element, props) {
-        const { x, y, traverse } = props
-        const { width } = element.getBoundingClientRect()
-
-        if (traverse === true && width === 0) {
-            return this.computeDimensionsElement(element.parentNode, props)
+    ref = createRef()
+    computeDimensionsElement(element = this.ref?.current) {
+        if (!element) {
+            return this.state
         }
+
+        const { x, y } = this.props
+        const { width } = element.getBoundingClientRect()
 
         return {
             width: Math.round(width),
             height: Math.round(width * (y / x)),
         }
     }
-    componentWillReceiveProps(next) {
-        this.setState(this.getComputedDimensions(next))
-    }
     componentDidMount() {
-        this.setState({
-            ...this.getComputedDimensions(this.props),
-            hasComputed: true,
-        })
+        this.setState(this.computeDimensionsElement())
 
-        window.addEventListener('resize', this.handleResize, false)
+        window.addEventListener('resize', this.handleResize)
+    }
+    componentDidUpdate({ x, y }) {
+        if (x !== this.props.x || y !== this.props.y) {
+            this.setState(this.computeDimensionsElement())
+        }
     }
     componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize, false)
+        window.removeEventListener('resize', this.handleResize)
     }
     handleResize = debounce(() => {
-        this.setState(
-            {
-                hasComputed: false,
-            },
-            () => {
-                this.setState({
-                    hasComputed: true,
-                    ...this.getComputedDimensions(this.props),
-                })
-            }
-        )
+        this.setState(this.computeDimensionsElement())
     }, 100)
     render() {
-        return (
-            <div ref={ref => (this.container = ref)}>
-                {this.props.children(
-                    this.state.width,
-                    this.state.height,
-                    this.state.hasComputed
-                )}
-            </div>
-        )
+        return this.props.children(this.ref, this.state)
     }
 }
