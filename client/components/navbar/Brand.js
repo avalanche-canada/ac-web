cd91f1bbb7842c82c6da5f9687ff5f960a1aea69
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-11-02 15:18:26 -0700

    [PERFORMANCE] Stop rerendering navbar components

--- client/components/navbar/Brand.js
+++ client/components/navbar/Brand.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { memo } from 'react'
 import styles from './Navbar.css'
 import { Link } from '@reach/router'
 
-export default function Brand({ children, ...props }) {
+function Brand({ children, ...props }) {
+    console.warn('render Brand')
     return (
         <Link className={styles.Brand} {...props}>
             {children}
         </Link>
     )
 }
+
+export default memo(Brand, () => true)
--- client/components/navbar/Burger.js
+++ client/components/navbar/Burger.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import StaticComponent from 'components/StaticComponent'
 import { Menu } from 'components/icons'
 import Button, { INCOGNITO } from '../button'
 import styles from './Navbar.css'
 
-export default class Burger extends StaticComponent {
-    static propTypes = {
-        onClick: PropTypes.func.isRequired,
-    }
-    render() {
-        return (
-            <div className={styles['Burger--Container']}>
-                <Button
-                    kind={INCOGNITO}
-                    className={styles.Burger}
-                    onClick={this.props.onClick}>
-                    <Menu width={32} height={32} />
-                </Button>
-            </div>
-        )
-    }
+Burger.propTypes = {
+    onClick: PropTypes.func.isRequired,
 }
+
+function Burger({ onClick }) {
+    return (
+        <div className={styles['Burger--Container']}>
+            <Button
+                kind={INCOGNITO}
+                className={styles.Burger}
+                onClick={onClick}>
+                <Menu width={32} height={32} />
+            </Button>
+        </div>
+    )
+}
+
+export default memo(Burger, () => true)
