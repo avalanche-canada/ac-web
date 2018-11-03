e7b8d3696ed287ca8d55ed5f91a0d8c2eb46b438
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-11-03 11:33:00 -0700

    Update client/components/button/Locate.js

--- client/components/button/Locate.js
+++ client/components/button/Locate.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import { memo } from 'utils/react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import Button from './Button'
 import { MyLocation } from 'components/icons'
 import { SUBTILE } from './kinds'
 
-function Locate(props) {
-    return (
-        <Button kind={SUBTILE} {...props}>
-            <MyLocation />
-        </Button>
-    )
+export default class Locate extends PureComponent {
+    static propTypes = {
+        ref: PropTypes.func.isRequired,
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        return false
+    }
+    render() {
+        return (
+            <Button kind={SUBTILE} {...this.props}>
+                <MyLocation />
+            </Button>
+        )
+    }
 }
-
-export default memo.static(Locate)
