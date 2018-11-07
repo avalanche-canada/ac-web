2bdbbde46280d7f5569d5c4996913cdf39228daf
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-11-07 15:51:57 -0800

    Generate random map icon

--- client/layouts/main/layers/ForecastMarkers.js
+++ client/layouts/main/layers/ForecastMarkers.js
@@ -6,6 +6,27 @@ import { Layer } from 'contexts/layers'
 import { Marker } from 'components/map'
 import { FORECASTS } from 'constants/drawers'
 
+const ICONS = new Map([
+    ['Jasper National Park', createRatings()],
+    ['South Columbia', createRatings()],
+    ['Purcells', createRatings()],
+    ['Lizard Range and Flathead', createRatings()],
+    ['Cariboos', createRatings()],
+    ['South Coast Inland', createRatings()],
+    ['Northwest Coastal', createRatings()],
+    ['Northwest Inland', createRatings()],
+    ['North Columbia', createRatings()],
+    ['Sea To Sky', createRatings()],
+    ['Kootenay Boundary', createRatings()],
+    ['Glacier National Park', createRatings()],
+    ['South Rockies', createRatings()],
+    ['Little Yoho', createRatings()],
+    ['South Coast', createRatings()],
+    ['Waterton Lakes National Park', createRatings()],
+    ['Banff Yoho & Kootenay National Park', createRatings()],
+    ['Kananaskis Country, Alberta Parks', createRatings()],
+])
+
 export default class ForecastMarkers extends Component {
     static propTypes = {
         map: PropTypes.object, // actually isRequired
@@ -34,8 +55,14 @@ function createMarkerProps({ id, name, dangerIconUrl, centroid }) {
 
     element.classList.add('map-marker')
 
+    const src = ICONS.has(name)
+        ? `/api/forecasts/graphics/${ICONS.get(name).join(
+              '/'
+          )}/danger-rating-icon.svg`
+        : dangerIconUrl
+
     Object.assign(element, {
-        src: dangerIconUrl,
+        src,
         width: 50,
         height: 50,
         alt: name,
@@ -51,4 +78,13 @@ function createMarkerProps({ id, name, dangerIconUrl, centroid }) {
             onMarkerClick(id)
         },
     }
+}
+function getRandomRating(min = 1, max = 5) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min)) + min
+}
+function createRatings() {
+    return [getRandomRating(3, 5), getRandomRating(2, 4), getRandomRating(1, 3)]
 }
