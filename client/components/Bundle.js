e1653b4f28fc0d6dd692250919402fff8f87d7d6
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-11-08 12:01:26 -0800

    Fix Bundle error handling

--- client/components/Bundle.js
+++ client/components/Bundle.js
@@ -1,55 +1,37 @@
-import React, { Component, Suspense } from 'react'
+import React, { Suspense } from 'react'
 import PropTypes from 'prop-types'
-import { SessionStorage } from 'services/storage'
 import ErrorBoundary from 'components/ErrorBoundary'
 import { Loading, Error } from 'components/text'
+import Button from 'components/button'
 
-// TODO: HOOKS
-
-export default class Bundle extends Component {
-    static propTypes = {
-        children: PropTypes.element.isRequired,
-        fallback: PropTypes.element.isRequired,
-    }
-    static defaultProps = {
-        fallback: <Loading />,
-    }
-    get ref() {
-        return STORAGE.get(KEY)
-    }
-    set ref(ref) {
-        STORAGE.set(KEY, ref)
-    }
-    componentDidMount() {
-        if (this.ref !== window.location.href) {
-            this.ref = null
-        }
-    }
-    handleError({ error }) {
-        if (error instanceof SyntaxError && this.ref === null) {
-            this.ref = window.location.href
-
-            window.location.reload(true)
-
-            return null
-        } else {
-            throw error
-        }
-    }
-    render() {
-        const { fallback, children } = this.props
-
-        // TODO: Finish that!!!
-
-        return (
-            <ErrorBoundary fallback={<Error />}>
-                <Suspense fallback={fallback}>{children}</Suspense>
-            </ErrorBoundary>
-        )
-    }
+Bundle.propTypes = {
+    children: PropTypes.element.isRequired,
+    fallback: PropTypes.element.isRequired,
 }
 
-const STORAGE = SessionStorage.create()
-const KEY = 'bundle.reloaded'
+export default function Bundle({ children, fallback = <Loading /> }) {
+    return (
+        <ErrorBoundary fallback={renderError}>
+            <Suspense fallback={fallback}>{children}</Suspense>
+        </ErrorBoundary>
+    )
+}
 
-function renderError({ error }) {}
+function renderError({ error }) {
+    if (error instanceof SyntaxError) {
+        function reload() {
+            window.location.reload(true)
+        }
+
+        return (
+            <div>
+                <Error>
+                    An error happened while loading script. {error.message}
+                </Error>
+                <Button onClick={reload}>Reload the page</Button>
+            </div>
+        )
+    }
+
+    throw error
+}
