a54e74dafe83faf5f2bd74761a4f30c5743418e4
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-04 14:43:34 -0800

    [WEATHER/STATION] Handle not found weather station properly instead of crashing

--- client/layouts/weather/station/WeatherStation.js
+++ client/layouts/weather/station/WeatherStation.js
@@ -1,8 +1,16 @@
 import React, { PureComponent, Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { Page, Content, Header, Main } from 'components/page'
+import {
+    List,
+    ListItem,
+    Page,
+    Content,
+    Header,
+    Main,
+    Headline,
+} from 'components/page'
 import { Metadata, Station, Footer } from 'components/weather/station'
-import { Error, Muted } from 'components/text'
+import { Error, Muted, Loading } from 'components/text'
 import ErrorBoundary from 'components/ErrorBoundary'
 import { Fulfilled, Pending } from 'components/fetch'
 import * as containers from 'containers/weather'
@@ -19,7 +27,7 @@ export default class WeatherStation extends PureComponent {
         )
     }
     renderStation = station => {
-        return (
+        return station ? (
             <Fragment>
                 <Metadata {...station} />
                 <containers.Measurements id={this.props.id}>
@@ -27,10 +35,43 @@ export default class WeatherStation extends PureComponent {
                 </containers.Measurements>
                 <Footer />
             </Fragment>
+        ) : (
+            <Fragment>
+                <Headline>
+                    Weather station #{this.props.id} does not exist. Click on a
+                    link below to see another weather station.
+                </Headline>
+                <containers.Stations>
+                    {() => (
+                        <Fragment>
+                            <Pending>
+                                <Loading>
+                                    Loading all weather stations...
+                                </Loading>
+                            </Pending>
+                            <Fulfilled>
+                                {data => (
+                                    <List>
+                                        {data.map(({ stationId, name }) => {
+                                            return (
+                                                <ListItem
+                                                    key={stationId}
+                                                    to={`/weather/stations/${stationId}`}>
+                                                    {name}
+                                                </ListItem>
+                                            )
+                                        })}
+                                    </List>
+                                )}
+                            </Fulfilled>
+                        </Fragment>
+                    )}
+                </containers.Stations>
+            </Fragment>
         )
     }
     getTitle({ loading, data }) {
-        return loading ? 'Loading...' : data?.name || 'No station'
+        return loading ? 'Loading...' : data?.name || 'No weather station'
     }
     children = props => (
         <Fragment>
