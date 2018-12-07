ed9412549fdd64091fecb9364bd47d14e4b752f1
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-07 11:15:03 -0800

    [FORECAST/PROBLEMS] Add a dot to the placeholder

--- client/layouts/products/forecast/problem/index.js
+++ client/layouts/products/forecast/problem/index.js
@@ -12,7 +12,7 @@ ProblemSet.propTypes = {
 
 export default function ProblemSet({ problems }) {
     if (problems.length === 0) {
-        return <h3>No problems identified</h3>
+        return <h3>No problems identified.</h3>
     }
 
     return <Fragment>{problems.map(renderProblem)}</Fragment>
