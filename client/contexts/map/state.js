99007bdc94aeff94e9e25a86a4cb367a014047d6
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-11 08:27:28 -0800

    [MAP] Simplify <MapState> implementation

--- client/contexts/map/state.js
+++ client/contexts/map/state.js
@@ -2,7 +2,7 @@ import React, { Component, createContext } from 'react'
 import PropTypes from 'prop-types'
 import { SessionStorage } from 'services/storage'
 
-// TODO HOOKS
+// TODO USE HOOKS
 
 const MapStateContext = createContext()
 
@@ -18,19 +18,9 @@ export class Provider extends Component {
         this.state = {
             zoom: STORAGE.get('zoom', ZOOM),
             center: STORAGE.get('center', CENTER),
-            setZoom: this.setZoom,
-            setCenter: this.setCenter,
+            setZoom: set.bind(this, 'zoom'),
+            setCenter: set.bind(this, 'center'),
         }
-    }
-    setZoom = zoom => {
-        this.setState({ zoom })
-
-        STORAGE.set('zoom', zoom)
-    }
-    setCenter = center => {
-        this.setState({ center })
-
-        STORAGE.set('center', center)
     }
     render() {
         return (
@@ -43,6 +33,14 @@ export class Provider extends Component {
 
 export const Consumer = MapStateContext.Consumer
 
+// Constants
 const STORAGE = SessionStorage.create({ keyPrefix: 'map:state' })
 const CENTER = [-125.15, 54.8]
 const ZOOM = 4.3
+
+// Utils
+function set(key, value) {
+    this.setState({ [key]: value })
+
+    STORAGE.set(key, value)
+}
