3fe33827de1d0513c3b2631a058126a63f552094
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-18 02:31:09 -0800

    Use <StaticComponent>

--- client/components/map/Control.js
+++ client/components/map/Control.js
@@ -1,5 +1,5 @@
-import { Component } from 'react'
 import PropTypes from 'prop-types'
+import StaticComponent from 'components/StaticComponent'
 
 export const TOP_LEFT = 'top-left'
 export const TOP_RIGHT = 'top-right'
@@ -8,22 +8,17 @@ export const BOTTOM_RIGHT = 'bottom-right'
 
 // TODO: HOOKS
 
-export default class Control extends Component {
+export default class Control extends StaticComponent {
     static propTypes = {
-        map: PropTypes.object, // actually isRequired
+        map: PropTypes.object,
         factory: PropTypes.func.isRequired,
-        position: PropTypes.oneOf([
-            TOP_LEFT,
-            TOP_RIGHT,
-            BOTTOM_LEFT,
-            BOTTOM_RIGHT,
-        ]),
+        position: PropTypes.oneOf(
+            // actually isRequired
+            [TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT]
+        ),
     }
     static defaultProps = {
         position: BOTTOM_RIGHT,
-    }
-    shouldComponentUpdate(nextProps, nextState) {
-        return false
     }
     componentDidMount() {
         const { position, factory, map } = this.props
