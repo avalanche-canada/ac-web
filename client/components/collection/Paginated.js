474b49ee63c0c4315e2b7ef791918e8e4ea18c37
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-18 02:46:20 -0800

    Update Paginated.js
    Convert to functional component

--- client/components/collection/Paginated.js
+++ client/components/collection/Paginated.js
@@ -1,24 +1,20 @@
-import { PureComponent } from 'react'
+import { memo } from 'react'
 import PropTypes from 'prop-types'
 import { renderChildren } from './utils'
 
-export default class PaginatedCollection extends PureComponent {
-    static propTypes = {
-        values: PropTypes.array,
-        page: PropTypes.number,
-        pageSize: PropTypes.number,
-        children: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
-            .isRequired,
-    }
-    static defaultProps = {
-        page: 1,
-        pageSize: 25,
-    }
-    render() {
-        const { children, values, page, pageSize } = this.props
-        const begin = (page - 1) * pageSize
-        const end = begin + pageSize
-
-        return renderChildren(children, values.slice(begin, end))
-    }
+PaginatedCollection.propTypes = {
+    values: PropTypes.array,
+    page: PropTypes.number,
+    pageSize: PropTypes.number,
+    children: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
+        .isRequired,
 }
+
+function PaginatedCollection({ children, values, page = 1, pageSize = 25 }) {
+    const begin = (page - 1) * pageSize
+    const end = begin + pageSize
+
+    return renderChildren(children, values.slice(begin, end))
+}
+
+export default memo(PaginatedCollection)
