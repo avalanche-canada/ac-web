93e6bababd56b2217af7bb5e28a8ef6bf393cd04
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-18 21:49:42 -0800

    Update Pagination.js
    Convert to functional component

--- client/layouts/ast/tables/Pagination.js
+++ client/layouts/ast/tables/Pagination.js
@@ -1,23 +1,18 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import Base from 'components/pagination'
 
-export default class Pagination extends PureComponent {
-    static propTypes = {
-        page: PropTypes.number.isRequired,
-        pageSize: PropTypes.number,
-        count: PropTypes.number.isRequired,
-        onChange: PropTypes.func.isRequired,
-    }
-    static defaultProps = {
-        pageSize: 25,
-    }
-    get total() {
-        return Math.ceil(this.props.count / this.props.pageSize)
-    }
-    render() {
-        const { page, onChange } = this.props
-
-        return <Base total={this.total} active={page} onChange={onChange} />
-    }
+Pagination.propTypes = {
+    page: PropTypes.number.isRequired,
+    pageSize: PropTypes.number,
+    count: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired,
 }
+
+function Pagination({ page, count, pageSize = 25, onChange }) {
+    const total = Math.ceil(count / pageSize)
+
+    return <Base total={total} active={page} onChange={onChange} />
+}
+
+export default memo(Pagination)
