bca7e9ab359133b45c1c29785a85ff46efbbe38b
Karl Guillotte <karlguillotte@MacBook-Pro-de-Karl-2.local>
2018-12-20 18:21:46 -0800

    Update Tutorial.js
    Convert to functional component

--- client/layouts/weather/forecast/Tutorial.js
+++ client/layouts/weather/forecast/Tutorial.js
@@ -1,25 +1,27 @@
-import React, { PureComponent, Fragment } from 'react'
+import React, { memo, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Loading } from 'components/text'
 import { StructuredText } from 'prismic/components/base'
 import { Document } from 'prismic/containers'
 import { mw } from 'prismic/params'
 
-export default class Tutorial extends PureComponent {
-    static propTypes = {
-        uid: PropTypes.string.isRequired,
-    }
-    renderChildren = ({ loading, document }) => (
-        <Fragment>
-            <Loading show={loading}>Loading tutorial...</Loading>
-            {document && <StructuredText value={document.data.tutorial} />}
-        </Fragment>
-    )
-    render() {
-        return (
-            <Document {...mw.tutorial(this.props.uid)}>
-                {this.renderChildren}
-            </Document>
-        )
-    }
+Tutorial.propTypes = {
+    uid: PropTypes.string.isRequired,
 }
+
+function Tutorial({ uid }) {
+    return (
+        <Document {...mw.tutorial(uid)}>
+            {({ loading, document }) => (
+                <Fragment>
+                    <Loading show={loading}>Loading tutorial...</Loading>
+                    {document && (
+                        <StructuredText value={document.data.tutorial} />
+                    )}
+                </Fragment>
+            )}
+        </Document>
+    )
+}
+
+export default memo(Tutorial)
