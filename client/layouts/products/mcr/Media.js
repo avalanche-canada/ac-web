6e3ef8689fc7b2e2e52aaa855d9694dc2f9e03ed
karlguillotte <karl.guillotte.1@gmail.com>
2019-02-18 15:28:28 -0800

    Update Media.js
    - Convert to functionnal component

--- client/layouts/products/mcr/Media.js
+++ client/layouts/products/mcr/Media.js
@@ -1,26 +1,23 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Gallery from 'components/gallery'
 import IMAGE from 'styles/mountain-climbers-default.jpg'
 
-export default class Media extends PureComponent {
-    static propTypes = {
-        images: PropTypes.array,
-    }
-    render() {
-        const { images } = this.props
+Media.propTypes = {
+    images: PropTypes.array,
+}
 
-        if (!Array.isArray(images) || images.length === 0) {
-            return <img src={IMAGE} />
-        } else {
-            return (
-                <Gallery
-                    items={images.map(original => ({ original }))}
-                    showThumbnails={false}
-                    showBullets={images.length > 1}
-                    showPlayButton={false}
-                />
-            )
-        }
+export default function Media({ images }) {
+    if (!Array.isArray(images) || images.length === 0) {
+        return <img src={IMAGE} />
+    } else {
+        return (
+            <Gallery
+                items={images.map(original => ({ original }))}
+                showThumbnails={false}
+                showBullets={images.length > 1}
+                showPlayButton={false}
+            />
+        )
     }
 }
