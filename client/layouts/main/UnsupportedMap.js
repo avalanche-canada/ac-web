a5e1ae5a82d6602f812c09ec083e565c4101a7f4
karlguillotte <karl.guillotte.1@gmail.com>
2019-02-18 15:04:24 -0800

    Update UnsupportedMap.js
    - Convert top function component
    - Fix link to advisories

--- client/layouts/main/UnsupportedMap.js
+++ client/layouts/main/UnsupportedMap.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from '@reach/router'
 import { supported } from 'utils/mapbox'
@@ -7,68 +7,60 @@ import { Mailto } from 'components/anchors'
 import { ButtonSet } from 'components/button'
 import styles from 'components/page/Page.css'
 
-export default class UnsupportedMap extends PureComponent {
-    static propTypes = {
-        links: PropTypes.instanceOf(Map),
-        headline: PropTypes.node,
-    }
-    static defaultProps = {
-        headline: (
-            <Fragment>
-                It seems that your browser does not support the technology
-                required (WebGL for the geeks) to show forecasts, hot zones and
-                other avalanche-related information on our map.
-            </Fragment>
-        ),
-        links: new Map([
-            ['/forecasts', 'Forecast regions'],
-            ['/advisory', 'Avalanche Advisories'],
-            ['/weather/stations', 'Weather stations'],
-        ]),
-    }
-    render() {
-        const { links, headline } = this.props
-
-        return (
-            <Error>
-                <Main>
-                    <h1>Uh oh! You never thought that would happen...</h1>
-                    <Headline>
-                        {headline}
-                        <br />
-                        We suggest you{' '}
-                        <a href="//outdatedbrowser.com" target="_blank">
-                            update your browser
-                        </a>{' '}
-                        and make sure that WebGL is{' '}
-                        <a href="//get.webgl.org/" target="_blank">
-                            enabled
-                        </a>
-                        .
-                    </Headline>
-                    <p />
-                    <ButtonSet>
-                        {Array.from(links).map(([link, text]) => (
-                            <Link key={link} className={styles.Link} to={link}>
-                                {text}
-                            </Link>
-                        ))}
-                    </ButtonSet>
-                    <Headline>
-                        If you need help or have questions, do not hesitate to
-                        send us an{' '}
-                        <Mailto
-                            email="kguillotte@avalanche.ca,wharding@avalanche.ca"
-                            subject="Unsupported map"
-                            body={`\n\n\nMapBox GL supported: ${supported()}\nNavigator: ${
-                                navigator.userAgent
-                            }`}>
-                            email
-                        </Mailto>
-                        .
-                    </Headline>
-                </Main>
-            </Error>
-        )
-    }
+UnsupportedMap.propTypes = {
+    links: PropTypes.instanceOf(Map),
+    headline: PropTypes.node,
 }
+
+export default function UnsupportedMap({ headline = HEADLINE, links = LINKS }) {
+    return (
+        <Error>
+            <Main>
+                <h1>Uh oh! You never thought that would happen...</h1>
+                <Headline>
+                    {headline}
+                    <br />
+                    We suggest you{' '}
+                    <a href="//outdatedbrowser.com" target="_blank">
+                        update your browser
+                    </a>{' '}
+                    and make sure that WebGL is{' '}
+                    <a href="//get.webgl.org/" target="_blank">
+                        enabled
+                    </a>
+                    .
+                </Headline>
+                <p />
+                <ButtonSet>
+                    {Array.from(links).map(([link, text]) => (
+                        <Link key={link} className={styles.Link} to={link}>
+                            {text}
+                        </Link>
+                    ))}
+                </ButtonSet>
+                <Headline>
+                    If you need help or have questions, do not hesitate to send
+                    us an{' '}
+                    <Mailto
+                        email="kguillotte@avalanche.ca,wharding@avalanche.ca"
+                        subject="Unsupported map"
+                        body={`\n\n\nMapBox GL supported: ${supported()}\nNavigator: ${
+                            navigator.userAgent
+                        }`}>
+                        email
+                    </Mailto>
+                    .
+                </Headline>
+            </Main>
+        </Error>
+    )
+}
+
+const HEADLINE =
+    'It seems that your browser does not support the technology required (WebGL for the geeks) to show forecasts, hot zones and other avalanche - related information on our map.'
+
+const LINKS = new Map([
+    ['/forecasts', 'Forecast regions'],
+    ['/advisories', 'Avalanche Advisories'],
+    ['/weather/stations', 'Weather stations'],
+])
