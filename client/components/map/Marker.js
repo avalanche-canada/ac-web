90edb11bac0d5c6678c6758b582c1af35db9cf93
karlguillotte <karl.guillotte.1@gmail.com>
2019-03-13 11:50:39 -0700

    Convert class components to functional components

--- client/components/map/Marker.js
+++ client/components/map/Marker.js
@@ -1,50 +1,35 @@
-import { Component } from 'react'
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import mapbox from 'mapbox-gl/dist/mapbox-gl'
 
-export default class Marker extends Component {
-    static propTypes = {
-        map: PropTypes.object.isRequired,
-        lngLat: PropTypes.arrayOf(PropTypes.number).isRequired,
-        element: PropTypes.object.isRequired,
-        onClick: PropTypes.func,
-        options: PropTypes.object,
-    }
-    remove() {
-        if (this.marker) {
-            this.marker.remove()
-        }
-    }
-    createMarker() {
-        const { element, lngLat, options, onClick, map } = this.props
+Marker.propTypes = {
+    map: PropTypes.object.isRequired,
+    lngLat: PropTypes.arrayOf(PropTypes.number).isRequired,
+    element: PropTypes.object.isRequired,
+    onClick: PropTypes.func,
+    options: PropTypes.object,
+}
 
+export default function Marker(props) {
+    const { element, lngLat, options, onClick, map } = props
+
+    useEffect(() => {
         if (typeof onClick === 'function') {
             Object.assign(element, {
-                onclick: event => onClick(this.props, event),
+                onclick(event) {
+                    onClick(props, event)
+                },
             })
         }
 
-        this.marker = new mapbox.Marker(element, options)
+        const marker = new mapbox.Marker(element, options)
             .setLngLat(lngLat)
             .addTo(map)
-    }
-    componentDidMount() {
-        this.createMarker()
-    }
-    componentDidUpdate({ element, lngLat }) {
-        if (this.props.element !== element) {
-            this.remove()
-            this.createMarker()
-        }
 
-        if (this.props.lngLat !== lngLat) {
-            this.marker.setLngLat(this.props.lngLat)
+        return () => {
+            marker.remove()
         }
-    }
-    componentWillUnmount() {
-        this.remove()
-    }
-    render() {
-        return null
-    }
+    }, [element, lngLat])
+
+    return null
 }
--- client/layouts/tutorial/Question.js
+++ client/layouts/tutorial/Question.js
@@ -1,109 +1,101 @@
-import React, { Component, createRef } from 'react'
+import React, { useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Submit, Reset } from 'components/button'
 import { StructuredText } from 'prismic/components/base'
 import { Translate } from 'contexts/locale'
 import styles from './Question.css'
 
-export default class Question extends Component {
-    static propTypes = {
-        nonRepeat: PropTypes.shape({
-            question: PropTypes.string.isRequired,
-            answer: PropTypes.array.isRequired,
-        }).isRequired,
-    }
-    state = {
-        hasErrors: false,
-        userAnswer: null,
-    }
-    userAnswer = createRef()
-    handleSubmit = event => {
+Question.propTypes = {
+    nonRepeat: PropTypes.shape({
+        question: PropTypes.string.isRequired,
+        answer: PropTypes.array.isRequired,
+    }).isRequired,
+}
+
+export default function Question({ nonRepeat }) {
+    const { question, answer } = nonRepeat
+    const [marking, setMarking] = useState(false)
+    const [valid, setValid] = useState(true)
+    const input = useRef(null)
+    function handleSubmit(event) {
         event.preventDefault()
 
         const isValid = event.target.checkValidity()
 
-        this.setState({ hasErrors: !isValid })
-
         if (isValid) {
-            this.setState({
-                userAnswer: this.userAnswer.current.value,
-            })
+            setMarking(true)
         }
-    }
-    handleReset = () => {
-        this.setState({
-            userAnswer: null,
-            hasErrors: false,
-        })
-    }
-    get allAnswers() {
-        const { answer, question } = this.props.nonRepeat
 
-        return (
-            <section className={styles.AllAnswers}>
-                <header>{question}</header>
-                <dl>
-                    <div>
-                        <dt>
-                            <Translate>Our answer</Translate>
-                        </dt>
-                        <dd>
-                            <StructuredText value={answer} />
-                        </dd>
-                    </div>
-                    <div>
-                        <dt>
-                            <Translate>Your answer</Translate>
-                        </dt>
-                        <dd>
-                            <p>{this.state.userAnswer}</p>
-                        </dd>
-                    </div>
-                </dl>
-            </section>
-        )
+        setValid(isValid)
     }
-    get form() {
-        const { question } = this.props.nonRepeat
+    function handleReset() {
+        setMarking(false)
+    }
+    function handleChange() {
+        setValid(true)
+    }
 
-        return (
-            <label>
-                {question}{' '}
-                <textarea
-                    name="user-answer"
-                    ref={this.userAnswer}
-                    autoFocus
-                    required
-                />
-                <span data-error-message>
-                    <Translate>Your answer is required.</Translate>
-                </span>
-            </label>
-        )
-    }
-    render() {
-        const { userAnswer } = this.state
+    return (
+        <form
+            onSubmit={handleSubmit}
+            onReset={handleReset}
+            data-has-errors={!valid}
+            noValidate>
+            <fieldset>
+                <legend>Question</legend>
+                <label>
+                    {question}{' '}
+                    <textarea
+                        name="user-answer"
+                        ref={input}
+                        autoFocus
+                        required
+                        onChange={handleChange}
+                        style={marking ? { display: 'none' } : null}
+                    />
+                    <span data-error-message>
+                        <Translate>Your answer is required.</Translate>
+                    </span>
+                </label>
+                {marking && (
+                    <Answers answer={answer} user={input.current.value} />
+                )}
+                {marking ? (
+                    <Reset className={styles.Button}>
+                        <Translate>Give another answer</Translate>
+                    </Reset>
+                ) : (
+                    <Submit className={styles.Button}>
+                        <Translate>See answers</Translate>
+                    </Submit>
+                )}
+            </fieldset>
+        </form>
+    )
+}
 
-        return (
-            <form
-                onSubmit={this.handleSubmit}
-                onReset={this.handleReset}
-                data-has-errors={this.state.hasErrors}
-                noValidate>
-                <fieldset>
-                    <legend>Question</legend>
-                    {userAnswer ? this.allAnswers : this.form}
-                    {userAnswer ? (
-                        <Reset className={styles.Button}>
-                            <Translate>Give another answer</Translate>
-                        </Reset>
-                    ) : (
-                        <Submit className={styles.Button}>
-                            <Translate>See answers</Translate>
-                        </Submit>
-                    )}
-                </fieldset>
-            </form>
-        )
-    }
+// Utils
+function Answers({ answer, user }) {
+    return (
+        <section className={styles.Answers}>
+            <dl>
+                <div>
+                    <dt>
+                        <Translate>Our answer</Translate>
+                    </dt>
+                    <dd>
+                        <StructuredText value={answer} />
+                    </dd>
+                </div>
+                <div>
+                    <dt>
+                        <Translate>Your answer</Translate>
+                    </dt>
+                    <dd>
+                        <p>{user}</p>
+                    </dd>
+                </div>
+            </dl>
+        </section>
+    )
 }
