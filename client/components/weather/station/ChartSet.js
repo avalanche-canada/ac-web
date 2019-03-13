588cf70a0acbee784321e380b284c24d3eb29ea3
karlguillotte <karl.guillotte.1@gmail.com>
2019-03-13 15:12:42 -0700

    [WX] [AUTH] Convert ChartSet into a functional component

--- client/components/weather/station/ChartSet.js
+++ client/components/weather/station/ChartSet.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import RelativeHumidity from './charts/RelativeHumidity'
 import Snow from './charts/Snow'
@@ -10,61 +10,59 @@ import ErrorBoundary from 'components/ErrorBoundary'
 import { Error } from 'components/text'
 import styles from './Station.css'
 
+ChartSet.propTypes = {
+    measurements: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default function ChartSet({ measurements }) {
+    return (
+        <Ratio>
+            {(ref, { width, height }) => {
+                const { min, max } = getDateExtent(measurements)
+                const fallback = (
+                    <Error>An error happened while showing charts.</Error>
+                )
+
+                height = Math.max(MIN_HEIGHT, height)
+
+                return (
+                    <ErrorBoundary fallback={fallback}>
+                        <div ref={ref} className={styles.ChartSet}>
+                            <Snow
+                                data={measurements}
+                                min={min}
+                                max={max}
+                                width={width}
+                                height={height}
+                            />
+                            <Temperature
+                                data={measurements}
+                                min={min}
+                                max={max}
+                                width={width}
+                                height={height}
+                            />
+                            <Wind
+                                data={measurements}
+                                min={min}
+                                max={max}
+                                width={width}
+                                height={height}
+                            />
+                            <RelativeHumidity
+                                data={measurements}
+                                min={min}
+                                max={max}
+                                width={width}
+                                height={height}
+                            />
+                        </div>
+                    </ErrorBoundary>
+                )
+            }}
+        </Ratio>
+    )
+}
+
+// Constants
 const MIN_HEIGHT = 200
-
-// Needs to be a class Component, ref is required
-// TODO: HOOKS
-
-export default class ChartSet extends Component {
-    static propTypes = {
-        measurements: PropTypes.arrayOf(PropTypes.object).isRequired,
-    }
-    renderCharSet = (ref, { width, height }) => {
-        const { measurements } = this.props
-        const { min, max } = getDateExtent(measurements)
-
-        height = Math.max(MIN_HEIGHT, height)
-
-        return (
-            <ErrorBoundary fallback={renderError}>
-                <div ref={ref} className={styles.ChartSet}>
-                    <Snow
-                        data={measurements}
-                        min={min}
-                        max={max}
-                        width={width}
-                        height={height}
-                    />
-                    <Temperature
-                        data={measurements}
-                        min={min}
-                        max={max}
-                        width={width}
-                        height={height}
-                    />
-                    <Wind
-                        data={measurements}
-                        min={min}
-                        max={max}
-                        width={width}
-                        height={height}
-                    />
-                    <RelativeHumidity
-                        data={measurements}
-                        min={min}
-                        max={max}
-                        width={width}
-                        height={height}
-                    />
-                </div>
-            </ErrorBoundary>
-        )
-    }
-    render() {
-        return <Ratio>{this.renderCharSet}</Ratio>
-    }
-}
-
-function renderError() {
-    return <Error>An error happened while showing charts.</Error>
-}
