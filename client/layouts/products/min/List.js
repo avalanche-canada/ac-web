cbb8ec4482cab7269f6de452a61a6cb3106107bb
karlguillotte <karl.guillotte.1@gmail.com>
2019-03-21 21:32:48 -0700

    [MIN] Convert <Entry> (part of the <List>) to functional component

--- client/layouts/products/min/List.js
+++ client/layouts/products/min/List.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment, Children } from 'react'
+import React, { Fragment, Children } from 'react'
 import PropTypes from 'prop-types'
 import Section from './Section'
 import { List as Base, Term, Definition } from 'components/description'
@@ -27,61 +27,61 @@ export default function List({ title = 'Information', children }) {
     )
 }
 
-export class Entry extends PureComponent {
-    static children(value) {
-        switch (typeof value) {
-            case 'string':
-            case 'number':
-                return value
-            case 'boolean':
-                return BooleanValues.get(value)
-            case 'function':
-                return value(this)
-            case 'object': {
-                if (Array.isArray(value)) {
-                    if (value.length === 0) {
-                        return null
-                    } else {
-                        return (
-                            <ul>
-                                {value.map((value, index) => (
-                                    <li key={index}>{Entry.children(value)}</li>
-                                ))}
-                            </ul>
-                        )
-                    }
-                } else if (value instanceof Date) {
-                    return <DateTime value={value} />
+Entry.propTypes = {
+    term: PropTypes.string.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.instanceOf(Date),
+        PropTypes.array,
+        PropTypes.object,
+    ]),
+}
+
+export function Entry({ term, children }) {
+    if (children == null) {
+        return null
+    }
+
+    return (
+        <Fragment>
+            <Term>{term}</Term>
+            <Definition>
+                <Value value={children} />
+            </Definition>
+        </Fragment>
+    )
+}
+
+function Value({ value }) {
+    switch (typeof value) {
+        case 'string':
+        case 'number':
+            return value
+        case 'boolean':
+            return value ? 'Yes' : 'No'
+        case 'function':
+            return value(this)
+        case 'object': {
+            if (Array.isArray(value)) {
+                if (value.length === 0) {
+                    return null
                 } else {
-                    return Entry.children(trulyKeys(value))
+                    return (
+                        <ul>
+                            {value.map((value, index) => (
+                                <li key={index}>
+                                    <Value value={value} />
+                                </li>
+                            ))}
+                        </ul>
+                    )
                 }
+            } else if (value instanceof Date) {
+                return <DateTime value={value} />
+            } else {
+                return <Value value={trulyKeys(value)} />
             }
         }
     }
-    static propTypes = {
-        term: PropTypes.string.isRequired,
-        children: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number,
-            PropTypes.instanceOf(Date),
-            PropTypes.array,
-            PropTypes.object,
-        ]).isRequired,
-    }
-    render() {
-        const { term, children } = this.props
-
-        if (children == null) {
-            return null
-        }
-
-        return (
-            <Fragment>
-                <Term>{term}</Term>
-                <Definition>{Entry.children(children)}</Definition>
-            </Fragment>
-        )
-    }
 }
-
-const BooleanValues = new Map([[true, 'Yes'], [false, 'No']])
