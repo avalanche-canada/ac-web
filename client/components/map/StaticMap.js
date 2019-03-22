f5f0d3d9771b20a4a76b1309d2bcb5f60edffb00
karlguillotte <karl.guillotte.1@gmail.com>
2019-03-21 18:56:00 -0700

    [STATICMAP] Convert to functional component + fix issue w/ url

--- client/components/map/StaticMap.js
+++ client/components/map/StaticMap.js
@@ -1,85 +1,63 @@
-import React, { PureComponent, Children } from 'react'
+import React, { useState, useRef, useEffect, Children } from 'react'
 import PropTypes from 'prop-types'
-import { createStyleUrl } from 'services/mapbox/api'
-import Marker from './Marker'
 import debounce from 'lodash/debounce'
+import { createStyleUrl } from 'services/mapbox/api'
+import { PRIMARY } from 'constants/colors'
+import Marker from './Marker'
 
-export default class StaticMap extends PureComponent {
-    static propTypes = {
-        tracked: PropTypes.bool,
-        styleId: PropTypes.string,
-        overlay: PropTypes.arrayOf(PropTypes.string),
-        longitude: PropTypes.number,
-        latitude: PropTypes.number,
-        zoom: PropTypes.number,
-        bearing: PropTypes.number,
-        pitch: PropTypes.number,
-        auto: PropTypes.bool,
-        width: PropTypes.number,
-        height: PropTypes.number,
-        retina: PropTypes.bool,
-    }
-    static Managed({ children, center, ...props }) {
-        return (
-            <StaticMap
-                {...props}
-                longitude={center.lng}
-                latitude={center.lat}
-                overlay={createOverlay(children)}
-            />
-        )
-    }
-    state = {
-        url: null,
-        isLoading: false,
-        isError: false,
-    }
-    setUrl = () => {
-        const { tracked, ...props } = this.props
+StaticMap.propTypes = {
+    tracked: PropTypes.bool,
+    styleId: PropTypes.string,
+    overlay: PropTypes.arrayOf(PropTypes.string),
+    longitude: PropTypes.number,
+    latitude: PropTypes.number,
+    zoom: PropTypes.number,
+    bearing: PropTypes.number,
+    pitch: PropTypes.number,
+    auto: PropTypes.bool,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    retina: PropTypes.bool,
+}
+
+StaticMap.Managed = function Managed({ children, center, ...props }) {
+    return (
+        <StaticMap
+            {...props}
+            longitude={center.lng}
+            latitude={center.lat}
+            overlay={createOverlay(children)}
+        />
+    )
+}
+
+export default function StaticMap({ tracked, ...props }) {
+    const ref = useRef(null)
+    const [url, setUrl] = useState(null)
+
+    useEffect(() => {
+        function resizeHandler() {
+            setUrl(
+                createStyleUrl({
+                    ...props,
+                    width: ref.current.clientWidth,
+                })
+            )
+        }
+        const handleResize = debounce(resizeHandler, 500)
 
         if (tracked) {
-            props.width = this.map.clientWidth
-            // props.height = this.map.clientHeight
+            window.addEventListener('resize', handleResize)
         }
 
-        this.setState({
-            url: createStyleUrl(props),
-        })
-    }
-    handleResize = debounce(this.setUrl, 250)
-    handleLoad = () => {
-        this.setState({
-            isError: false,
-            isLoading: false,
-        })
-    }
-    handleError = () => {
-        this.setState({
-            isError: true,
-            isLoading: false,
-        })
-    }
-    componentDidMount() {
-        if (this.props.tracked) {
-            window.addEventListener('resize', this.handleResize)
-        }
+        resizeHandler()
 
-        this.setUrl()
-    }
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize)
-    }
-    render() {
-        return (
-            <div ref={map => (this.map = map)}>
-                <img
-                    src={this.state.url}
-                    onLoad={this.handleLoad}
-                    onError={this.handleError}
-                />
-            </div>
-        )
-    }
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [tracked])
+
+    return <div ref={ref}>{url && <img src={url} />}</div>
 }
 
 // Utils
@@ -88,9 +66,16 @@ function createOverlay(children) {
         .filter(Boolean)
         .filter(({ type }) => type === Marker)
         .map(({ props }) => {
-            const url = encodeURIComponent(props.element.src)
             const { lng, lat } = props.lngLat
+            try {
+                const url = new URL(props.element.src)
 
-            return `url-${url}(${lng},${lat})`
+                url.protocol = 'https'
+                url.host = 'www.avalanche.ca'
+
+                return `url-${encodeURIComponent(url.href)}(${lng},${lat})`
+            } catch (error) {
+                return `pin-s+${PRIMARY.substr(1)}(${lng},${lat})`
+            }
         })
 }
