f047dbc4fc7e01190888f5a32e0b8f3d0d343505
karlguillotte <karl.guillotte.1@gmail.com>
2019-03-23 13:50:02 -0700

    Update Courses.js
    Remove unused module

--- client/containers/ast/Courses.js
+++ client/containers/ast/Courses.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import addDays from 'date-fns/add_days'
 import areRangesOverlapping from 'date-fns/are_ranges_overlapping'
