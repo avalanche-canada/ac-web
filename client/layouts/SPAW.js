ffd86f40ee76ca237e75764d2c0f2d1d61d52b96
karlguillotte <karl.guillotte.1@gmail.com>
2019-03-28 13:00:31 -0700

    Update SPAW.js
    Convert to a function component using hook

--- client/layouts/SPAW.js
+++ client/layouts/SPAW.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, memo } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import camelCase from 'lodash/camelCase'
 import Highlight, { DANGER } from 'components/highlight'
@@ -6,45 +6,48 @@ import { Link } from 'prismic/components/base'
 import { Document } from 'prismic/containers'
 import { spaw } from 'prismic/params'
 import { Banner } from 'components/application'
-import { SessionStorage } from 'services/storage'
 import { isTouchable } from 'utils/device'
+import { useSessionStorage } from 'utils/react/hooks'
 
-export default class SPAW extends PureComponent {
-    storage = SessionStorage.create()
-    state = {
-        hidden: this.storage.get('spaw-hidden'),
+export default function SPAW() {
+    const [hidden, setHidden] = useSessionStorage(
+        'spaw-hidden',
+        false,
+        Boolean,
+        String
+    )
+    function handleDismiss() {
+        setHidden(true)
     }
-    handleDismiss = () => {
-        this.setState({ hidden: true }, () => {
-            this.storage.set('spaw-hidden', true)
-        })
-    }
-    children = ({ document }) => {
-        if (!document || this.state.hidden) {
-            return null
-        }
 
-        const { link, description } = document.data
+    return (
+        <Document {...spaw()}>
+            {({ document }) => {
+                if (!document || hidden) {
+                    return null
+                }
 
-        return (
-            <Banner>
-                <Highlight
-                    type={DANGER}
-                    onDismiss={this.handleDismiss}
-                    dismissable>
-                    <Link {...link}>
-                        <p>
-                            {description[0].text}{' '}
-                            {isTouchable ? 'Tap' : 'Click'} for more information
-                        </p>
-                    </Link>
-                </Highlight>
-            </Banner>
-        )
-    }
-    render() {
-        return <Document {...spaw()}>{this.children}</Document>
-    }
+                const { link, description } = document.data
+
+                return (
+                    <Banner>
+                        <Highlight
+                            type={DANGER}
+                            onDismiss={handleDismiss}
+                            dismissable>
+                            <Link {...link}>
+                                <p>
+                                    {description[0].text}{' '}
+                                    {isTouchable ? 'Tap' : 'Click'} for more
+                                    information
+                                </p>
+                            </Link>
+                        </Highlight>
+                    </Banner>
+                )
+            }}
+        </Document>
+    )
 }
 
 BaseRegion.propTypes = {
