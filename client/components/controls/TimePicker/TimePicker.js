8dc9350ffe9662ab4122a395aff45c9a0663dde2
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-01 21:31:35 -0700

    Update TimePicker.js
    Convert to functional component

--- client/components/controls/TimePicker/TimePicker.js
+++ client/components/controls/TimePicker/TimePicker.js
@@ -1,36 +1,39 @@
-import React, { PureComponent } from 'react'
+import React, { useReducer } from 'react'
 import PropTypes from 'prop-types'
 import styles from './TimePicker.css'
 
-export default class TimePicker extends PureComponent {
-    static propTypes = {
-        value: PropTypes.string,
-        onChange: PropTypes.func.isRequired,
-        onKeyDown: PropTypes.func,
-        onFocus: PropTypes.func,
-        step: PropTypes.number,
-        autoFocus: PropTypes.bool,
-    }
-    constructor(props) {
-        super(props)
+TimePicker.propTypes = {
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+    onKeyDown: PropTypes.func,
+    onFocus: PropTypes.func,
+    step: PropTypes.number,
+    autoFocus: PropTypes.bool,
+}
 
-        const { value = '' } = props
-
-        this.state = {
-            hour: value.split(':')[0] || 0,
-            minute: padMinute(value.split(':')[1]),
-        }
-    }
-    handleHourChange = event => {
+export default function TimePicker({
+    value = '',
+    onChange,
+    onKeyDown,
+    onFocus,
+    step,
+    autoFocus,
+}) {
+    const [{ hour, minute }, setState] = useReducer(reducer, {
+        hour: value.split(':')[0] || 0,
+        minute: padMinute(value.split(':')[1]),
+    })
+    function handleHourChange(event) {
         let hour = Number(event.target.value)
 
         if (hour > 23 || hour < 0) {
             hour = 0
         }
 
-        this.setState({ hour }, this.sendChange)
+        setState({ hour })
+        onChange(`${hour}:${minute}`)
     }
-    handleMinuteChange = event => {
+    function handleMinuteChange(event) {
         let minute = Number(event.target.value)
 
         if (minute > 59 || minute < 0) {
@@ -39,53 +42,51 @@ export default class TimePicker extends PureComponent {
 
         minute = padMinute(minute)
 
-        this.setState({ minute }, this.sendChange)
+        setState({ minute })
+        onChange(`${hour}:${minute}`)
     }
-    sendChange = () => {
-        const { hour, minute } = this.state
 
-        this.props.onChange(`${hour}:${minute}`)
-    }
-    render() {
-        const { step, onKeyDown, autoFocus, onFocus } = this.props
-        const { hour, minute } = this.state
-
-        return (
-            <div className={styles.Container}>
-                <label title="Hour" className={styles.Hour}>
-                    <input
-                        value={hour}
-                        name="time-picker-hour"
-                        onChange={this.handleHourChange}
-                        onKeyDown={onKeyDown}
-                        type="number"
-                        min="0"
-                        max="23"
-                        onFocus={onFocus}
-                        autoFocus={autoFocus}
-                    />
-                    <span className={styles.Label}>Hour</span>
-                </label>
-                <label title="Minute" className={styles.Minute}>
-                    <input
-                        value={minute}
-                        name="time-picker-minute"
-                        onChange={this.handleMinuteChange}
-                        onKeyDown={onKeyDown}
-                        type="number"
-                        min="0"
-                        max="59"
-                        step={step}
-                        onFocus={onFocus}
-                    />
-                    <span className={styles.Label}>Minute</span>
-                </label>
-            </div>
-        )
-    }
+    return (
+        <div className={styles.Container}>
+            <label title="Hour" className={styles.Hour}>
+                <input
+                    value={hour}
+                    name="time-picker-hour"
+                    onChange={handleHourChange}
+                    onKeyDown={onKeyDown}
+                    type="number"
+                    min="0"
+                    max="23"
+                    onFocus={onFocus}
+                    autoFocus={autoFocus}
+                />
+                <span className={styles.Label}>Hour</span>
+            </label>
+            <label title="Minute" className={styles.Minute}>
+                <input
+                    value={minute}
+                    name="time-picker-minute"
+                    onChange={handleMinuteChange}
+                    onKeyDown={onKeyDown}
+                    type="number"
+                    min="0"
+                    max="59"
+                    step={step}
+                    onFocus={onFocus}
+                />
+                <span className={styles.Label}>Minute</span>
+            </label>
+        </div>
+    )
 }
 
 // Utils
 function padMinute(minute = 0) {
     return String(minute).padStart(2, '0')
+}
+function reducer(oldState, newState) {
+    return {
+        ...oldState,
+        ...newState,
+    }
 }
