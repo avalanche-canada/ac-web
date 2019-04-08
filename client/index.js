d25704d64926689fb40d96cfbe51de6d4f5f5493
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-08 11:38:29 -0700

    Start using polyfill.io service

--- client/index.js
+++ client/index.js
@@ -1,9 +1,3 @@
-// Polyfills and shims
-import 'raf/polyfill'
-import 'url-search-params-polyfill'
-import 'whatwg-fetch'
-import 'utils/polyfills/requestIdleCallback'
-
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Redirect, Location } from '@reach/router'
