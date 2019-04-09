728b1b0f84cc81a4451c7830b972e8c6467f5a37
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-08 20:22:18 -0700

    [BUTTONS/SUBMIT] Remove unsued component and reuse existing one

--- client/components/form/index.js
+++ client/components/form/index.js
@@ -5,5 +5,3 @@ export Legend from './Legend'
 
 export Control from './Control'
 export ControlSet from './ControlSet'
-
-export Submit from './Submit'
--- client/services/mailchimp/Subscribe.js
+++ client/services/mailchimp/Subscribe.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Input } from 'components/controls'
-import Button from 'components/button'
+import { Submit } from 'components/button'
 import styles from './Subscribe.css'
 
 Subscribe.propTypes = {
@@ -24,9 +24,7 @@ export default function Subscribe({ url }) {
                 required
                 className={styles.Input}
             />
-            <Button type="submit" className={styles.Submit}>
-                Subscribe
-            </Button>
+            <Submit chevron="RIGHT">Subscribe</Submit>
             {Array.from(params.entries()).map(([name, value]) => (
                 <input key={name} name={name} type="hidden" value={value} />
             ))}
