950150c1d202e1ea75df3d8b3648fe6a6574e093
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-12 05:24:45 -0700

    [MWF] Remove <Panel> from "Day 5 to 7" content

--- client/components/weather/tabs/ExtendedWeatherForecast.js
+++ client/components/weather/tabs/ExtendedWeatherForecast.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import GramSet, { Location } from './gram'
 import ExceedanceProbability from './ExceedanceProbability'
-import BasePanel, { INVERSE } from 'components/panel'
 import { carte, epsgram, spaghetti } from 'services/msc/naefs'
 import Loop from 'components/loop'
-import Shim from 'components/Shim'
 
 ExtendedWeatherForecast.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
@@ -34,60 +32,53 @@ export default function ExtendedWeatherForecast({ date }) {
     )
 
     return (
-        <section>
-            <Panel header="500 hPa Mean / Standard Deviation (N. American Ensemble)">
-                <Shim all>
-                    <Loop urls={hpaUrls} titles={LOOP_TITLES} />
-                </Shim>
-            </Panel>
-            <Panel header="1000 – 500 hPa Thickness (N. American Ensemble)">
-                <Shim all>
-                    <Loop urls={thicknessUrls} titles={LOOP_TITLES} />
-                </Shim>
-            </Panel>
-            <Panel header="EPSgrams (N. American Ensemble)">
-                <Shim all>
-                    <GramSet>
-                        <Location>
-                            <header>Terrace</header>
-                            <img src={epsgram({ code: 'yxt', date })} />
-                        </Location>
-                        <Location>
-                            <header>Prince George</header>
-                            <img src={epsgram({ code: 'yxs', date })} />
-                        </Location>
-                        <Location>
-                            <header>Vancouver</header>
-                            <img src={epsgram({ code: 'yvr', date })} />
-                        </Location>
-                        <Location>
-                            <header>Revelstoke</header>
-                            <img src={epsgram({ code: 'yrv', date })} />
-                        </Location>
-                    </GramSet>
-                </Shim>
-            </Panel>
-            <Panel header="Exceedance Probability (N. American Ensemble)">
+        <Fragment>
+            <Section header="500 hPa Mean / Standard Deviation (N. American Ensemble)">
+                <Loop urls={hpaUrls} subtitles={LOOP_SUBTITLES} />
+            </Section>
+            <Section header="1000 – 500 hPa Thickness (N. American Ensemble)">
+                <Loop urls={thicknessUrls} subtitles={LOOP_SUBTITLES} />
+            </Section>
+            <Section header="EPSgrams (N. American Ensemble)">
+                <GramSet>
+                    <Location>
+                        <header>Terrace</header>
+                        <img src={epsgram({ code: 'yxt', date })} />
+                    </Location>
+                    <Location>
+                        <header>Prince George</header>
+                        <img src={epsgram({ code: 'yxs', date })} />
+                    </Location>
+                    <Location>
+                        <header>Vancouver</header>
+                        <img src={epsgram({ code: 'yvr', date })} />
+                    </Location>
+                    <Location>
+                        <header>Revelstoke</header>
+                        <img src={epsgram({ code: 'yrv', date })} />
+                    </Location>
+                </GramSet>
+            </Section>
+            <Section header="Exceedance Probability (N. American Ensemble)">
                 <ExceedanceProbability date={date} />
-            </Panel>
-            <Panel header="546 dam – 500 hPa Contour Line (Canadian Ensemble)">
-                <Shim all>
-                    <Loop urls={spaghettiUrls} titles={LOOP_TITLES} />
-                </Shim>
-            </Panel>
-        </section>
+            </Section>
+            <Section header="546 dam – 500 hPa Contour Line (Canadian Ensemble)">
+                <Loop urls={spaghettiUrls} subtitles={LOOP_SUBTITLES} />
+            </Section>
+        </Fragment>
     )
 }
 
 // Utils
-function Panel({ children, ...props }) {
+function Section({ children, header }) {
     return (
-        <BasePanel expandable expanded theme={INVERSE} {...props}>
+        <section>
+            <h4>{header}</h4>
             {children}
-        </BasePanel>
+        </section>
     )
 }
 
 // Constants
 const SEQUENCE = [5, 6, 7, 8, 9, 10]
-const LOOP_TITLES = SEQUENCE.map(value => `Day ${value}`)
+const LOOP_SUBTITLES = SEQUENCE.map(value => `Day ${value}`)
