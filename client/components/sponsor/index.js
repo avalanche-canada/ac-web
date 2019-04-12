a9a70a9d981591318751fc7b57ade3ed3e11dd9d
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-12 14:12:57 -0700

    [COMPONENTS/SPONSOR] Remove modules indirection

--- client/components/sponsor/index.js
+++ client/components/sponsor/index.js
@@ -1,2 +1,31 @@
-export ItemSet from './ItemSet'
-export Item from './Item'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Sponsor.css'
+import { handleOutboundSponsorClick } from 'services/analytics'
+
+Item.propTypes = {
+    src: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+}
+
+export function Item({ src, url, title = url }) {
+    return (
+        <a
+            className={styles.Item}
+            href={url}
+            target="_blank"
+            title={title}
+            onClick={handleOutboundSponsorClick}>
+            <img className={styles.Logo} src={src} alt={title} />
+        </a>
+    )
+}
+
+export function ItemSet({ children, ...props }) {
+    return (
+        <div {...props} className={styles.ItemSet}>
+            {children}
+        </div>
+    )
+}
