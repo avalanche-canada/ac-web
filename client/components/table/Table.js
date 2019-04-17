9e68ba8f18133297a8c577a96e7f5318a0c88471
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-17 09:46:23 -0700

    Revert "[COMPONENTS/TABLE] Remove "classnames.bind" usage"
    This reverts commit 46380bcda408bf214e23042874c99794325287d2.

--- client/components/table/Table.js
+++ client/components/table/Table.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
+import classnames from 'classnames/bind'
 import styles from './Table.css'
 
 Table.propTypes = {
@@ -9,7 +9,12 @@ Table.propTypes = {
 }
 
 export default function Table({ condensed, children }) {
-    const className = classnames(styles.Table, condensed && styles.Condensed)
+    const className = classNames({
+        Table: true,
+        Condensed: condensed,
+    })
 
     return <table className={className}>{children}</table>
 }
+
+const classNames = classnames.bind(styles)
