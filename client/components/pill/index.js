48f32dbcd15c6699d5eb7f51bc3c39cd083e19e9
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-17 22:18:55 -0700

    [COMPONENTS/PILL] Reduce modules indirection

--- client/components/pill/index.js
+++ client/components/pill/index.js
@@ -1,3 +1,52 @@
-export PillSet from './Set'
-export Pill from './Item'
-export Container from './Container'
+import React, { Children, cloneElement, useState } from 'react'
+import PropTypes from 'prop-types'
+import noop from 'lodash/noop'
+import styles from './Pill.css'
+
+Pill.propTypes = {
+    children: PropTypes.node.isRequired,
+    active: PropTypes.bool,
+    onClick: PropTypes.func.isRequired,
+}
+
+export function Pill({ active = false, onClick, children }) {
+    const className = active ? 'Item--Active' : 'Item'
+
+    return (
+        <li className={styles[className]} onClick={onClick}>
+            {children}
+        </li>
+    )
+}
+
+PillSet.propTypes = {
+    children: PropTypes.node.isRequired,
+    activeIndex: PropTypes.number,
+    onActivate: PropTypes.func,
+}
+
+export function PillSet({ activeIndex, onActivate = noop, children }) {
+    const [value, set] = useState(activeIndex)
+
+    return (
+        <ul className={styles.Set}>
+            {Children.map(children, (item, index) =>
+                cloneElement(item, {
+                    active: value === index,
+                    onClick() {
+                        set(index)
+                        onActivate(index)
+                    },
+                })
+            )}
+        </ul>
+    )
+}
+
+export function Container({ children, ...props }) {
+    return (
+        <div {...props} className={styles.Container}>
+            {children}
+        </div>
+    )
+}
