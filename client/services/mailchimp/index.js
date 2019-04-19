a9eacedaf4a41d261d00b84fa7316b3c27d831b2
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-18 18:03:15 -0700

    [SERVICES/MAILCHIMP] Reduce modules indirection

--- client/services/mailchimp/index.js
+++ client/services/mailchimp/index.js
@@ -1,1 +1,33 @@
-export Subscribe from './Subscribe'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Input } from 'components/controls'
+import { Submit } from 'components/button'
+import styles from './Subscribe.css'
+
+Subscribe.propTypes = {
+    url: PropTypes.string.isRequired,
+}
+
+export function Subscribe({ url }) {
+    const params = new URLSearchParams(url)
+
+    return (
+        <form
+            action={url}
+            method="post"
+            target="_blank"
+            className={styles.Subscribe}>
+            <Input
+                type="email"
+                name="EMAIL"
+                placeholder="Email address"
+                required
+                className={styles.Input}
+            />
+            <Submit chevron="RIGHT">Subscribe</Submit>
+            {Array.from(params.entries()).map(([name, value]) => (
+                <input key={name} name={name} type="hidden" value={value} />
+            ))}
+        </form>
+    )
+}
