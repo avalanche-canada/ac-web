32d9dbc497f5851ee3d5c6106d9fd04b478b46a2
karlguillotte <karl.guillotte.1@gmail.com>
2019-04-25 12:53:26 -0700

    Update MultiLine.js
    Remove "as" property as it is not used yet!

--- client/components/misc/MultiLine.js
+++ client/components/misc/MultiLine.js
@@ -1,16 +1,15 @@
-import { createElement } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 MultiLine.propTypes = {
     children: PropTypes.node,
-    as: PropTypes.string,
 }
 
-export default function MultiLine({ children = null, as = 'p' }) {
+export default function MultiLine({ children = null }) {
     if (typeof children === 'string') {
         return children
             .split(/\r\n|\r|\n/gm)
-            .map((text, key) => createElement(as, { key }, text))
+            .map((text, index) => <p key={index}>{text}</p>)
     }
 
     return children
