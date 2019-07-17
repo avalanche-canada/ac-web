281f2a903005fb5b9073f10f37f1feb12999eb21
karlguillotte <karl.guillotte.1@gmail.com>
2019-07-16 23:19:50 -0700

    Remove unused module

--- client/utils/function.js
+++
@@ -1,7 +0,0 @@
-export function compose(...fns) {
-    return x => fns.reduceRight((acc, fn) => fn(acc), x)
-}
-
-export function pipe(...fns) {
-    return x => fns.reduce((acc, fn) => fn(acc), x)
-}
