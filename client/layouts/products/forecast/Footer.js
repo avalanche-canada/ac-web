0792f6eee7403d840c6849601ca47a726aec5055
karlguillotte <karl.guillotte.1@gmail.com>
2019-07-18 22:49:32 -0700

    [PRODUCT/FORECAST/FOOTER] Use <Shim> and remove "memo.static" usage

--- client/layouts/products/forecast/Footer.js
+++ client/layouts/products/forecast/Footer.js
@@ -1,9 +1,8 @@
 import React, { Fragment, Children, cloneElement } from 'react'
 import PropTypes from 'prop-types'
-import { memo } from 'utils/react'
 import { useForecast } from './Context'
 import Panel from 'components/panel'
-
+import Shim from 'components/Shim'
 import RatingExplanation from 'layouts/products/forecast/RatingExplanation'
 import { Generic } from 'prismic/layouts'
 import ArchiveDatePicker from './ArchiveDatePicker'
@@ -13,48 +12,45 @@ export default function Footer() {
     const forecast = useForecast()
 
     return forecast ? (
-        <OptimizedFooterComponent
-            region={forecast.region}
-            date={forecast.date}
-        />
+        <FooterComponent region={forecast.region} date={forecast.date} />
     ) : null
 }
 
-export const ArchivedBulletins = memo.static(function ArchivedBulletins(props) {
+export function ArchivedBulletins(props) {
     return (
         <Panel header="Archived bulletins">
             <ArchiveDatePicker region={props.region} />
         </Panel>
     )
-})
+}
 
-export const Inbox = memo.static(function Inbox() {
+export function Inbox() {
     return (
         <Panel header="Avalanche Forecasts in your Inbox">
-            <div className={styles.PanelContent}>
+            <Shim horizontal>
                 <Generic uid="forecast-rss-message" />
-            </div>
+            </Shim>
         </Panel>
     )
-})
+}
 
-export const Disclaimer = memo.static(function Disclaimer() {
+export function Disclaimer() {
     return (
         <Panel header="Forecast Disclaimer">
-            <div className={styles.PanelContent}>
+            <Shim horizontal>
                 <Generic uid="forecast-disclaimer" />
-            </div>
+            </Shim>
         </Panel>
     )
-})
+}
 
-export const DangerRatings = memo.static(function DangerRatings() {
+export function DangerRatings() {
     return (
         <Panel header="Danger Ratings Explained">
             <RatingExplanation />
         </Panel>
     )
-})
+}
 
 FooterComponent.propTypes = {
     region: PropTypes.string.isRequired,
@@ -89,5 +85,3 @@ function FooterComponent({ children, region }) {
         </footer>
     )
 }
-
-const OptimizedFooterComponent = memo.static(FooterComponent)
