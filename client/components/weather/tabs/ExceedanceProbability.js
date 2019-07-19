df404062849d271bc097a4472fb40805c79867bf
karlguillotte <karl.guillotte.1@gmail.com>
2019-07-19 14:59:52 -0700

    Update ExceedanceProbability.js
    Convert to functional component

--- client/components/weather/tabs/ExceedanceProbability.js
+++ client/components/weather/tabs/ExceedanceProbability.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Container, Set, Item } from 'components/pill'
 import { DropdownFromOptions, DayPicker } from 'components/controls'
@@ -16,50 +16,7 @@ import {
     PARAMETERS,
     format,
 } from 'services/msc/naefs'
-
-const TEMPERATURE = 'TEMPERATURE'
-
-const TITLES = new Map([
-    [TEMPERATURE, 'Temperature'],
-    [PRECIPITATION, 'Precipitation'],
-    [ACCUMULATED_PRECIPITATION, 'Accumulated precipitation'],
-])
-
-const CONTENT_PADDING = {
-    padding: '0 1em 2em 1em',
-}
-
-const OPTIONS = new Map([
-    [
-        TEMPERATURE,
-        new Map([
-            ['GT0', 'over 0°C'],
-            ['LT0', 'under 0°C'],
-            ['LT-5', 'under -5°C'],
-            ['LT-15', 'under -15°C'],
-            ['LT-30', 'under -30°C'],
-        ]),
-    ],
-    [
-        PRECIPITATION,
-        new Map([
-            ['GT0.002', 'more than 2mm'],
-            ['GT0.005', 'more than 5mm'],
-            ['GT0.010', 'more than 10mm'],
-            ['GT0.025', 'more than 25mm'],
-        ]),
-    ],
-    [
-        ACCUMULATED_PRECIPITATION,
-        new Map([
-            ['GT0.002', 'more than 2mm'],
-            ['GT0.005', 'more than 5mm'],
-            ['GT0.010', 'more than 10mm'],
-            ['GT0.025', 'more than 25mm'],
-            ['GT0.050', 'more than 50mm'],
-        ]),
-    ],
-])
+import Shim from 'components/Shim'
 
 Image.propTypes = {
     date: PropTypes.instanceOf(Date).isRequired,
@@ -115,98 +72,138 @@ function Title({ product, children }) {
     )
 }
 
+ExceedanceProbability.propTypes = {
+    date: PropTypes.instanceOf(Date).isRequired,
+}
+
+export default function ExceedanceProbability({ date }) {
+    const [state, setState] = useState({
+        product: TEMPERATURE,
+        param: DEFAULT_PARAMETERS.get(TEMPERATURE),
+        from: addDays(date, 4),
+        to: addDays(date, 7),
+    })
+    const { product, param, from, to } = state
+    const activeIndex = Array.from(TITLES.keys()).indexOf(product)
+    function fromDisabledDays(day) {
+        return !isWithinRange(day, date, addDays(date, 5))
+    }
+    function toDisabledDays(day) {
+        return !isWithinRange(day, addDays(date, 5), addDays(date, 9))
+    }
+    const realProduct = getRealProduct(product, param)
+    function handleFromChange(from) {
+        setState(state => ({ ...state, from }))
+    }
+    function handleToChange(to) {
+        setState(state => ({ ...state, to }))
+    }
+    function handleParamChange(param) {
+        if (!param) {
+            return
+        }
+
+        setState(state => ({ ...state, param }))
+    }
+    function handleActivateType(index) {
+        // Use "TITLES" because index reflects which tab clicked
+        const [product] = Array.from(TITLES)[index]
+
+        setState(state => ({
+            ...state,
+            product,
+            param: DEFAULT_PARAMETERS.get(product),
+        }))
+    }
+
+    return (
+        <section>
+            <Container>
+                <Set onActivate={handleActivateType} activeIndex={activeIndex}>
+                    {Array.from(TITLES, ([product, title]) => (
+                        <Item key={product}>{title}</Item>
+                    ))}
+                </Set>
+            </Container>
+            <Shim right bottom left>
+                <Title product={product}>
+                    <DropdownFromOptions
+                        value={param}
+                        options={OPTIONS.get(product)}
+                        onChange={handleParamChange}
+                    />
+                    <div>between</div>
+                    <DayPicker
+                        date={from}
+                        onChange={handleFromChange}
+                        disabledDays={fromDisabledDays}
+                    />
+                    <div>and</div>
+                    <DayPicker
+                        date={to}
+                        onChange={handleToChange}
+                        disabledDays={toDisabledDays}
+                    />
+                </Title>
+                <Image {...state} product={realProduct} date={date} />
+            </Shim>
+        </section>
+    )
+}
+
+// Utils
+function getRealProduct(product, param) {
+    if (product !== TEMPERATURE) {
+        return product
+    }
+
+    if (PARAMETERS.get(MINIMUM_TEMPERATURE).has(param)) {
+        return MINIMUM_TEMPERATURE
+    } else {
+        return MAXIMUM_TEMPERATURE
+    }
+}
+
+// Constants
+const TEMPERATURE = 'TEMPERATURE'
+const OPTIONS = new Map([
+    [
+        TEMPERATURE,
+        new Map([
+            ['GT0', 'over 0°C'],
+            ['LT0', 'under 0°C'],
+            ['LT-5', 'under -5°C'],
+            ['LT-15', 'under -15°C'],
+            ['LT-30', 'under -30°C'],
+        ]),
+    ],
+    [
+        PRECIPITATION,
+        new Map([
+            ['GT0.002', 'more than 2mm'],
+            ['GT0.005', 'more than 5mm'],
+            ['GT0.010', 'more than 10mm'],
+            ['GT0.025', 'more than 25mm'],
+        ]),
+    ],
+    [
+        ACCUMULATED_PRECIPITATION,
+        new Map([
+            ['GT0.002', 'more than 2mm'],
+            ['GT0.005', 'more than 5mm'],
+            ['GT0.010', 'more than 10mm'],
+            ['GT0.025', 'more than 25mm'],
+            ['GT0.050', 'more than 50mm'],
+        ]),
+    ],
+])
 const DEFAULT_PARAMETERS = new Map([
     [TEMPERATURE, 'LT-5'],
     [PRECIPITATION, 'GT0.025'],
     [ACCUMULATED_PRECIPITATION, 'GT0.025'],
 ])
-
-// TODO: HOOKS
-export default class ExceedanceProbability extends Component {
-    static propTypes = {
-        date: PropTypes.instanceOf(Date).isRequired,
-    }
-    state = {
-        product: TEMPERATURE,
-        param: DEFAULT_PARAMETERS.get(TEMPERATURE),
-        from: addDays(this.props.date, 4),
-        to: addDays(this.props.date, 7),
-    }
-    get realProduct() {
-        const { product, param } = this.state
-
-        if (product !== TEMPERATURE) {
-            return product
-        }
-
-        if (PARAMETERS.get(MINIMUM_TEMPERATURE).has(param)) {
-            return MINIMUM_TEMPERATURE
-        } else {
-            return MAXIMUM_TEMPERATURE
-        }
-    }
-    handleParamChange = param => param && this.setState({ param })
-    handleDateRangeChange = dates => this.setState(dates)
-    handleActivateType = index => {
-        // Use "TITLES" because index reflects which tab clicked
-        const [product] = Array.from(TITLES)[index]
-
-        this.setState({
-            product,
-            param: DEFAULT_PARAMETERS.get(product),
-        })
-    }
-    handleFromChange = from => this.setState({ from })
-    handleToChange = to => this.setState({ to })
-    render() {
-        const { product, param, from, to } = this.state
-        const { date } = this.props
-        const activeIndex = Array.from(TITLES.keys()).indexOf(product)
-        function fromDisabledDays(day) {
-            return !isWithinRange(day, date, addDays(date, 5))
-        }
-        function toDisabledDays(day) {
-            return !isWithinRange(day, addDays(date, 5), addDays(date, 9))
-        }
-
-        return (
-            <section>
-                <Container>
-                    <Set
-                        onActivate={this.handleActivateType}
-                        activeIndex={activeIndex}>
-                        {Array.from(TITLES, ([product, title]) => (
-                            <Item key={product}>{title}</Item>
-                        ))}
-                    </Set>
-                </Container>
-                <div style={CONTENT_PADDING}>
-                    <Title product={product}>
-                        <DropdownFromOptions
-                            value={param}
-                            options={OPTIONS.get(product)}
-                            onChange={this.handleParamChange}
-                        />
-                        <div>between</div>
-                        <DayPicker
-                            date={from}
-                            onChange={this.handleFromChange}
-                            disabledDays={fromDisabledDays}
-                        />
-                        <div>and</div>
-                        <DayPicker
-                            date={to}
-                            onChange={this.handleToChange}
-                            disabledDays={toDisabledDays}
-                        />
-                    </Title>
-                    <Image
-                        {...this.state}
-                        product={this.realProduct}
-                        date={date}
-                    />
-                </div>
-            </section>
-        )
-    }
-}
+const TITLES = new Map([
+    [TEMPERATURE, 'Temperature'],
+    [PRECIPITATION, 'Precipitation'],
+    [ACCUMULATED_PRECIPITATION, 'Accumulated precipitation'],
+])
