06a401b96c882ac3e029fbc83824b93c807d72da
karlguillotte <karl.guillotte.1@gmail.com>
2019-07-19 08:10:02 -0700

    [COMPONENTS/DESCRIPTION] Fix <Term> export

--- client/components/description/index.js
+++ client/components/description/index.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames/bind'
 import styles from './Description.css'
 
-Base.propTypes = {
+Term.propTypes = {
     children: PropTypes.node.isRequired,
     style: PropTypes.object,
     className: PropTypes.string,
     block: PropTypes.bool,
 }
 
-export function Base({ style, block, className, children }) {
+export function Term({ style, block, className, children }) {
     className = classNames(className, { Term: true, Block: block })
 
     return (
@@ -45,7 +45,7 @@ Entry.propTypes = {
 export function Entry({ term, children }) {
     return (
         <Fragment>
-            <Base>{term}</Base>
+            <Term>{term}</Term>
             <Definition>{children}</Definition>
         </Fragment>
     )
