30e8fa0f7528c22025166030cfba44deb6f03ccb
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-03 12:34:57 -0700

    [GLOSSARY] Fix to handle reset
    - A better implementation should be done

--- client/components/form/Search.js
+++ client/components/form/Search.js
@@ -22,6 +22,7 @@ export default function Search({ value, onChange, placeholder = 'Search...' }) {
         onChange(value)
     }
     function handleReset() {
+        input.current.value = ''
         onChange('')
         focus()
     }
