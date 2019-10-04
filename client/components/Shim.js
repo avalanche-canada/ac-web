27a3dc1a93dd940c0f5d1243cf3d011b97a48025
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-04 13:12:27 -0700

    [COMPONENTS] Make <Shim> component more flexible
    - Adds ability to provide any component

--- client/components/Shim.js
+++ client/components/Shim.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { createElement } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames/bind'
 import styles from './Shim.css'
@@ -12,10 +12,14 @@ Shim.propTypes = {
     vertical: PropTypes.bool,
     horizontal: PropTypes.bool,
     all: PropTypes.bool,
+    as: PropTypes.string,
+    className: PropTypes.string,
 }
 
-export default function Shim({ children, ...values }) {
-    return <div className={classNames(values)}>{children}</div>
+export default function Shim({ children, as = 'div', className, ...values }) {
+    className = classNames(className, values)
+
+    return createElement(as, { className }, children)
 }
 
 const classNames = classnames.bind(styles)
