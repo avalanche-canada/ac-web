43b7bf56355f06beffad5befa956ef5e4f9ce0dd
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-07 15:20:06 -0700

    Add season 2019 data folder

---
+++ server/data/season/2019/forecast-regions.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var path = require('path');
+
+var metadata = require('./forecast-metadata.json');
+var avalxMapping = require('./avalxMapping.json');
+var logger = require('../../../logger');
+var config = require('../../../config/environment');
+
+logger.info('season: 2019');
+
+//TODO(wnh): Remove old URLS from the forecast json
+
+module.exports = {
+    type: 'FeatureCollection',
+    features: Object.keys(metadata).map(id => {
+        var properties = metadata[id];
+        var geometry = fs.readFileSync(path.join(__dirname, 'geometries', id + '.geojson'));
+    
+        var url = null;
+        if (properties.type === 'avalx') {
+            url =
+                config.AVALX2016_ENDPOINT + '?r=' + String(avalxMapping[id]);
+        }
+
+        properties.url = url || properties.url;
+        properties.id = id;
+
+        return {
+            id: id,
+            type: 'Feature',
+            geometry: JSON.parse(geometry),
+            properties: properties
+        }
+    }),
+};
+
