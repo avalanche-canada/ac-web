3085e66a4dd69b2c2b624b50e5c2c0797762a17b
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-10 12:10:16 -0700

    Use polyfills from 'core-js' + create a module for loading polyfills

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,2 +1,52 @@
+export default async function polyfills(self) {
+    if (!self.fetch) {
+        await import('whatwg-fetch')
+    }
 
-import 'promise-polyfill/src/polyfill'
+    if (!self.Promise) {
+        await import('core-js/features/promise')
+    }
+
+    if (!self.requestAnimationFrame) {
+        await import('raf/polyfill')
+    }
+
+    if (!urlSearchParamsSupported(self)) {
+        await import('url-search-params-polyfill')
+    }
+
+    if (!self.Set) {
+        await import('core-js/features/set')
+    }
+}
+
+// Utils
+// Copied from https://github.com/jerrybendy/url-search-params-polyfill/blob/master/index.js
+function urlSearchParamsSupported(self) {
+    var nativeURLSearchParams =
+            self.URLSearchParams && self.URLSearchParams.prototype.get
+                ? self.URLSearchParams
+                : null,
+        isSupportObjectConstructor =
+            nativeURLSearchParams &&
+            new nativeURLSearchParams({ a: 1 }).toString() === 'a=1',
+        // There is a bug in safari 10.1 (and earlier) that incorrectly decodes `%2B` as an empty space and not a plus.
+        decodesPlusesCorrectly =
+            nativeURLSearchParams &&
+            new nativeURLSearchParams('s=%2B').get('s') === '+',
+        // Fix bug in Edge which cannot encode ' &' correctly
+        encodesAmpersandsCorrectly = nativeURLSearchParams
+            ? (function() {
+                  var ampersandTest = new nativeURLSearchParams()
+                  ampersandTest.append('s', ' &')
+                  return ampersandTest.toString() === 's=+%26'
+              })()
+            : true
+
+    return (
+        nativeURLSearchParams &&
+        isSupportObjectConstructor &&
+        decodesPlusesCorrectly &&
+        encodesAmpersandsCorrectly
+    )
+}
