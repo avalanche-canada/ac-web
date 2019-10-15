831800603c019da94bccb1660dc0b55ad8c7b15f
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-14 23:30:45 -0700

    [HOOK] Set the cache (oups!!!) & smarter initial pending value

--- client/utils/react/hooks/index.js
+++ client/utils/react/hooks/index.js
@@ -78,7 +78,9 @@ export function useEventListener(eventName, handler, element = window) {
 
 export function useFetch(url, cache = new None()) {
     const [data, setData] = useSafeState(cache.get(url))
-    const [pending, setPending] = useSafeState(FETCHING.has(url))
+    const [pending, setPending] = useSafeState(
+        FETCHING.has(url) || (Boolean(url) && !data)
+    )
     const controller = useRef(null)
 
     async function fetcher() {
@@ -100,6 +102,7 @@ export function useFetch(url, cache = new None()) {
             const payload = await status(response)
 
             setData(payload)
+            cache.set(url, payload)
         } catch (error) {
             throw error
         } finally {
