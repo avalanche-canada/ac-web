99133ff717b3e7d5f6f17b7557903b6ee1bec2a8
Will Harding <wharding@avalanche.ca>
2019-10-18 07:03:55 -0700

    Add icon set to region configs

--- server/api/forecasts/region_config.js
+++ server/api/forecasts/region_config.js
@@ -2,8 +2,9 @@ var _ = require('lodash');
 var xml2js = require('xml2js');
 var Q = require('q');
 
-var fetch = require('./fetch');
-var avalx = require('./avalx');
+var fetch    = require('./fetch');
+var avalx    = require('./avalx');
+var icon_set = require('./icon_set');
 
 var reg_metadata = require('../../data/season')
 
@@ -34,71 +35,74 @@ function parseAvalx(region_id) {
             });
     }
 }
+function addStaticIcons(tz) { return function(fx) { return icon_set.addStaticIcons(tz, fx); } }
+function addMovingIcons(tz) { return function(fx) { return icon_set.addMovingIcons(tz, fx); } }
 
+function consoledir(val) { console.dir(val); return val; }
 
 var AVCAN = {
     'northwest-coastal': {
-        fetchNow: function() { return fetch.fetchAvalx2019(16).then(parseAvalx('northwest-coastal')); },
         metadata: reg_properties['northwest-coastal'],
+        fetchNow: function() { return fetch.fetchAvalx2019(16) .then(parseAvalx('northwest-coastal')) .then(addStaticIcons('America/Vancouver')); },
     },
     'northwest-inland': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(17).then(parseAvalx('northwest-inland')); },
         metadata: reg_properties['northwest-inland'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(17).then(parseAvalx('northwest-inland')).then(addStaticIcons('America/Vancouver')); },
     },
     'sea-to-sky': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(14).then(parseAvalx('sea-to-sky')); },
         metadata: reg_properties['sea-to-sky'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(14).then(parseAvalx('sea-to-sky')).then(addStaticIcons('America/Vancouver')); },
     },
     'south-coast-inland': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(15).then(parseAvalx('south-coast-inland')); },
         metadata: reg_properties['south-coast-inland'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(15).then(parseAvalx('south-coast-inland')).then(addStaticIcons('America/Vancouver')); },
     },
     'south-coast': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(8).then(parseAvalx('south-coast')); },
         metadata: reg_properties['south-coast'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(8).then(parseAvalx('south-coast')).then(addStaticIcons('America/Vancouver')); },
     },
     'north-rockies': {
-        fetchNow: function(){ return fetch.fetchAvalx2016(9).then(parseAvalx('north-rockies')); },
         metadata: reg_properties['north-rockies'],
+        fetchNow: function(){ return fetch.fetchAvalx2016(9).then(parseAvalx('north-rockies')).then(addMovingIcons('America/Vancouver')); },
     },
     'cariboos': {
-        fetchNow: function () { return fetch.fetchAvalx2019(19).then(parseAvalx('cariboos')); },
         metadata: reg_properties['cariboos'],
+        fetchNow: function () { return fetch.fetchAvalx2019(19).then(parseAvalx('cariboos')).then(addStaticIcons('America/Vancouver')); },
     },
     'north-columbia': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(18).then(parseAvalx('north-columbia')); },
         metadata: reg_properties['north-columbia'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(18).then(parseAvalx('north-columbia')).then(addStaticIcons('America/Vancouver')); },
     },
     'south-columbia': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(10).then(parseAvalx('south-columbia')); },
         metadata: reg_properties['south-columbia'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(10).then(parseAvalx('south-columbia')).then(addStaticIcons('America/Vancouver')); },
     },
     'purcells': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(11).then(parseAvalx('purcells')); },
         metadata: reg_properties['purcells'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(11).then(parseAvalx('purcells')).then(addStaticIcons('America/Vancouver')); },
     },
     'kootenay-boundary': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(6).then(parseAvalx('kootenay-boundary')); },
         metadata: reg_properties['kootenay-boundary'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(6).then(parseAvalx('kootenay-boundary')).then(addStaticIcons('America/Vancouver')); },
     },
     'south-rockies': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(13).then(parseAvalx('south-rockies')); },
         metadata: reg_properties['south-rockies'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(13).then(parseAvalx('south-rockies')).then(addStaticIcons('America/Vancouver')); },
     },
     'lizard-range': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(12).then(parseAvalx('lizard-range')); },
         metadata: reg_properties['lizard-range'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(12).then(parseAvalx('lizard-range')).then(addStaticIcons('America/Vancouver')); },
     },
     'yukon': {
-        fetchNow: function(){ return fetch.fetchAvalx2019(20).then(parseAvalx('yukon')); },
         metadata: reg_properties['yukon'],
+        fetchNow: function(){ return fetch.fetchAvalx2019(20).then(parseAvalx('yukon')).then(addMovingIcons('America/Vancouver')); },
     },
 };
 
 
 var KCOUNTRY =  {
     'kananaskis': {
-        fetchNow: function(){ return fetch.fetchAvalx2016(7).then(parseAvalx('kananaskis')); },
+        fetchNow: function(){ return fetch.fetchAvalx2016(7).then(parseAvalx('kananaskis')).then(addStaticIcons('America/Edmonton')); },
         metadata: reg_properties['kananaskis'],
     },
 };
@@ -108,23 +112,23 @@ var KCOUNTRY =  {
 
 var PARKS = {
     'glacier': {
-        fetchNow: function(){ return fetch.fetchParks(3).then(parseAvalx('glacier')); },
+        fetchNow: function(){ return fetch.fetchParks(3).then(parseAvalx('glacier')).then(addStaticIcons('America/Vancouver')); },
         metadata: reg_properties['glacier'],
     },
     'little-yoho': {
-        fetchNow: function(){ return fetch.fetchParks(5).then(parseAvalx('little-yoho')); },
+        fetchNow: function(){ return fetch.fetchParks(5).then(parseAvalx('little-yoho')).then(addStaticIcons('America/Edmonton')); },
         metadata: reg_properties['little-yoho'],
     },
     'banff-yoho-kootenay': {
-        fetchNow: function(){ return fetch.fetchParks(1).then(parseAvalx('banff-yoho-kootenay')); },
+        fetchNow: function(){ return fetch.fetchParks(1).then(parseAvalx('banff-yoho-kootenay')).then(addStaticIcons('America/Edmonton')); },
         metadata: reg_properties['banff-yoho-kootenay'],
     },
     'jasper': {
-        fetchNow: function(){ return fetch.fetchParks(2).then(parseAvalx('jasper')); },
+        fetchNow: function(){ return fetch.fetchParks(2).then(parseAvalx('jasper')).then(addStaticIcons('America/Edmonton')); },
         metadata: reg_properties['jasper'],
     },
     'waterton': {
-        fetchNow: function(){ return fetch.fetchParks(4).then(parseAvalx('waterton')); },
+        fetchNow: function(){ return fetch.fetchParks(4).then(parseAvalx('waterton')).then(addStaticIcons('America/Edmonton')); },
         metadata: reg_properties['waterton'],
     },
 };
