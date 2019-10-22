bca3417670a0d900896e4c7cb0cedc90f8d31536
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-22 15:43:06 -0700

    [SERVICES/SENTRY] Integrates as the documentation is telling us to do instead of legacy method
    - Use latest version of Sentry

--- client/services/sentry/index.js
+++ client/services/sentry/index.js
@@ -3,12 +3,14 @@
 
 import { supported } from 'utils/mapbox'
 
-withScope(scope => {
+Sentry.init({ dsn: 'https://3988f0f0f90f494cac5a2dbe256a5f2e@sentry.io/99286' })
+
+Sentry.withScope(scope => {
     scope.setExtra('mapboxgl.supported', supported())
 })
 
 export function captureException(error, context) {
-    withScope(scope => {
+    Sentry.withScope(scope => {
         scope.setExtras(context)
         Sentry.captureException(error)
 
@@ -17,16 +19,11 @@ export function captureException(error, context) {
 }
 
 export function setUserContext({ user_id, email, name }) {
-    withScope(scope => {
+    Sentry.withScope(scope => {
         scope.setUser({
             id: user_id,
             email,
             username: name,
         })
     })
-}
-
-// Fallback functions
-function withScope(callback) {
-    window?.Sentry?.withScope.apply(Sentry, callback)
 }
