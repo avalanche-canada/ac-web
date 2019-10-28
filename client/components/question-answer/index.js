029514b4c9be07331c5ea31ff4e7a74f2b1cec80
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-27 20:19:57 -0700

    [COMPONENTS/Q&A] Reuse <Panel> to improve use interface & accessibility
    It also reduces the bundle

--- client/components/question-answer/index.js
+++ client/components/question-answer/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styles from './QuestionAnswer.css'
+import Panel from 'components/panel'
+import Shim from 'components/Shim'
 
 QuestionAnswer.propTypes = {
     question: PropTypes.node.isRequired,
@@ -9,25 +10,8 @@ QuestionAnswer.propTypes = {
 
 export default function QuestionAnswer({ question, answer }) {
     return (
-        <section>
-            <Question>{question}</Question>
-            <Answer>{answer}</Answer>
-        </section>
-    )
-}
-
-Question.propTypes = {
-    children: PropTypes.node.isRequired,
-}
-
-export function Question({ children }) {
-    return <p className={styles.Question}>{children}</p>
-}
-
-export function Answer({ children, ...props }) {
-    return (
-        <div {...props} className={styles.Answer}>
-            {children}
-        </div>
+        <Panel header={question}>
+            <Shim horizontal>{answer}</Shim>
+        </Panel>
     )
 }
