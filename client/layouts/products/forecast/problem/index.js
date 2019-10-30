56c3c8bb6c0ad325cea4f89346759542f1e82189
karlguillotte <karl.guillotte.1@gmail.com>
2019-10-30 10:47:21 -0700

    [LAYOUTS/FORECAST] Bring all problems related components into one module

--- client/layouts/products/forecast/problem/index.js
+++ client/layouts/products/forecast/problem/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Problem from './Problem'
-import TopicSet from './TopicSet'
-import Topic from './Topic'
-import Advice from './Advice'
-import Comment from './Comment'
+import { InnerHTML } from 'components/misc'
+import classnames from 'classnames'
+import { useClientRect } from 'hooks'
+import styles from './Problem.css'
 
 ProblemSet.propTypes = {
     problems: PropTypes.array.isRequired,
@@ -18,6 +17,82 @@ export default function ProblemSet({ problems }) {
     return problems.map(renderProblem)
 }
 
+Problem.propTypes = {
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired,
+}
+
+function Problem({ title, children }) {
+    return (
+        <div className={styles.Container}>
+            <h2 className={styles.Header}>{title}</h2>
+            {children}
+        </div>
+    )
+}
+
+Advice.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.string.isRequired,
+}
+
+function Advice({ children }) {
+    return (
+        <div className={styles.Advice}>
+            <h3 className={styles.SubHeader}>Travel and Terrain Advice</h3>
+            <InnerHTML>{children}</InnerHTML>
+        </div>
+    )
+}
+
+Comment.propTypes = {
+    children: PropTypes.string.isRequired,
+}
+
+function Comment({ children }) {
+    return (
+        <div className={styles.Comment}>
+            <InnerHTML>{children}</InnerHTML>
+        </div>
+    )
+}
+
+Topic.propTypes = {
+    src: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+}
+
+function Topic({ title, src }) {
+    return (
+        <figure className={styles.Topic}>
+            <div className={styles['Topic--Content']}>
+                <img src={src} />
+            </div>
+            <figcaption>{title}</figcaption>
+        </figure>
+    )
+}
+
+TopicSet.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+
+function TopicSet({ children }) {
+    const [{ width }, ref] = useClientRect({ width: window.innerWidth })
+    const className = classnames({
+        [styles.TopicSet]: true,
+        [styles['TopicSet--2PerRow']]: width > 300 && width < 650,
+        [styles['TopicSet--4PerRow']]: width > 650,
+    })
+
+    return (
+        <div ref={ref} className={className}>
+            {children}
+        </div>
+    )
+}
+
+// Utils
 function renderProblem(problem, index) {
     const { type, icons, comment, travelAndTerrainAdvice } = problem
 
@@ -30,7 +105,9 @@ function renderProblem(problem, index) {
                 <Topic title="Expected Size?" src={icons.expectedSize} />
             </TopicSet>
             <Comment>{comment}</Comment>
-            {travelAndTerrainAdvice && <Advice>{travelAndTerrainAdvice}</Advice>}
+            {travelAndTerrainAdvice && (
+                <Advice>{travelAndTerrainAdvice}</Advice>
+            )}
         </Problem>
     )
 }
