8b53372b90ccf908b5738115473d7da29f9602ee
karlguillotte <karl.guillotte.1@gmail.com>
2019-12-04 14:45:42 -0800

    [ANCHORS W/ HASH] Fix scrolling to <a> that have a "#hash"
    There was few issues while navigating within the app. This tries to solve most of them. See comments in code for more details.

--- client/router/FragmentIdentifier.js
+++ client/router/FragmentIdentifier.js
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useLayoutEffect } from 'react'
 import PropTypes from 'prop-types'
-import { useEventListener } from 'hooks'
 import { useLocation } from './hooks'
 
 FragmentIdentifier.propTypes = {
@@ -8,21 +7,42 @@ FragmentIdentifier.propTypes = {
     children: PropTypes.node.isRequired,
 }
 
+// This component handles hash changes that are triggered by the router...
+// Because the router does not trigger an "hashchange", this component is quite important
+// Regular "hashchange" events are handle by the browser as expected.
+
 export default function FragmentIdentifier({ hash, children, ...rest }) {
     const { location } = useLocation()
     const href = '#' + hash
     const anchor = useRef()
-    function scroll() {
-        if (window.location.hash === href) {
-            anchor.current.scrollIntoView(true)
-        }
-    }
 
-    useEventListener('hashchange', scroll)
-    useEffect(scroll, [location.hash])
+    useLayoutEffect(() => {
+        if (location.hash !== href) {
+            return
+        }
+
+        const { current } = anchor
+
+        current.scrollIntoView()
+
+        // "setTimeout" to make sure it does not intefere with the browser,
+        // when it move to a page, tries to find an anchor, does not find it,
+        // so scroll position is set to the top of the page.
+        // I do not know how to fix that without involving some weird logics with "hashchange" event.
+        // To see it in actions, comment out the code,
+        // In Chrome (using the navbar),
+        //   - go to ambassadors page
+        //   - then the main map
+        //   - back to ambassadors page to #aleks-klassen
+        // Boom! The page will not scroll.
+        // 75ms is a magic number ;(
+        setTimeout(() => {
+            current.scrollIntoView()
+        }, 75)
+    }, [location.hash, href])
 
     return (
-        <a ref={anchor} name={hash} href={href} {...rest}>
+        <a name={hash} {...rest} ref={anchor} href={href}>
             {children}
         </a>
     )
