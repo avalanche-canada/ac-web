5393f2b68a777e4295856f1d0235aeb36a7c7bd3
karlguillotte <karl.guillotte.1@gmail.com>
2019-12-23 14:57:55 -0800

    Update 2019-12-23 SPAW script to generate the image

--- scripts/spaws/2019-12-23.js
+++ scripts/spaws/2019-12-23.js
@@ -6,10 +6,10 @@ const API = 'https://api.mapbox.com'
 
 const styleId = 'ciy4r3jhm001f2rn4nyg6gluw'
 const longitude = -119.5
-const latitude = 50.85
-const zoom = 5.7
-const width = 1200
-const height = 800
+const latitude = 50.7
+const zoom = 5.4
+const width = 900
+const height = 600
 
 console.log(
     `${API}/styles/v1/${USENAME}/${styleId}/static/${longitude},${latitude},${zoom}/${width}x${height}@2x?access_token=${ACCESS_TOKEN}`
@@ -25,3 +25,4 @@ console.log(
 //     'sea-to-sky'
 //     'south-coast-inland'
 //     'south-rockies'
+//     'watertown'
