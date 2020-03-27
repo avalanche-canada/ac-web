e55bada3532a464012e89060aae0ad542f64e5c7
karlguillotte <karl.guillotte.1@gmail.com>
2020-03-27 14:24:41 -0700

    [COMPONENTS/MENU/LAYER] Add disabled property
    - This allows for map menu layer to do not react to click event

--- client/components/page/drawer/layers/Layer.js
+++ client/components/page/drawer/layers/Layer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import noop from 'lodash/noop'
 import { useToggle } from 'hooks'
 import { Expand } from 'components/button'
 import css from './Layer.css'
@@ -8,16 +9,25 @@ import css from './Layer.css'
 Layer.propTypes = {
     title: PropTypes.string.isRequired,
     visible: PropTypes.bool.isRequired,
+    disabled: PropTypes.bool.isRequired,
     icon: PropTypes.node,
     onClick: PropTypes.func.isRequired,
     children: PropTypes.element,
 }
 
-export default function Layer({ title, visible, icon, onClick, children }) {
+export default function Layer({
+    title,
+    visible,
+    disabled,
+    icon,
+    onClick,
+    children,
+}) {
     const [expanded, toggle] = useToggle(false)
     const className = classnames({
-        [css.Layer]: !visible,
-        [css['Layer--Visible']]: visible,
+        [css.Layer]: true,
+        [css.Visible]: visible,
+        [css.Disabled]: disabled,
     })
     function handleExpandClick(event) {
         event.stopPropagation()
@@ -27,10 +37,10 @@ export default function Layer({ title, visible, icon, onClick, children }) {
 
     return (
         <div className={className}>
-            <div className={css.Header} onClick={onClick}>
+            <div className={css.Header} onClick={disabled ? noop : onClick}>
                 {icon}
                 <span className={css.Title}>{title}</span>
-                {children && (
+                {Boolean(children && !disabled) && (
                     <Expand
                         chevron
                         expanded={expanded}
@@ -38,7 +48,7 @@ export default function Layer({ title, visible, icon, onClick, children }) {
                     />
                 )}
             </div>
-            {expanded && children}
+            {Boolean(expanded && !disabled) && children}
         </div>
     )
 }
