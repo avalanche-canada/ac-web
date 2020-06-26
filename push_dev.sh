09819858238d3bf462ba2519815aeb6bce1d6848
Bryce Schroers <bschroers@avalanche.ca>
2020-06-26 11:58:07 -0700

    update build scripts

--- push_dev.sh
+++ push_dev.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
  
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 349501831018.dkr.ecr.us-west-2.amazonaws.com
+
+make build
+
 docker build -t ac-web-dev .
 
 docker tag ac-web-dev:latest 349501831018.dkr.ecr.us-west-2.amazonaws.com/ac-web-dev:latest
 
 docker push 349501831018.dkr.ecr.us-west-2.amazonaws.com/ac-web-dev:latest
+
+aws ecs update-service --cluster AvCan --service ac-web-dev --force-new-deployment
--- push_qa.sh
+++ push_qa.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
  
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 349501831018.dkr.ecr.us-west-2.amazonaws.com
+
+make build
+
 docker build -t ac-web-qa .
 
 docker tag ac-web-qa:latest 349501831018.dkr.ecr.us-west-2.amazonaws.com/ac-web-qa:latest
 
 docker push 349501831018.dkr.ecr.us-west-2.amazonaws.com/ac-web-qa:latest
+
+aws ecs update-service --cluster AvCan --service ac-web-qa --force-new-deployment
