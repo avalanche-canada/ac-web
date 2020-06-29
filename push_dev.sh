b3e7ef6bed861e71c008ae123e81554084f6eddc
Bryce Schroers <bschroers@avalanche.ca>
2020-06-29 13:53:49 -0700

    Update build scripts

--- push_dev.sh
+++ push_dev.sh
@@ -2,6 +2,10 @@
  
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 349501831018.dkr.ecr.us-west-2.amazonaws.com
 
+docker login
+
+npm install
+
 make build
 
 docker build -t ac-web-dev .
--- push_qa.sh
+++ push_qa.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
  
+
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 349501831018.dkr.ecr.us-west-2.amazonaws.com
+
+docker login
+
+npm install
 
 make build
 
