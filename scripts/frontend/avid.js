1b4379b82878932eb666001ad125d33133fa3408
karlguillotte <karl.guillotte.1@gmail.com>
2020-10-29 16:34:58 -0700

    Add Intl context to the avid distribution file

--- scripts/frontend/avid.js
+++ scripts/frontend/avid.js
@@ -1,4 +1,8 @@
 import * as Components from '../../client/layouts/products/forecast'
-// import moduleName from '../../server/api/forecasts/avid'
+import * as Intl from '../../client/contexts/intl'
+import fr from '../../client/services/intl/locales/compiled/fr.json'
+
+Intl.Messages = { fr }
 
 export const Forecasts = Components
+export { Intl } 
