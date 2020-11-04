aae6c27156be79291971bd3dc8ed41e90203ff98
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-04 13:34:17 -0800

    Order import statements

--- client/components/media/index.js
+++ client/components/media/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styles from './Media.css'
 import classnames from 'classnames'
+import styles from './Media.css'
 
 Media.propTypes = {
     caption: PropTypes.node,
