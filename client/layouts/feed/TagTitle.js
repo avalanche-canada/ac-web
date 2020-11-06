c5301c054cb053f761b7189348717ad02885bf70
Russell McWhae <russell.mcwhae@gmail.com>
2020-11-06 11:11:41 -0800

    Translate Layout feed/TagTitle

--- client/layouts/feed/TagTitle.js
+++ client/layouts/feed/TagTitle.js
@@ -1,5 +1,12 @@
+import { useIntl } from 'react-intl'
+
 export default function TagTitle({ value }) {
+    const intl = useIntl()
+
     return value === 'foundation'
-        ? 'AVALANCHE CANADA FOUNDATION'
+        ? intl.formatMessage({
+            defaultMessage: 'AVALANCHE CANADA FOUNDATION',
+            description: 'Layout feed/TagTitle',
+        })
         : value.toUpperCase()
 }
