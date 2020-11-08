bf229a1fef85a36146150359c5bf64b17071aa51
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-07 19:33:15 -0800

    [REQUESTS/ADMIN] Add "changeUsername" function

--- client/requests/admin.js
+++ client/requests/admin.js
@@ -1,13 +1,33 @@
 import { apiBaseURL } from './config.json'
 import fetch from 'utils/fetch'
 import { build } from 'utils/url'
+import LOCALE from 'constants/locale'
 
 export function users(token, username, page = 1) {
     const url = build('/users', { username, page }, apiBaseURL)
 
     return fetch(url, {
-        headers: new Headers({
-            Authorization: `Bearer ${token}`,
-        }),
+        headers: createHeaders(token),
+    })
+}
+
+export function changeUsername(token, userid, username, locale) {
+    const url = build(
+        '/users/' + encodeURIComponent(userid),
+        undefined,
+        apiBaseURL
+    )
+
+    return fetch(url, {
+        method: 'PATCH',
+        body: JSON.stringify({ username }),
+        headers: createHeaders(token, locale),
+    })
+}
+
+function createHeaders(token, locale = LOCALE) {
+    return new Headers({
+        Authorization: 'Bearer ' + token,
+        'Accept-Language': locale,
     })
 }
