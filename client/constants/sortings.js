6a1740a8cfe29dbbeebf79900ef4fd9e7246bdd4
Russell McWhae <russell.mcwhae@gmail.com>
2020-11-12 10:19:27 -0800

    Removes unused code

--- client/constants/sortings.js
+++ client/constants/sortings.js
@@ -3,20 +3,3 @@ import { useIntlMemo } from 'hooks/intl'
 export const ASC = 'asc'
 export const DESC = 'desc'
 export const NONE = 'none'
-
-export function useSortings() {
-  return useIntlMemo((intl) => new Map([
-    [ASC, intl.formatMessage({
-      defaultMessage: 'asc',
-      description: 'Constants prismic',
-    })],
-    [DESC, intl.formatMessage({
-      defaultMessage: 'desc',
-      description: 'Constants prismic',
-    })],
-    [NONE, intl.formatMessage({
-      defaultMessage: 'none',
-      description: 'Constants prismic',
-    })]
-  ]))
-}
