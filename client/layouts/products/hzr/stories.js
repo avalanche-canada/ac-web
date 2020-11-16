9dd1f426267de147817d075a95fb676598b745ae
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-15 21:42:30 -0800

    Remove all stories

--- client/layouts/products/hzr/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf, action } from '@storybook/react'
-import {Report} from './'
-import { storeDecorator } from '../../../../.storybook/decorators'
-
-storiesOf('Avalanche Advisory', module)
-    .addDecorator(storeDecorator)
-    .add('Avalanche Advisory', () => <Report />)
