c8752f19058c4215a5baea5f110b6ed1e3742560
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-18 16:13:49 -0800

    Update avid.js

--- scripts/frontend/avid.js
+++ scripts/frontend/avid.js
@@ -1,8 +1,9 @@
 import * as Components from '../../client/layouts/products/forecast'
 import * as Intl from '../../client/contexts/intl'
-import fr from '../../client/services/intl/locales/compiled/fr.json'
+import fr from '../../client/services/intl/locales/compiled/fr-CA.json'
+import en from '../../client/services/intl/locales/compiled/en-CA.json'
 
-Intl.Messages = { fr }
+Intl.Messages = { fr, en }
 
 export const Forecasts = Components
-export { Intl } 
+export { Intl }
