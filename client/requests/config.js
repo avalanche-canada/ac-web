c1be1efc0d57491caf4ccf998875a8d1bb688dc3
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-18 16:51:42 -0800

    Update config.js

--- client/requests/config.js
+++ client/requests/config.js
@@ -1,5 +1,5 @@
 export const baseURL = process.env.REACT_APP_AVCAN_API_URL_LEGACY || 'https://avalanche.ca/api'
-export const staticBaseURL = process.env.REACT_APP_STATIC_URL || 'https://avalanche.ca/static'
+export const staticBaseURL = process.env.REACT_APP_STATIC_URL || 'https://avalanche.ca/api/static'
 export const apiBaseURL = process.env.REACT_APP_AVCAN_API_URL || 'https://api.avalanche.ca'
 export const astBaseUrl = process.env.REACT_APP_AST_API_URL || 'https://ast.avalanche.ca/api'
 export const weatherBaseUrl =
