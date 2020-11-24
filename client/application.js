9eb76d37274339ae4bdcf2fbfff90036f08c964f
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-23 18:11:46 -0800

    Display <NotProductionWarning> if env var REACT_APP_SHOW_NOT_PRODUCTION_WARNING truth

--- client/application.js
+++ client/application.js
@@ -1,17 +1,15 @@
-import React, { StrictMode } from 'react'
+import React, { StrictMode, lazy } from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@reach/router'
 import { Provider as IntlProvider } from 'contexts/intl'
 import { AvalancheCanada, AvalancheCanadaFoundation } from 'layouts'
 import ScrollTo from 'components/ScrollTo'
-import Dialog, { Footer, Body } from 'components/dialog'
 import { Boundary as ErrorBoundary } from 'components/error'
 import Analytics from 'services/analytics'
 import { Fallback } from 'layouts/pages'
 import { LocationProvider } from 'router/hooks'
+import Bundle from 'components/Bundle'
 import Navbar from 'components/navbar'
-import Button from 'components/button'
-import { useBoolean } from 'hooks'
 
 import 'styles'
 
@@ -23,11 +21,15 @@ export default function application() {
                     <StrictMode>
                         <Analytics>
                             <ScrollTo>
-                                <NotProductionWarning />
                                 <Router primary={false}>
                                     <AvalancheCanada path="/*" />
                                     <AvalancheCanadaFoundation path="foundation/*" />
                                 </Router>
+                                {Boolean(process.env.REACT_APP_SHOW_NOT_PRODUCTION_WARNING) && (
+                                    <Bundle>
+                                        <NotProductionWarning />
+                                    </Bundle>
+                                )}
                             </ScrollTo>
                         </Analytics>
                     </StrictMode>
@@ -37,47 +39,4 @@ export default function application() {
         document.getElementById('app')
     )
 }
-
-// Constants
-function NotProductionWarning() {
-    const [open, , close] = useBoolean(true)
-    const { hostname } = document.location
-
-    if (!FAKE_WEBSITE_URLS.has(hostname)) {
-        return null
-    }
-
-    const STYLE = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '2em',
-        boxSizing: 'border-box',
-        height: '100%',
-    }
-
-    return (
-        <Dialog open={open}>
-            <Body>
-                <div style={STYLE}>
-                    <h1>This is not the real Avalanche Canada website.</h1>
-                    <p className="secondary-big-button" style={{ flex: 1 }}>
-                        <a href="https://avalanche.ca">
-                            Bring me to avalanche.ca
-                        </a>
-                    </p>
-                </div>
-            </Body>
-            <Footer>
-                <Button chevron="RIGHT" onClick={close}>
-                    I know what I am doing
-                </Button>
-            </Footer>
-        </Dialog>
-    )
-}
-const FAKE_WEBSITE_URLS = new Set([
-    'qa.avalanche.ca',
-    'dev.avalanche.ca',
-    // 'localhost',
-])
+const NotProductionWarning = lazy(() => import('layouts/NotProductionWarning'))
