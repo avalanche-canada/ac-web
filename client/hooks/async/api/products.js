8a878e3e6f283f2e2cd8b16daa1638dd6f3abce7
karlguillotte <karl.guillotte.1@gmail.com>
2020-11-25 10:37:55 -0800

    Integrate the archive product API + remove unused functions

--- client/hooks/async/api/products.js
+++ client/hooks/async/api/products.js
@@ -3,39 +3,30 @@ import * as Requests from 'requests/api'
 import * as Products from 'constants/products'
 import { useCacheAsync, createKey } from '../'
 import { useLanguage } from 'contexts/intl'
-import { DateParam } from 'hooks/params'
-
-export function useForecasts(date) {
-    return useProductsOfTypes(Products.isKindOfForecast, date)
-}
 
 export function useForecast(id) {
-    return useProduct(id)
-}
-
-function useProductsOfTypes(predicate, date) {
-    const [products = ARRAY, pending, error] = useProducts(date)
-
-    return React.useMemo(
-        () => [products.filter(product => predicate(product.type)), pending, error],
-        [predicate, products, pending, error]
-    )
-}
-
-function useProduct(id) {
     const language = useLanguage()
     const params = [language, id]
-    const key = createKey('product', params)
+    const key = createKey('products', params)
 
     return useCacheAsync(Requests.product, params, undefined, key)
 }
 
-function useProducts(date) {
+export function useArchiveForecasts(date) {
     const language = useLanguage()
     const params = [language, date]
-    const key = createKey('products', language, DateParam.format(date))
+    const key = createKey('products', 'archive', language, date.toISOString())
+    const [products = ARRAY, pending, error] = useCacheAsync(
+        Requests.archive,
+        params,
+        undefined,
+        key
+    )
 
-    return useCacheAsync(Requests.products, params, undefined, key)
+    return React.useMemo(
+        () => [products.filter(product => Products.isKindOfForecast(product.type)), pending, error],
+        [products, pending, error]
+    )
 }
 
 const ARRAY = []
--- client/requests/api/index.js
+++ client/requests/api/index.js
@@ -7,26 +7,28 @@ export function product(language, id) {
     return get(language, 'products', id)
 }
 
-export function products(language, date) {
-    return get(language, 'products', undefined, date)
+export function products(language) {
+    return get(language, 'products')
 }
 
 export function area(language, id) {
     return get(language, 'areas', id)
 }
 
-export function areas(language, date) {
-    return get(language, 'areas', undefined, date)
+export function areas(language) {
+    return get(language, 'areas')
 }
 
 export function metadata(language) {
     return get(language, 'metadata')
 }
 
+export function archive(language, date) {
+    return get(language, 'archive', undefined, date.toISOString())
+}
+
 // Utils
 function get(language, type, id, date) {
-    date = DateParam.format(date)
-
     const url = utils.path(config.url, 'forecasts', language, type, id, date)
     const options = {
         headers: new Headers({
