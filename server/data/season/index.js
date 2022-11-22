b65c5e30aed9f4c85f541f3ff3f8cccaaee201e6
Bryce Schroers <bschroers@avalanche.ca>
2022-11-22 15:25:56 -0800

    update sponsors config

--- server/data/season/index.js
+++ server/data/season/index.js
@@ -3,8 +3,8 @@ var config = require('../../config/environment');
 
 var season = config.AC_SEASON;
 
-if (!season.match(/^(2015|2016|2019|2022)$/)) {
-    throw new Error('Season MUST be 2016, 2015, 2019 or 2022');
+if (!season.match(/^(2015|2016|2019|2022|2023)$/)) {
+    throw new Error('Season MUST be 2016, 2015, 2019, 2022 or 2023');
 }
 
 module.exports = {
